047d35b2aeb527f54aa09c79ca5ed51557587cd1
David Flores <dmousex@gmail.com>
2013-10-19 22:06:12 -0500

    generate:module and update composer.json and update bin/console

---
+++ lib/Drupal/AppConsole/Generator/ControllerGenerator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\AppConsole\Generator;
+
+use Symfony\Component\DependencyInjection\Container;
+
+class ControllerGenerator extends Generator {
+
+  private $filesystem;
+
+  public function __construct() {}
+
+  public function generate($module, $name, $controller, $services ) {
+    $path = DRUPAL_ROOT . '/' . drupal_get_path('module', $module);
+
+    $path_controller = $path . '/lib/Drupal/' . $module . '/Controller';
+
+    $parameters = array(
+      'name' => $name,
+      'services' => $services,
+      'module' => $module
+    );
+
+    $this->renderFile(
+      'module/module.DefaultController.php.twig',
+      $path_controller . '/'. $name .'.php',
+      $parameters
+    );
+
+  }
+
+}
