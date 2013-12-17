1e00947bd353095cc470161ff96dde5556e84591
Luis Montealegre <luis.montealegre@mandragora-web-systems.com>
2013-12-17 23:58:14 +0000

    Fixed format.

--- bin/stub.php
+++ bin/stub.php
@@ -1,17 +1,16 @@
 <?php
-function autoload($className)
-{
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+function autoload($className) {
+  $className = ltrim($className, '\\');
+  $fileName  = '';
+  $namespace = '';
+  if ($lastNsPos = strrpos($className, '\\')) {
+    $namespace = substr($className, 0, $lastNsPos);
+    $className = substr($className, $lastNsPos + 1);
+    $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+  }
+  $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
-    require 'phar://' . __FILE__ . DIRECTORY_SEPARATOR .$fileName;
+  require 'phar://' . __FILE__ . DIRECTORY_SEPARATOR .$fileName;
 }
 
 spl_autoload_register('autoload');
