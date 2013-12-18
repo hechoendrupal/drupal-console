b32a7008ae9684ecb8c12f1fda230abb8b6b5080
Luis Montealegre <luis.montealegre@mandragora-web-systems.com>
2013-12-18 06:56:09 +0000

    Update unit tests.

--- tests/unit/Drupal/AppConsole/Console/ApplicationTest.php
+++ tests/unit/Drupal/AppConsole/Console/ApplicationTest.php
@@ -12,131 +12,220 @@ class ApplicationTest extends TestCase {
    */
   protected $dispatcher;
 
-  public function setUp() {
+  /**
+   * @var \Symfony\Component\Console\Helper\HelperSet
+   */
+  protected $helperSet;
+
+  /**
+   * @var \Drupal\AppConsole\Command\Helper\BootstrapFinderHelper
+   */
+  protected $bootstrapFinder;
+
+  /**
+   * @var \Drupal\AppConsole\Command\Helper\DrupalBootstrapHelper
+   */
+  protected $drupalBootstrap;
+
+  /**
+   * @var \Drupal\AppConsole\Command\Helper\KernelHelper
+   */
+  protected $kernel;
+
+  /**
+   * @var \Symfony\Component\EventDispatcher\EventDispatcher
+   */
+  protected $eventDispatcher;
+
+  /**
+   * @var \Drupal\AppConsole\Command\Helper\ShellHelper
+   */
+  protected $shellHelper;
+
+  /**
+   * @var \Drupal\AppConsole\Console\Shell
+   */
+  protected $shell;
+
+  /**
+   * @var \Drupal\AppConsole\Command\GeneratorModuleCommand
+   */
+  protected $command;
+
+  protected function setUp() {
     $this->dispatcher = $this->getMockBuilder('Symfony\Component\EventDispatcher\EventDispatcherInterface')
                              ->getMock();
-  }
-
-  protected function getKernelMock() {
-      $container = $this->getMockBuilder('Symfony\Component\DependencyInjection\ContainerInterface')
+    $this->helperSet = $this->getMockBuilder('Symfony\Component\Console\Helper\HelperSet')
+                            ->getMock();
+    $this->bootstrapFinder = $this->getMockBuilder('Drupal\AppConsole\Command\Helper\BootstrapFinderHelper')
+                                  ->disableOriginalConstructor()
+                                  ->getMock();
+    $this->drupalBootstrap = $this->getMockBuilder('Drupal\AppConsole\Command\Helper\DrupalBootstrapHelper')
+                                  ->getMock();
+    $this->kernel = $this->getMockBuilder('Drupal\AppConsole\Command\Helper\KernelHelper')
+                         ->getMock();
+    $this->eventDispatcher = $this->getMockBuilder('Symfony\Component\EventDispatcher\EventDispatcher')
+                                  ->getMock();
+    $this->shell = $this->getMockBuilder('Drupal\AppConsole\Console\Shell')
+                        ->disableOriginalConstructor()
                         ->getMock();
-
-      $container->expects($this->once())
-                ->method('get')
-                ->with('event_dispatcher')
-                ->will($this->returnValue($this->dispatcher));
-
-      $kernel = $this->getMockBuilder('Drupal\Core\DrupalKernel')
-                     ->disableOriginalConstructor()
-                     ->getMock();
-
-      $kernel->expects($this->once())
-             ->method('boot');
-
-      $kernel->expects($this->once())
-             ->method('getContainer')
-             ->will($this->returnValue($container));
-
-      return $kernel;
+    $this->shellHelper = $this->getMockBuilder('Drupal\AppConsole\Command\Helper\ShellHelper')
+                              ->disableOriginalConstructor()
+                              ->getMock();
+    $this->input = $this->getMockBuilder('Symfony\Component\Console\Input\ArgvInput')
+                        ->disableOriginalConstructor()
+                        ->getMock();
+    $this->command = $this->getMockBuilder('Drupal\AppConsole\Command\GeneratorModuleCommand')
+                          ->disableOriginalConstructor()
+                          ->getMock();
   }
 
   public function testCanRunApplication() {
-    $kernel = $this->getKernelMock();
+    $this->expectsThatDrupalBootstrapHelperIsRegistered();
+    $this->expectsThatBootstrapFinderHelperIsRegistered();
+    $this->expectsThatKernelHelperIsRegistered();
+    $this->expectsThatKernelHelperIsRetrievedToGetDrupalKernelConfigured();
+    $this->expectsThatKernelHelperIsCalledToConfigureDrupalKernel();
 
-    $application = new Application($kernel);
+    $application = new Application();
     $application->setAutoExit(false);
+    $application->setHelperSet($this->helperSet);
 
     $this->assertEquals(0, $application->run(new ArrayInput(array()), new NullOutput()));
   }
 
-  public function testShellOptionsUsesCustomShell() {
+  public function testApplicationUsesDrupalShell() {
+    $this->expectsThatDrupalBootstrapHelperIsRegistered();
+    $this->expectsThatBootstrapFinderHelperIsRegistered();
+    $this->expectsThatKernelHelperIsRegistered();
+    $this->expectsThatKernelHelperIsRetrievedToGetDrupalKernelConfigured();
+    $this->expectsThatKernelHelperIsCalledToConfigureDrupalKernel();
 
-    $shell = $this->getMockBuilder('Drupal\AppConsole\Console\Shell')
-                  ->disableOriginalConstructor()
-                  ->getMock();
+    $this->expectsThatShellHelperIsRegistered();
+    $this->expectsThatShellHelperGetsShell();
+    $this->expectsThatDrupalShellIsRun();
+    $this->expectsThatInpuHasShellParameters();
 
-    $shell->expects($this->once())
-          ->method('setProcessIsolation');
-
-    $shell->expects($this->once())
-          ->method('run');
-
-    $shellHelper = $this->getMockBuilder('Drupal\AppConsole\Command\Helper\ShellHelper')
-                        ->disableOriginalConstructor()
-                        ->getMock();
-
-    $shellHelper->expects($this->once())
-                ->method('getShell')
-                ->will($this->returnValue($shell));
-
-    $helperSet = $this->getMockBuilder('Symfony\Component\Console\Helper\HelperSet')
-                      ->getMock();
-
-    $helperSet->expects($this->once())
-              ->method('get')
-              ->with('shell')
-              ->will($this->returnValue($shellHelper));
-
-    $kernel = $this->getKernelMock();
-
-    $application = new Application($kernel);
+    $application = new Application();
     $application->setAutoExit(false);
-    $application->setHelperSet($helperSet);
+    $application->setHelperSet($this->helperSet);
 
-    $input = $this->getMockBuilder('Symfony\Component\Console\Input\ArgvInput')
-                  ->disableOriginalConstructor()
-                  ->getMock();
-
-    $input->expects($this->at(14))
-          ->method('hasParameterOption')
-          ->with(array('--shell', '-s'))
-          ->will($this->returnValue(true));
-
-    $input->expects($this->at(15))
-          ->method('hasParameterOption')
-          ->with(array('--process-isolation'));
-
-    $this->assertEquals(0, $application->run($input, new NullOutput()));
+    $this->assertEquals(0, $application->run($this->input, new NullOutput()));
   }
 
   public function testCanRunDrupalCommand() {
+    $this->expectsThatDrupalBootstrapHelperIsRegistered();
+    $this->expectsThatBootstrapFinderHelperIsRegistered();
+    $this->expectsThatKernelHelperIsRegistered();
+    $this->expectsThatKernelHelperIsRetrievedToGetDrupalKernelConfigured();
+    $this->expectsThatKernelHelperIsCalledToConfigureDrupalKernel();
 
-    $kernel = $this->getKernelMock();
-    $application = new Application($kernel);
+    $this->expectsThatDrupalCommandIsRun();
+    $this->expectsThatInputFirstArgumentIsGenerateModuleCommand();
+    $this->expectsThatRunningACommandTriggersTheDispatcher();
+
+    $application = new Application();
     $application->setAutoExit(false);
+    $application->setHelperSet($this->helperSet);
+    $application->add($this->command);
 
-    $command = $this->getMockBuilder('\Drupal\AppConsole\Command\GeneratorModuleCommand')
-                    ->disableOriginalConstructor()
-                    ->getMock();
+    $this->assertEquals(0, $application->run($this->input, new NullOutput()));
+  }
 
-    $command->expects($this->once())
-            ->method('isEnabled')
-            ->will($this->returnValue(true));
+  protected function expectsThatDrupalBootstrapHelperIsRegistered() {
+    $this->helperSet->expects($this->at(1))
+                    ->method('get')
+                    ->with('bootstrap')
+                    ->will($this->returnValue($this->drupalBootstrap));
+  }
 
-    $command->expects($this->any())
-            ->method('getAliases')
-            ->will($this->returnValue(array()));
+  protected function expectsThatBootstrapFinderHelperIsRegistered() {
+    $this->helperSet->expects($this->at(2))
+                    ->method('get')
+                    ->with('finder')
+                    ->will($this->returnValue($this->bootstrapFinder));
+  }
 
-    $command->expects($this->any())
-            ->method('getName')
-            ->will($this->returnValue('generate:module'));
+  protected function expectsThatKernelHelperIsRegistered() {
+    $this->helperSet->expects($this->at(3))
+                    ->method('get')
+                    ->with('kernel')
+                    ->will($this->returnValue($this->kernel));
+  }
 
-    $command->expects($this->once())
-            ->method('run')
-            ->will($this->returnValue(0));
+  protected function expectsThatKernelHelperIsRetrievedToGetDrupalKernelConfigured() {
+    $this->helperSet->expects($this->at(4))
+                    ->method('get')
+                    ->with('kernel')
+                    ->will($this->returnValue($this->kernel));
+  }
 
-    $input = $this->getMockBuilder('Symfony\Component\Console\Input\ArgvInput')
-                  ->disableOriginalConstructor()
-                  ->getMock();
+  protected function expectsThatKernelHelperIsCalledToConfigureDrupalKernel() {
+    $this->kernel->expects($this->once())
+                 ->method('bootKernel');
+    $this->kernel->expects($this->once())
+                ->method('initCommands');
+    $this->kernel->expects($this->once())
+                 ->method('getEventDispatcher')
+                 ->will($this->returnValue($this->eventDispatcher));
+  }
 
-    $input->expects($this->once())
-          ->method('getFirstArgument')
-          ->will($this->returnValue('generate:module'));
+  protected function expectsThatShellHelperIsRegistered() {
+    $this->helperSet->expects($this->at(5))
+                    ->method('get')
+                    ->with('shell')
+                    ->will($this->returnValue($this->shellHelper));
+  }
 
-    $this->dispatcher->expects($this->any())->method('dispatch');
+  protected function expectsThatShellHelperGetsShell() {
+    $this->shellHelper->expects($this->once())
+                      ->method('getShell')
+                      ->will($this->returnValue($this->shell));
+  }
 
-    $application->add($command);
+  protected function expectsThatDrupalShellIsRun() {
+    $this->shell->expects($this->once())
+                ->method('setProcessIsolation');
+    $this->shell->expects($this->once())
+                ->method('run');
+  }
 
-    $this->assertEquals(0, $application->run($input, new NullOutput()));
+  protected function expectsThatInpuHasShellParameters() {
+    $this->input->expects($this->at(17))
+                ->method('hasParameterOption')
+                ->with(array('--shell', '-s'))
+                ->will($this->returnValue(true));
+    $this->input->expects($this->at(18))
+                ->method('hasParameterOption')
+                ->with(array('--process-isolation'));
+  }
+
+  protected function expectsThatDrupalCommandIsRun() {
+    $this->command->expects($this->once())
+                  ->method('isEnabled')
+                  ->will($this->returnValue(true));
+
+    $this->command->expects($this->any())
+                  ->method('getAliases')
+                  ->will($this->returnValue(array()));
+
+    $this->command->expects($this->any())
+                  ->method('getName')
+                  ->will($this->returnValue('generate:module'));
+
+    $this->command->expects($this->once())
+                  ->method('run')
+                  ->will($this->returnValue(0));
+  }
+
+  protected function expectsThatInputFirstArgumentIsGenerateModuleCommand() {
+    $this->input->expects($this->once())
+                ->method('getFirstArgument')
+                ->will($this->returnValue('generate:module'));
+  }
+
+  protected function expectsThatRunningACommandTriggersTheDispatcher() {
+    $this->dispatcher->expects($this->any())
+                     ->method('dispatch');
   }
 }
