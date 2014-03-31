8c516b87f2e52f252f873f0a332bc93f0515d33b
David Flores <dmousex@gmail.com>
2014-03-30 22:44:38 -0600

    convert registrationCommands in helper to easy test

--- lib/Drupal/AppConsole/Console/Application.php
+++ lib/Drupal/AppConsole/Console/Application.php
@@ -109,59 +109,7 @@ class Application extends BaseApplication {
   }
 
   protected function registerCommands() {
-
-    // Get Container
-    $kernelHelper = $this->getHelperSet()->get('kernel');
-    $kernel = $kernelHelper->getKernel();
-    $container = $kernel->getContainer();
-
-    // Get Module handler
-    $module_handler = $container->get('module_handler');
-    $modules = $module_handler->getModuleDirectories();
-
-    // Get Transversal, namespaces
-    $namespaces = $container->get('container.namespaces');
-    $namespaces = $namespaces->getArrayCopy();
-
-    $finder = new Finder();
-    foreach ($modules as $module => $directory) {
-
-      $place = $namespaces['Drupal\\'.$module];
-      $dir = $place. '/Drupal/' . $module . '/Command';
-      $prefix = 'Drupal\\'.$module . '\\Command';
-
-      if (!is_dir($dir)) {
-        continue;
-      }
-      
-      $finder->files()
-        ->name('*Command.php')
-        ->in($dir)
-        ->depth('< 2')
-      ;
-
-      foreach ($finder as $file) {
-        $ns = $prefix;
-        
-        if ($relativePath = $file->getRelativePath()) {
-          $ns .= '\\'.strtr($relativePath, '/', '\\');
-        }
-        
-        $class = $ns.'\\'.$file->getBasename('.php');
-
-        if (class_exists($class)){
-          $r = new \ReflectionClass($class);
-          // if is a valid command
-          if ($r->isSubclassOf('Symfony\\Component\\Console\\Command\\Command') 
-            && !$r->isAbstract() 
-            && !$r->getConstructor()->getNumberOfRequiredParameters()) {
-
-            // Register command
-            $this->add($r->newInstance());
-          }
-        }
-      }
-
-    }
+    $rc = $this->getHelperSet()->get('register_commands');
+    $rc->register();
   }
 }
