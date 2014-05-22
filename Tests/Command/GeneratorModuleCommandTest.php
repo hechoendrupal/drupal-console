664de8ad287d2549d754f4a9a9adcf25072960af
Luis Cordova <cordoval@gmail.com>
2014-05-21 22:06:28 -0500

    fixing more docblocks

--- Tests/Command/GeneratorModuleCommandTest.php
+++ Tests/Command/GeneratorModuleCommandTest.php
@@ -31,20 +31,17 @@ class GeneratorModuleCommandTest extends GenerateCommandTest {
     $cmd->execute($options);
   }
 
-  /**
-   * data provider
-   */
   public function getInteractiveData(){
     $dir = sys_get_temp_dir();
 
     return [
-      //case one basic options
+      // case one basic options
       [
         [],
         ['foo', $dir, 'My Awesome Module', '8.x', 'Other', false, true, false, true, false],
         "foo\n$dir\n"
       ],
-      //case two skip-root
+      // case two skip-root
       [
         ['--skip-root'=> true,'--module-path'=> $dir,'--description'=>'My old module','--package'=>'Other'],
         ['foo', $dir, "My old module", '8.x', 'Other', false, true, false, true, true],
@@ -73,9 +70,10 @@ class GeneratorModuleCommandTest extends GenerateCommandTest {
   }
 
   public function getNoInteractiveData(){
+
     $dir = sys_get_temp_dir();
     return [
-      //case one
+      // case one
       [
         ['--module'=>'bar','--module-path'=>$dir, '--description'=>'My Awesome Module','--core'=>'8.x','--package'=>'Other', '--controller'=>true,'--tests'=>true,'--setting'=>true,'--structure'=>true],
         ['bar', $dir, "My Awesome Module", '8.x', 'Other', true, true, true, true, false],
@@ -87,8 +85,9 @@ class GeneratorModuleCommandTest extends GenerateCommandTest {
     ];
   }
 
-
   protected function getCommand($generator, $input){
+
+    /** @var \Drupal\AppConsole\Command\GeneratorModuleCommand $command */
     $command = $this
       ->getMockBuilder('Drupal\AppConsole\Command\GeneratorModuleCommand')
       ->setMethods(null)
