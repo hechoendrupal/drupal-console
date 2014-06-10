67ed91077038d7c17cc61efc2fb4d406ab8db3dd
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-06-10 12:04:56 -0700

    Add service generator
    * Register generator on bin/console.php
    * Add GeneratorServiceCommand.php & ServiceGenerator.php classes
    * Add servicesAsParametersKeys funtion on Generator class
    * Remove file module.services.yml.twig
    * Add Twig templates services.yml.twig & services.class.php.twig
    * Update template services-use-operator.php.twig passing argument

---
+++ lib/Drupal/AppConsole/Generator/ServiceGenerator.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ *@file
+ * Contains \Drupal\AppConsole\Generator\ServiceGenerator.
+ */
+
+namespace Drupal\AppConsole\Generator;
+
+class ServiceGenerator extends Generator {
+
+  /**
+   * Generator Plugin Block
+   * @param  string $module       Module name
+   * @param  string $service_name Service name
+   * @param  string $class_name   Class name
+   * @param  array  $services     list of services
+   */
+  public function generate($module, $service_name, $class_name, $services) {
+
+    $module_path = DRUPAL_ROOT . '/' . drupal_get_path('module', $module);
+
+    // set syntax for arguments
+    $args = ', ';
+    $i = 0;
+    foreach ($services as $service) {
+      $args .= $service['short'] . ' $' . $service['machine_name'];
+      if ( ++$i != count($services)) {
+        $args .= ', ';
+      }
+    }
+
+    $parameters = [
+      'module'   => $module,
+      'service_name'     => $service_name,
+      'name'     => [
+        'class'      => $class_name,
+        'underscore' => $this->camelCaseToUnderscore($class_name)
+      ],
+      'services'    => $services,
+      'args'   => $args
+    ];
+
+    $parameters['file_exists'] = file_exists($module_path.'/'.$module.'.services.yml');
+
+    $this->renderFile(
+      'module/services.yml.twig',
+      $module_path.'/'.$module.'.services.yml',
+      $parameters,
+      FILE_APPEND
+    );
+
+    $this->renderFile(
+      'module/services.class.php.twig',
+      $module_path.'/src/'. $class_name .'.php',
+      $parameters
+    );
+
+  }
+
+}
