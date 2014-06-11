f05630e5c3bec4188e29870f9ad0bf87324287a7
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-06-11 08:50:56 -0700

    Add new method to Util.php
    * Add new const REGEX_CAMEL_CASE_UNDER
    * Add new method camelCaseToUnderscore
    * Fix dockblock indentation
    * Rename input parameter on functions:
     * createMachineName
     * camelCaseToMachineName

--- lib/Drupal/AppConsole/Utils/Utils.php
+++ lib/Drupal/AppConsole/Utils/Utils.php
@@ -14,15 +14,16 @@ class Utils
   const REGEX_UPPER_CASE_LETTERS = '/(?<=\\w)(?=[A-Z])/';
   // This REGEX captures non alphanumeric characters and non underscores
   const REGEX_MACHINE_NAME_CHARS = '@[^a-z0-9_]+@';
+  // This REGEX captures
+  const REGEX_CAMEL_CASE_UNDER = '/([a-z])([A-Z])/';
 
   /**
    * Replaces non alphanumeric characters with underscores
-   * @param String $input User input
+   * @param String  $name         User input
    * @return String $machine_name User input in machine-name format
    */
-  public static function createMachineName($input)
-  {
-    $machine_name = preg_replace(self::REGEX_MACHINE_NAME_CHARS,'_',strtolower($input));
+  public static function createMachineName($name) {
+    $machine_name = preg_replace(self::REGEX_MACHINE_NAME_CHARS,'_',strtolower($name));
     $machine_name = trim($machine_name, '_');
     
     return $machine_name;
@@ -30,16 +31,24 @@ class Utils
 
   /**
    *  Converts camel-case strings to machine-name format
-   *  @param String $name User input
-   *  @return String $machine_name User input in machine-name format
+   *  @param  String $name          User input
+   *  @return String $machine_name  User input in machine-name format
    */
-  public static function camelCaseToMachineName($camel_case)
-  {
-  	$machine_name = preg_replace(self::REGEX_UPPER_CASE_LETTERS,"_$1", $camel_case);
+  public static function camelCaseToMachineName($name) {
+  	$machine_name = preg_replace(self::REGEX_UPPER_CASE_LETTERS,"_$1", $name);
     $machine_name = preg_replace(self::REGEX_MACHINE_NAME_CHARS,'_',strtolower($machine_name));
     $machine_name = trim($machine_name, '_');
     
     return $machine_name;
+  }
+
+  /**
+   *  Converts camel-case strings to under-score format
+   *  @param  String $camel_case  User input
+   *  @return String
+   */
+  public static function camelCaseToUnderscore($camel_case){
+    return strtolower(preg_replace(REGEX_CAMEL_CASE_UNDER, '$1_$2', $camel_case));
   }
 
 }
