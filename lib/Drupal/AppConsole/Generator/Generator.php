dfda0bcb457551d91d6640f59ae2153513a7bd8a
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-06-11 09:41:11 -0700

    Call camelCaseToUnderscore method from Utils

--- lib/Drupal/AppConsole/Generator/Generator.php
+++ lib/Drupal/AppConsole/Generator/Generator.php
@@ -11,6 +11,8 @@
 
 namespace Drupal\AppConsole\Generator;
 
+use Drupal\AppConsole\Utils\Utils;
+
 class Generator {
   private $skeletonDirs;
 
@@ -47,8 +49,8 @@ class Generator {
     return file_put_contents($target, $this->render($template, $parameters), $flag);
   }
 
-  public function camelCaseToUnderscore($camelcase){
-    return strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $camelcase));
+  public function camelCaseToUnderscore($camel_case){
+    return Utils::camelCaseToUnderscore($camel_case);
   }
 
   public function getServiceAsParamater() {
