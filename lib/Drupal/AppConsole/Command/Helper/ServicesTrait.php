af07b147b1c800e0673999f38c595ddb9288b1a1
David Flores <dmousex@gmail.com>
2014-06-22 20:20:40 -0500

    Services Trait

---
+++ lib/Drupal/AppConsole/Command/Helper/ServicesTrait.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @file
+ * Containt Drupal\AppConsole\Command\Helper\ServicesTrait.
+ */
+
+namespace Drupal\AppConsole\Command\Helper;
+
+trait ServicesTrait
+{
+
+	public function servicesQuestion($input, $output, $dialog)
+	{
+		if ($dialog->askConfirmation(
+      $output,
+      $dialog->getQuestion('Do you like add service(s)', 'yes', '?'),
+      true
+    )) {
+      $service_collection = [];
+      $output->writeln([
+        '',
+        'You can add some services, type the name or use keyup and keydown',
+        'This is optional, press <info>enter</info> to <info>continue</info>',
+        ''
+      ]);
+
+      $services = $this->getServices();
+      while (true) {
+        $service = $dialog->askAndValidate(
+          $output,
+          $dialog->getQuestion(' Enter your service',''),
+          function ($service) use ($services) {
+            return $this->validateServiceExist($service, $services);
+          },
+          false,
+          null,
+          $services
+        );
+
+        if ($service == null) {
+          break;
+        }
+
+        array_push($service_collection, $service);
+        $service_key = array_search($service, $services, true);
+
+        if ($service_key >= 0)
+          unset($services[$service_key]);
+      }
+      
+      return $service_collection;
+    }
+    return null;
+	}
+
+	/**
+	 * 
+	 * @param  Array $services
+	 * @return Array  
+	 */
+	public function buildServices($services)
+	{
+		$build_service = [];
+    if (!empty($services)) {
+      foreach ($services as $service) {
+        $class = get_class($this->getContainer()->get($service));
+        $build_service[$service] = [
+          'name' => $service,
+          'machine_name' => str_replace('.', '_', $service),
+          'class' => $class,
+          'short' => end(explode('\\', $class))
+      	];
+      }
+    }
+    return $build_service;
+	}
+}
