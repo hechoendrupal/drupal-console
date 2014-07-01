060396fa77acab31947336ccb9a647dbf2abfaf2
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-07-01 16:05:35 -0700

    Call getStringUtils method instead of static

--- src/Command/GeneratorPluginBlockCommand.php
+++ src/Command/GeneratorPluginBlockCommand.php
@@ -86,7 +86,7 @@ class GeneratorPluginBlockCommand extends GeneratorCommand
     }
     $input->setOption('class-name', $class_name);
 
-    $machine_name = Utils::camelCaseToUnderscore($class_name);
+    $machine_name = $this->getStringUtils()->camelCaseToUnderscore($class_name);
 
     // --plugin-label option
     $plugin_label = $input->getOption('plugin-label');
