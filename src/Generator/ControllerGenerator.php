d9e6544b5e9d55ea4e324e1a558539827b386683
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-07-02 10:03:40 -0700

    Add new argument

--- src/Generator/ControllerGenerator.php
+++ src/Generator/ControllerGenerator.php
@@ -9,7 +9,7 @@ namespace Drupal\AppConsole\Generator;
 class ControllerGenerator extends Generator
 {
 
-  public function generate($module, $class_name, $method_name, $route, $test, $services)
+  public function generate($module, $class_name, $method_name, $route, $test, $services, $class_machine_name)
   {
 
     $parameters = array(
