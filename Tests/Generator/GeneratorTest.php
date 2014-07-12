871599f9ac3a045000cae2be6b9ebb55eaab40aa
David Flores <dmousex@gmail.com>
2014-07-12 03:16:06 -0500

    update GeneratorTest base

--- Tests/Generator/GeneratorTest.php
+++ Tests/Generator/GeneratorTest.php
@@ -10,10 +10,17 @@ use Symfony\Component\Filesystem\Filesystem;
 
 abstract class GeneratorTest extends \PHPUnit_Framework_TestCase 
 {
+  var $dir;
+
   public function setUp()
   {
+    $this->setUpTemporalDirectory();
+  }
+
+  public function setUpTemporalDirectory()
+  {
     $this->dir = sys_get_temp_dir() . "/module";
-    
+
     $this->filesystem = new Filesystem();
     $this->filesystem->remove($this->dir);
   }
