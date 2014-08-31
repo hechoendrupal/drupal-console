c89a4dc33bd01180d4cdb95e05ebe3bde8dc575f
Omar Aguirre <omersguchigu@gmail.com>
2014-08-30 22:00:39 -0500

    initialize EntityType and CommandName on GeneratorEntityContentCommand class

--- src/Command/GeneratorEntityContentCommand.php
+++ src/Command/GeneratorEntityContentCommand.php
@@ -4,8 +4,18 @@ namespace Drupal\AppConsole\Command;
 
 class GeneratorEntityContentCommand extends GeneratorEntityCommand {
 
+  protected function getEntityType()
+  {
+    return 'EntityContent';
+  }
+
+  protected function getCommandName()
+  {
+    return 'generate:entity:content';
+  }
+
   protected function configure()
   {
-    parent::configure('EntityContent', 'generate:entity:content');
+    parent::configure();
   }
 }
