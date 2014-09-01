f58ec00771c63050089c699ff89bda6b7b24f369
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-09-01 08:34:09 -0700

    Remove methods getEntityType, getCommandName

--- src/Command/GeneratorEntityContentCommand.php
+++ src/Command/GeneratorEntityContentCommand.php
@@ -8,16 +8,6 @@ namespace Drupal\AppConsole\Command;
 
 class GeneratorEntityContentCommand extends GeneratorEntityCommand {
 
-  protected function getEntityType()
-  {
-    return 'EntityContent';
-  }
-
-  protected function getCommandName()
-  {
-    return 'generate:entity:content';
-  }
-
   protected function configure()
   {
     parent::configure();
