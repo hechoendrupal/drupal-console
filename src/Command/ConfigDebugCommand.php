9898a47aa1c471b6fe9fbcc6879e2183641a2655
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-10-14 11:01:24 -0700

    Import InputArgument class

--- src/Command/ConfigDebugCommand.php
+++ src/Command/ConfigDebugCommand.php
@@ -6,6 +6,7 @@
 
 namespace Drupal\AppConsole\Command;
 
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
