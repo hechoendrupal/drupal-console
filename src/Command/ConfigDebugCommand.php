1c4f7b1a6f9a48c7dd97b62bdb5744f40719650f
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-10-14 10:53:23 -0700

    Add ConfigDebugCommand Class

---
+++ src/Command/ConfigDebugCommand.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\AppConsole\Command\ConfigDebugCommand.
+ */
+
+namespace Drupal\AppConsole\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ConfigDebugCommand extends ContainerAwareCommand
+{
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure()
+  {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+
+  }
+}
