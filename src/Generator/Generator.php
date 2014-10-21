1b8603ad358bd08180142303e147b05543ab40e4
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-10-21 13:03:04 -0700

    Remove insertHookTheme method

--- src/Generator/Generator.php
+++ src/Generator/Generator.php
@@ -57,20 +57,6 @@ class Generator
     return file_put_contents($target, $this->render($template, $parameters), $flag);
   }
 
-  protected function insertHookTheme($template, $target, $parameters, $flag=null)
-  {
-    if (!is_dir(dirname($target))) {
-        mkdir(dirname($target), 0777, true);
-    }
-
-    $module_content = file_get_contents($target);
-    $pattern = '$theme = [];';
-    $position = strpos($module_content, $pattern) + strlen($pattern) + 1;
-    $append = substr_replace($module_content, $this->render($template, $parameters), $position, 0);
-
-    return file_put_contents($target, $append);
-  }
-
   public function getModulePath($module_name)
   {
     if (!$this->module_path) {
