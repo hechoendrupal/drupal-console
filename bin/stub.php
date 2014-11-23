1c7c57d272f3e64d3276bf827dc206197560aa14
David Flores <dmousex@gmail.com>
2014-11-22 20:35:09 -0600

    update path to src directory

--- bin/stub.php
+++ bin/stub.php
@@ -1,19 +1,21 @@
 <?php
 
-function autoload($className) {
-    $className = ltrim($className, '\\');
-      $fileName  = '';
-      $namespace = '';
-        if ($lastNsPos = strrpos($className, '\\')) {
-              $namespace = substr($className, 0, $lastNsPos);
-                  $className = substr($className, $lastNsPos + 1);
-                  $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-                    }
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-        require 'phar://' . __FILE__ . DIRECTORY_SEPARATOR .$fileName;
+function consoleAutoload($className) {
+  $className = ltrim($className, '\\');
+  $fileName  = '';
+  $namespace = '';
+
+  if ($lastNsPos = strrpos($className, '\\')) {
+    $namespace = substr($className, 0, $lastNsPos);
+    $className = substr($className, $lastNsPos + 1);
+    $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+  }
+
+  $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+  require 'phar://' . __FILE__ . DIRECTORY_SEPARATOR .$fileName;
 }
 
-spl_autoload_register('autoload');
+spl_autoload_register('consoleAutoload');
 
 $class_loader = require __DIR__ . '/core/vendor/autoload.php';
 require 'phar://' . __FILE__ . '/console.php';
