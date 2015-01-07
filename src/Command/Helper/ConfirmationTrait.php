cada435b5633b208951eb036d8dc6e9600bfc7f3
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-01-06 22:30:52 -0800

    Add ConfirmationTrait

---
+++ src/Command/Helper/ConfirmationTrait.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @file
+ * Containt Drupal\AppConsole\Command\Helper\Confirmation.
+ */
+
+namespace Drupal\AppConsole\Command\Helper;
+
+use Symfony\Component\Console\Helper\HelperInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+
+trait ConfirmationTrait
+{
+  /**
+   * @param InputInterface  $input
+   * @param OutputInterface $output
+   * @param HelperInterface $dialog
+   * @return mixed
+   */
+  public function confirmationQuestion(InputInterface $input, OutputInterface $output, HelperInterface $dialog)
+  {
+    if ($input->isInteractive()) {
+      if (!$dialog->askConfirmation($output, $dialog->getQuestion($this->trans('common.questions.confirm'), 'yes', '?'), true)) {
+        $output->writeln('<error>'.$this->trans('common.messages.canceled').'</error>');
+        return true;
+      }
+      return false;
+    }
+    return false;
+  }
+}
