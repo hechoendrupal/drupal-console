159713492fa56a631083dce8478eb68981556813
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-01-15 10:31:28 -0800

    remove unused class

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -9,7 +9,6 @@ namespace Drupal\AppConsole\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Herrera\Phar\Update\Manager;
 use Herrera\Phar\Update\Manifest;
