7dfea2ed22960e2a0cfea407eab249948632e97a
Daniel Noyola <danielnv18@gmail.com>
2015-01-28 21:20:02 -0600

    Added InputOption class to the commands module:install and module:uninstall.

--- src/Command/ModuleInstallCommand.php
+++ src/Command/ModuleInstallCommand.php
@@ -7,6 +7,7 @@
 namespace Drupal\AppConsole\Command;
 
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
