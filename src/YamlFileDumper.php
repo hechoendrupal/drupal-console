2b4de56b44ca6700d534afb5074c8f1a32be8721
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-02-04 22:10:56 -0800

    Relocate TranslatorHelper class

--- src/YamlFileDumper.php
+++ src/YamlFileDumper.php
@@ -7,8 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Drupal\AppConsole\Utils;
-
+namespace Drupal\AppConsole;
 
 use Symfony\Component\Translation\MessageCatalogue;
 use Symfony\Component\Yaml\Yaml;
