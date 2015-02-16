7ab17a8a6b8e193533ceae8857e587f0944fb819
dmouse <dmousex@gmail.com>
2015-02-16 13:46:25 -0600

    Update docblock and set the psr-1/2 format code

--- src/Command/Helper/KernelHelper.php
+++ src/Command/Helper/KernelHelper.php
@@ -6,6 +6,7 @@
 
 namespace Drupal\AppConsole\Command\Helper;
 
+use Composer\Autoload\ClassLoader;
 use Symfony\Component\Console\Helper\Helper;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,126 +14,131 @@ use Drupal\Core\DrupalKernel;
 
 class KernelHelper extends Helper
 {
-  protected $class_loader;
+    /**
+     * @var \Composer\Autoload\ClassLoader
+     */
+    protected $class_loader;
 
-  /**
-   * @var \Drupal\Core\DrupalKernel
-   */
-  protected $kernel;
+    /**
+     * @var \Drupal\Core\DrupalKernel
+     */
+    protected $kernel;
 
-  /**
-   * @var \Symfony\Component\HttpFoundation\Request
-   */
-  protected $request;
+    /**
+     * @var \Symfony\Component\HttpFoundation\Request
+     */
+    protected $request;
 
-  /**
-   * @var string
-   */
-  protected $environment;
+    /**
+     * @var string
+     */
+    protected $environment;
 
-  /**
-   * @var boolean
-   */
-  protected $debug;
+    /**
+     * @var boolean
+     */
+    protected $debug;
 
-  public function setClassLoader($class_loader)
-  {
-    $this->class_loader = $class_loader;
-  }
-
-  /**
-   * @param \Drupal\Core\DrupalKernel $kernel
-   */
-  public function setKernel(DrupalKernel $kernel)
-  {
-    $this->kernel = $kernel;
-  }
-
-  /**
-   * @return \Drupal\Core\DrupalKernel
-   */
-  public function getKernel()
-  {
-    if (!$this->kernel) {
-      $this->request = Request::createFromGlobals();
-      $this->kernel = DrupalKernel::createFromRequest(
-        $this->request,
-        $this->class_loader,
-        'dev'
-      );
+    public function setClassLoader(ClassLoader $class_loader)
+    {
+        $this->class_loader = $class_loader;
     }
 
-    return $this->kernel;
-  }
+    /**
+     * @param \Drupal\Core\DrupalKernel $kernel
+     */
+    public function setKernel(DrupalKernel $kernel)
+    {
+        $this->kernel = $kernel;
+    }
 
-  /**
-   * @param string $environment
-   */
-  public function setEnvironment($environment)
-  {
-    $this->environment = $environment;
-  }
+    /**
+     * @return \Drupal\Core\DrupalKernel
+     */
+    public function getKernel()
+    {
+        if (!$this->kernel) {
+            $this->request = Request::createFromGlobals();
+            $this->kernel = DrupalKernel::createFromRequest(
+              $this->request,
+              $this->class_loader,
+              $this->environment
+            );
+        }
 
-  /**
-   * @param boolean $debug
-   */
-  public function setDebug($debug)
-  {
-    $this->debug = $debug;
-  }
+        return $this->kernel;
+    }
 
-  /**
-   * @return void
-   */
-  public function bootKernel()
-  {
-    $kernel = $this->getKernel();
-    $kernel->boot();
-    $kernel->preHandle($this->request);
+    /**
+     * @param string $environment
+     */
+    public function setEnvironment($environment)
+    {
+        $this->environment = $environment;
+    }
 
-    $container = $kernel->getContainer();
-    $container->set('request', $this->request);
-    $container->get('request_stack')->push($this->request);
+    /**
+     * @param boolean $debug
+     */
+    public function setDebug($debug)
+    {
+        $this->debug = $debug;
+    }
 
-  }
+    /**
+     * @return void
+     */
+    public function bootKernel()
+    {
+        $kernel = $this->getKernel();
+        $kernel->boot();
+        $kernel->preHandle($this->request);
 
-  /**
-   * @param array $commands
-   */
-  public function initCommands(array $commands)
-  {
-    $container = $this->getKernel()->getContainer();
-    array_walk($commands, function ($command) use ($container) {
-      if ($command instanceof ContainerAwareInterface) {
-        $command->setContainer($container);
-      }
-    });
-  }
+        $container = $kernel->getContainer();
+        $container->set('request', $this->request);
+        $container->get('request_stack')->push($this->request);
+    }
 
-  /**
-   * @return \Symfony\Component\EventDispatcher\EventDispatcherInterface
-   */
-  public function getEventDispatcher()
-  {
-    return $this->getKernel()->getContainer()->get('event_dispatcher');
-  }
+    /**
+     * @param array $commands
+     */
+    public function initCommands(array $commands)
+    {
+        $container = $this->getKernel()->getContainer();
+        array_walk($commands, function ($command) use ($container) {
+            if ($command instanceof ContainerAwareInterface) {
+              $command->setContainer($container);
+            }
+        });
+    }
 
-  /**
-   * @see \Symfony\Component\Console\Helper\HelperInterface::getName()
-   */
-  public function getName()
-  {
-      return 'kernel';
-  }
+    /**
+     * @return \Symfony\Component\EventDispatcher\EventDispatcherInterface
+     */
+    public function getEventDispatcher()
+    {
+        return $this->getKernel()->getContainer()->get('event_dispatcher');
+    }
 
-  public function getClassLoader(){
-    return $this->class_loader;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'kernel';
+    }
 
-  /**
-   * @return \Symfony\Component\HttpFoundation\Request
-   */
-  public function getRequest(){
-    return $this->request;
-  }
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
+    public function getClassLoader(){
+        return $this->class_loader;
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Request
+     */
+    public function getRequest(){
+        return $this->request;
+    }
 }
