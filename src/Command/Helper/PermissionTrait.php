c7c48f0bcd32bf8a474bd630be0e7b094fadc9cf
Eric Sod <esod124@gmail.com>
2015-02-17 14:37:09 -0500

    Adds permissions generator

---
+++ src/Command/Helper/PermissionTrait.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\AppConsole\Command\Helper\PermissionsTrait.
+ */
+
+namespace Drupal\AppConsole\Command\Helper;
+
+use Symfony\Component\Console\Helper\HelperInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+trait PermissionTrait
+{
+  /**
+   * @param OutputInterface $output
+   * @param HelperInterface $dialog
+   * @return mixed
+   */
+  public function permissionQuestion(OutputInterface $output, HelperInterface $dialog)
+  {
+    if ($dialog->askConfirmation(
+      $output,
+      $dialog->getQuestion($this->trans('commands.common.questions.inputs.permission'), 'yes', '?'),
+      true
+    )) {
+
+      $permissions = [];
+      while (true) {
+        $permission = $dialog->ask(
+          $output,
+          $dialog->getQuestion($this->trans('commands.generate.permission.options.permission'), 'Access Content'),
+          null
+        );
+
+        if (empty($permission)) {
+          break;
+        }
+        $permission = $this->getStringUtils()->camelCaseToLowerCase($permission);
+        $permission_uc = $this->getStringUtils()->camelCaseToUpperCaseWords($permission);
+
+
+        array_push($permissions, array(
+          'permission' => $permission,
+          'permission_uc' => $permission_uc,
+        ));
+      }
+
+      return $permissions;
+    }
+    return null;
+  }
+}
