2a0a9a6dc5fc7ad12935ab4371639451ace545c8
Eric Sod <esod124@gmail.com>
2015-02-17 15:16:20 -0500

    refactors new code for PSR2 compliance

--- src/Command/Helper/PermissionTrait.php
+++ src/Command/Helper/PermissionTrait.php
@@ -11,42 +11,42 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 trait PermissionTrait
 {
-  /**
-   * @param OutputInterface $output
-   * @param HelperInterface $dialog
-   * @return mixed
-   */
-  public function permissionQuestion(OutputInterface $output, HelperInterface $dialog)
-  {
-    if ($dialog->askConfirmation(
-      $output,
-      $dialog->getQuestion($this->trans('commands.common.questions.inputs.permission'), 'yes', '?'),
-      true
-    )) {
-
-      $permissions = [];
-      while (true) {
-        $permission = $dialog->ask(
+    /**
+     * @param OutputInterface $output
+     * @param HelperInterface $dialog
+     * @return mixed
+     */
+    public function permissionQuestion(OutputInterface $output, HelperInterface $dialog)
+    {
+        if ($dialog->askConfirmation(
           $output,
-          $dialog->getQuestion($this->trans('commands.generate.permission.options.permission'), 'Access Content'),
-          null
-        );
+          $dialog->getQuestion($this->trans('commands.common.questions.inputs.permission'), 'yes', '?'),
+          true
+        )
+        ) {
+            $permissions = [];
+            while (true) {
+                $permission = $dialog->ask(
+                  $output,
+                  $dialog->getQuestion($this->trans('commands.generate.permission.options.permission'),
+                    'Access Content'),
+                  null
+                );
 
-        if (empty($permission)) {
-          break;
+                if (empty($permission)) {
+                    break;
+                }
+                $permission = $this->getStringUtils()->camelCaseToLowerCase($permission);
+                $permission_uc = $this->getStringUtils()->camelCaseToUpperCaseWords($permission);
+
+                array_push($permissions, array(
+                  'permission' => $permission,
+                  'permission_uc' => $permission_uc,
+                ));
+            }
+
+            return $permissions;
         }
-        $permission = $this->getStringUtils()->camelCaseToLowerCase($permission);
-        $permission_uc = $this->getStringUtils()->camelCaseToUpperCaseWords($permission);
-
-
-        array_push($permissions, array(
-          'permission' => $permission,
-          'permission_uc' => $permission_uc,
-        ));
-      }
-
-      return $permissions;
+        return null;
     }
-    return null;
-  }
 }
