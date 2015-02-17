3117a1cdbdf9247edffad507f462676499cf6335
Eric Sod <esod124@gmail.com>
2015-02-17 15:16:20 -0500

    refactors new code for PSR2 compliance

--- src/Generator/PermissionGenerator.php
+++ src/Generator/PermissionGenerator.php
@@ -7,32 +7,23 @@ namespace Drupal\AppConsole\Generator;
 
 class PermissionGenerator extends Generator
 {
-  /**
-   * @param  $module
-   * @param  $permission
-   * @param  $permission_uc
-   */
-  public function generate($module, $permission, $permission_uc)
-  {
-    $parameters = array(
-      'module_name' => $module,
-      'permissions' => $permission,
-      'permission_uc' => $permission_uc,
-    );
+    /**
+     * @param  $module
+     * @param  $permission
+     * @param  $permission_uc
+     */
+    public function generate($module, $permission, $permission_uc)
+    {
+        $parameters = array(
+          'module_name' => $module,
+          'permissions' => $permission,
+          'permission_uc' => $permission_uc,
+        );
 
-    $this->renderFile(
-      'module/permission.yml.twig',
-      $this->getModulePath($module).'/'.$module.'.permissions.yml',
-      $parameters
-    );
-
-//    if ($update_routing) {
-//      $this->renderFile(
-//        'module/permission.yml.twig',
-//        $this->getModulePath($module).'/'.$module.'.permissions.yml',
-//        $parameters,
-//        FILE_APPEND
-//      );
-//    }
-  }
+        $this->renderFile(
+          'module/permission.yml.twig',
+          $this->getModulePath($module) . '/' . $module . '.permissions.yml',
+          $parameters
+        );
+    }
 }
