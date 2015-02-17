0f66fea0ed4e969b31e0259acefb916e0bfd3ffa
David Flores <dmousex@gmail.com>
2015-02-17 10:37:12 -0600

    update test application

--- Test/Console/ApplicationTest.php
+++ Test/Console/ApplicationTest.php
@@ -9,79 +9,70 @@ use Drupal\AppConsole\Console\Application;
 class ApplicationTest extends \PHPUnit_Framework_TestCase
 {
 
-  /**
-   * @var \Symfony\Component\Console\Helper\HelperSet
-   */
-  protected $helperSet;
+    /**
+     * @var \Symfony\Component\Console\Helper\HelperSet
+     */
+    protected $helperSet;
 
-  /**
-   * @var \Drupal\AppConsole\Command\Helper\BootstrapFinderHelper
-   */
-  protected $bootstrapFinder;
+    /**
+     * @var \Drupal\AppConsole\Command\Helper\BootstrapFinderHelper
+     */
+    protected $bootstrapFinder;
 
-  /**
-   * @var \Drupal\AppConsole\Command\Helper\RegisterCommandsHelper
-   */
-  protected $register_commands;
+    /**
+     * @var \Drupal\AppConsole\Command\Helper\RegisterCommandsHelper
+     */
+    protected $register_commands;
 
-  protected function setUp()
-  {
-    $this->helperSet = $this
-        ->getMockBuilder('Symfony\Component\Console\Helper\HelperSet')
-        ->getMock();
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->helperSet = $this
+            ->getMockBuilder('Symfony\Component\Console\Helper\HelperSet')
+            ->getMock();
+
+        $this->drupalAutoload = $this
+            ->getMockBuilder('Drupal\AppConsole\Command\Helper\DrupalAutoloadHelper')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->register_commands = $this
+            ->getMockBuilder('Drupal\AppConsole\Command\Helper\RegisterCommandsHelper')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testCanRunApplication()
+    {
+        $this->expectsThatAutoloadFinderHelperIsRegistered();
+
+        $config = [
+          'application' => [
+            'name' => 'Drupal Console',
+            'version' => '0.x.x',
+            'environment' => 'prod'
+          ]
+        ];
     
-    $this->bootstrapFinder = $this
-        ->getMockBuilder('Drupal\AppConsole\Command\Helper\BootstrapFinderHelper')
-        ->disableOriginalConstructor()
-        ->getMock();
-    
-    $this->register_commands = $this
-        ->getMockBuilder('Drupal\AppConsole\Command\Helper\RegisterCommandsHelper')
-        ->disableOriginalConstructor()
-        ->getMock();
-  }
+        $application = new Application($config);
+        $application->setAutoExit(false);
+        $application->setHelperSet($this->helperSet);
 
-  public function testCanRunApplication()
-  {
-    $this->expectsThatAutoloadFinderHelperIsRegistered();
-    $this->expectsThatRegisterCommandsIsCalled();
+        $this->assertEquals(0, $application->run(new ArrayInput([]), new NullOutput()));
+    }
 
-    $config = [
-      'application' => [
-        'name' => 'Drupal Console',
-        'version' => '0.x.x',
-        'environment' => 'prod'
-      ]
-    ];
-    
-    $application = new Application($config);
-    $application->setAutoExit(false);
-    $application->setHelperSet($this->helperSet);
-
-    $this->assertEquals(0, $application->run(new ArrayInput([]), new NullOutput()));
-  }
-
-  protected function expectsThatAutoloadFinderHelperIsRegistered()
-  {
-    $this->bootstrapFinder->expects($this->any())
-                    ->method('findBootstrapFile')
+    protected function expectsThatAutoloadFinderHelperIsRegistered()
+    {
+        $this->drupalAutoload->expects($this->any())
+                    ->method('findAutoload')
                     ->will($this->returnValue(false));
     
-    $this->helperSet->expects($this->at(1))
+        $this->helperSet->expects($this->at(1))
                     ->method('get')
-                    ->with('finder')
-                    ->will($this->returnValue($this->bootstrapFinder));
-  }
+                    ->with('drupal-autoload')
+                    ->will($this->returnValue($this->drupalAutoload));
+    }
 
-  protected function expectsThatRegisterCommandsIsCalled()
-  {  
-    $this->register_commands->expects($this->once())
-      ->method('register')
-      ->will($this->returnValue(true));
-      
-    $this->helperSet->expects($this->at(2))
-      ->method('get')
-      ->with('register_commands')
-      ->will($this->returnValue($this->register_commands));
-  }
 }
