014151c84fab85b4d326ede2dc158ae05ff9fa2c
dmouse <dmousex@gmail.com>
2015-02-19 00:37:22 -0600

    remove the bootstrap functions

--- src/Command/Helper/DrupalBootstrapHelper.php
+++ src/Command/Helper/DrupalBootstrapHelper.php
@@ -16,30 +16,11 @@ class DrupalBootstrapHelper extends Helper
      */
     private $booting = false;
 
-    /**
-     * @param string $pathToBootstrapFile
-     */
-    public function bootstrapConfiguration($pathToBootstrapFile)
-    {
-        if ($pathToBootstrapFile) {
-            require_once $pathToBootstrapFile;
-            \drupal_bootstrap(DRUPAL_BOOTSTRAP_CONFIGURATION);
-            $this->booting = true;
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public function bootstrapCode()
-    {
-        if ($this->booting) {
-            \drupal_bootstrap(DRUPAL_BOOTSTRAP_CODE);
-        }
-    }
     public function getDrupalRoot()
     {
         return $this->booting ? DRUPAL_ROOT : getcwd();
     }
+
     /**
      * @see \Symfony\Component\Console\Helper\HelperInterface::getName()
      */
