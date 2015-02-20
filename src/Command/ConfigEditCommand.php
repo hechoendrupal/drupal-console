16de8a73af97ee9752f7cde88641c14d9cb2c907
Omar Aguirre <omersguchigu@gmail.com>
2015-02-19 19:15:09 -0600

    calling editor (needs refactor)

--- src/Command/ConfigEditCommand.php
+++ src/Command/ConfigEditCommand.php
@@ -9,11 +9,15 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Drupal\Component\Serialization\Yaml;
 use Symfony\Component\Process\Process;
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Process\ProcessBuilder;
 
 class ConfigEditCommand extends ContainerAwareCommand
 {
+
   protected function configure()
   {
     $this
@@ -25,32 +29,70 @@ class ConfigEditCommand extends ContainerAwareCommand
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
+    $configName = $input->getArgument('config-name');
+    $config = $this->getConfigFactory()->getEditable($configName);
+    $path = '/tmp/console/config/file/';
+    $configFile = $path.$configName.'.yml';
+    $yaml = new Parser();
+    $fs = new Filesystem();
 
-    $process = new Process('vim');
+    try {
+      $fs->mkdir($path);
+      $fs->dumpFile($configFile, $this->getYamlConfig($configName) );
+    } catch (IOExceptionInterface $e) {
+      echo "An error occurred while creating your directory at ".$e->getPath();
+    }
+    $editor = $this->getEditor();
+    $processBuilder = new ProcessBuilder(array($editor, $configFile));
+    $process = $processBuilder->getProcess();
     $process->setTty('true');
+
     $process->run();
+
     if (!$process->isSuccessful()) {
       throw new \RuntimeException($process->getErrorOutput());
     }
-    echo $process->getOutput();
+    if($process->isSuccessful()){
+
+      $value = $yaml->parse(file_get_contents($configFile));
+      $config->setData($value);
+
+      $config->save();
+    }
+
+    //echo $process->getOutput();
   }
 
   /**
-   * @param $output         OutputInterface
-   * @param $table          TableHelper
    * @param $config_name    String
    */
-  private function getConfigurationByName($output, $table, $config_name)
+  protected function getYamlConfig($config_name)
   {
     $configStorage = $this->getConfigStorage();
     if ($configStorage->exists($config_name)) {
-      $table->setHeaders([$config_name]);
-
       $configuration = $configStorage->read($config_name);
       $configurationEncoded = Yaml::encode($configuration);
-
-      $table->addRow([$configurationEncoded]);
     }
-    $table->render($output);
+    return $configurationEncoded;
+  }
+
+  protected function getEditor(){
+    $vim = new Process('which vim');
+    $vim->run();
+    if ($vim->getOutput() != ''){
+      return 'vim';
+    }
+
+    $nano = new Process('which nano');
+    $nano->run();
+    if ($nano->getOutput() != ''){
+      return 'nano';
+    }
+
+    $pico = new Process('which pico');
+    $pico->run();
+    if ($pico->getOutput() != ''){
+      return 'pico';
+    }
   }
 }
