37f2d08f5a504644bb9d723dcf92371dd0102a43
Omar Aguirre Tenorio <omersguchigu@gmail.com>
2015-02-20 01:20:07 -0600

    formating code to PSR2

--- src/Command/ConfigEditCommand.php
+++ src/Command/ConfigEditCommand.php
@@ -18,81 +18,81 @@ use Symfony\Component\Process\ProcessBuilder;
 class ConfigEditCommand extends ContainerAwareCommand
 {
 
-  protected function configure()
-  {
-    $this
-      ->setName('config:edit')
-      ->setDescription($this->trans('commands.config.edit.description'))
-      ->addArgument('config-name', InputArgument::REQUIRED, $this->trans('commands.config.edit.arguments.config-name'))
-    ;
-  }
-
-  protected function execute(InputInterface $input, OutputInterface $output)
-  {
-    $configName = $input->getArgument('config-name');
-    $config = $this->getConfigFactory()->getEditable($configName);
-    $path = '/tmp/console/config/file/';
-    $configFile = $path.$configName.'.yml';
-    $yaml = new Parser();
-    $fs = new Filesystem();
-
-    try {
-      $fs->mkdir($path);
-      $fs->dumpFile($configFile, $this->getYamlConfig($configName) );
-    } catch (IOExceptionInterface $e) {
-      echo "An error occurred while creating your directory at ".$e->getPath();
-    }
-    $editor = $this->getEditor();
-    $processBuilder = new ProcessBuilder(array($editor, $configFile));
-    $process = $processBuilder->getProcess();
-    $process->setTty('true');
-
-    $process->run();
-
-    if (!$process->isSuccessful()) {
-      throw new \RuntimeException($process->getErrorOutput());
-    }
-    if($process->isSuccessful()){
-
-      $value = $yaml->parse(file_get_contents($configFile));
-      $config->setData($value);
-
-      $config->save();
+    protected function configure()
+    {
+        $this
+          ->setName('config:edit')
+          ->setDescription($this->trans('commands.config.edit.description'))
+          ->addArgument('config-name', InputArgument::REQUIRED,
+            $this->trans('commands.config.edit.arguments.config-name'));
     }
 
-    //echo $process->getOutput();
-  }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $configName = $input->getArgument('config-name');
+        $config = $this->getConfigFactory()->getEditable($configName);
+        $path = '/tmp/console/config/file/';
+        $configFile = $path . $configName . '.yml';
+        $yaml = new Parser();
+        $fs = new Filesystem();
 
-  /**
-   * @param $config_name    String
-   */
-  protected function getYamlConfig($config_name)
-  {
-    $configStorage = $this->getConfigStorage();
-    if ($configStorage->exists($config_name)) {
-      $configuration = $configStorage->read($config_name);
-      $configurationEncoded = Yaml::encode($configuration);
-    }
-    return $configurationEncoded;
-  }
+        try {
+            $fs->mkdir($path);
+            $fs->dumpFile($configFile, $this->getYamlConfig($configName));
+        } catch (IOExceptionInterface $e) {
+            echo "An error occurred while creating your directory at " . $e->getPath();
+        }
+        $editor = $this->getEditor();
+        $processBuilder = new ProcessBuilder(array($editor, $configFile));
+        $process = $processBuilder->getProcess();
+        $process->setTty('true');
+        $process->run();
 
-  protected function getEditor(){
-    $vim = new Process('which vim');
-    $vim->run();
-    if ($vim->getOutput() != ''){
-      return 'vim';
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException($process->getErrorOutput());
+        }
+        if ($process->isSuccessful()) {
+            $value = $yaml->parse(file_get_contents($configFile));
+            $config->setData($value);
+
+            $config->save();
+        }
+
+        echo $process->getOutput();
     }
 
-    $nano = new Process('which nano');
-    $nano->run();
-    if ($nano->getOutput() != ''){
-      return 'nano';
+    /**
+     * @param $config_name    String
+     */
+    protected function getYamlConfig($config_name)
+    {
+        $configStorage = $this->getConfigStorage();
+        if ($configStorage->exists($config_name)) {
+            $configuration = $configStorage->read($config_name);
+            $configurationEncoded = Yaml::encode($configuration);
+        }
+
+        return $configurationEncoded;
     }
 
-    $pico = new Process('which pico');
-    $pico->run();
-    if ($pico->getOutput() != ''){
-      return 'pico';
+    protected function getEditor()
+    {
+        $vim = new Process('which vim');
+        $vim->run();
+        if ($vim->getOutput() != '') {
+            return 'vim';
+        }
+
+        $nano = new Process('which nano');
+        $nano->run();
+        if ($nano->getOutput() != '') {
+            return 'nano';
+        }
+
+        $pico = new Process('which pico');
+        $pico->run();
+        if ($pico->getOutput() != '') {
+            return 'pico';
+        }
     }
-  }
 }
