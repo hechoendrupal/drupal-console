bf3ca69abb6b56b19eeb1f0e16d39ac5206aa235
enzo - Eduardo Garcia <enzo@anexusit.com>
2015-02-20 19:02:06 +0000

    New command to list tests unit available

---
+++ src/Command/TestDebugCommand.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\AppConsole\Command\TestDebugCommand.
+ */
+
+namespace Drupal\AppConsole\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Drupal\Component\Serialization\Yaml;
+use Drupal\simpletest\TestDiscovery;
+
+class TestDebugCommand extends ContainerAwareCommand
+{
+
+  protected function configure()
+  {
+    $this
+      ->setName('test:debug')
+      ->setDescription($this->trans('commands.test.debug.description'))
+      ->addArgument('test-id', InputArgument::OPTIONAL,
+            $this->trans('commands.test.debug.arguments.resource-id'))
+      ->addOption('group', '', InputOption::VALUE_OPTIONAL,
+            $this->trans('commands.test.debug.options.group'))
+    ;
+
+    $this->addDependency('simpletest');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $test_id = $input->getArgument('test-id');
+    $group = $input->getOption('group');
+
+
+    $table = $this->getHelperSet()->get('table');
+    $table->setlayout($table::LAYOUT_COMPACT);
+
+    if ($test_id) {
+        $this->getTestByID($output, $table, $test_id);
+    } else {
+        $this->getAllTests($output, $table, $group);
+    }
+  }
+
+  /**
+   * @param $output         OutputInterface
+   * @param $table          TableHelper
+   * @param $config_name    String
+   */
+  private function getTestByID($output, $table, $test_id)
+  {
+      $testing_groups = simpletest_test_get_all();
+
+      foreach ($testing_groups as $testing_group => $tests) {
+        foreach ($tests as $key => $test) {
+          break;
+        }
+      }
+
+      $configurationEncoded = Yaml::encode($test);
+      $table->addRow([$configurationEncoded]);
+      $table->render($output);
+  }
+
+  /**
+   * @param $output         OutputInterface
+   * @param $table          TableHelper
+   * @param $config_name    String
+   */
+  protected function getAllTests($output, $table, $group)
+    {
+
+      $testing_groups = simpletest_test_get_all();
+
+      $table->setHeaders(
+        [
+          $this->trans('commands.test.debug.messages.id'),
+          $this->trans('commands.test.debug.messages.group'),
+        ]);
+
+      foreach ($testing_groups as $testing_group => $tests) {
+        if(!empty($group) and $group != $testing_group) {
+          continue;
+        }
+
+        foreach ($tests as $test) {
+          $table->addRow(array($test['name'], $test['group']));
+        }
+      }
+
+      $table->render($output);
+    }
+}
