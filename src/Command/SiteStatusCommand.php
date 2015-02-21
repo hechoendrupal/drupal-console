b1211d26f6527357b80925b9642c343073e8938e
Lucas Mingarro <lucasmingarro@gmail.com>
2015-02-21 18:31:16 -0300

    issue #410 [data]   - drupal version  [ok]   - console version [ok]   - database info   [ok]   - theme info      [no]   - directories     [no]
    [output]
      - stdo   [ok]
      - json   [ok]
      - csv    [no]
    
    I think it needs a lot of more work and add some unit test.

--- src/Command/SiteStatusCommand.php
+++ src/Command/SiteStatusCommand.php
@@ -53,77 +53,119 @@ class SiteStatusCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $table = $this->getHelperSet()->get('table');
-        // Options
-        if ($input->getOption('all')){
-          $show_all = true;
-        } else {
-          $show_all = false;
-        }
-        $output_format = $input->getOption('format');
-        // Arguments
-        $fields_group = $input->getArgument('field-group');
-
-        $status = $this->getStatusData();
-
-        $table->setHeaders(
-          [
-            $this->trans('commands.site.mode.messages.configuration-key'),
-            $this->trans('commands.site.mode.messages.original'),
-            $this->trans('commands.site.mode.messages.updated')
-          ]);
-        $table->setlayout($table::LAYOUT_COMPACT);
-        $table->setRows($configurationOverrideResult);
-        $table->render($output);
-        $output->writeln($status['drupal_version']);
-
+        $status_data = $this->getStatusData($input);
+        $output->writeln($status_data);
     }
-
-    private function getStatusData($filter,$format = self::FORMAT_STANDARD_OUTPUT )
+    private function getStatusData(InputInterface $input)
     {
-        $status_data =  array (
-          'console_version' => '',
-          'drupal_version'  => '',
-          'database'        => array(
-              'driver'            => '',
-              'hostname'          => '',
-              'name'              => '',
-              'port'              => '',
-              'username'          => '',
-              'password'          => '',
-              'connection_string' => '',
-              'status'            => ''
-            ),
-          'theme'           => array(
-              'defaul'  => '',
-              'admin'   => ''
-            ),
-          'directories'     => array(
-              'root_directory' => '',
-              'site_path'      => '',
-              'theme_path'     => '',
-              'config_path'    => '',
-              'tmp_path'       => ''
-            )
-        );
-
-        $status_data['console_version'] = $this->getApplication()->getVersion();
-        $status_data['drupal_version'] = \Drupal::VERSION;
-        // database
-        $db_info = \Drupal\Core\Database\Database::getConnectionInfo();
-        $status_data['database'] = $arrayName = array(
+      $status_data =  array (
+        'console_version' => '',
+        'drupal_version'  => '',
+        'database'        => array(
+            'driver'            => '',
+            'hostname'          => '',
+            'name'              => '',
+            'port'              => '',
+            'username'          => '',
+            'password'          => '',
+            'connection_string' => '',
+            'status'            => ''
+          ),
+        'theme'           => array(
+            'defaul'  => '',
+            'admin'   => ''
+          ),
+        'directories'     => array(
+            'root'        => '',
+            'site_path'   => '',
+            'theme_path'  => '',
+            'config_path' => '',
+            'tmp_path'    => ''
+          )
+      );
+      // ==================
+      // Collect Data
+      // ==================
+      $status_data['console_version'] = $this->getApplication()->getVersion();
+      $status_data['drupal_version'] = \Drupal::VERSION;
+      // database
+      $db_info = \Drupal\Core\Database\Database::getConnectionInfo();
+      $status_data['database'] = $arrayName = array(
             'driver'            => $db_info['default']['driver'],
             'hostname'          => $db_info['default']['host'],
             'name'              => $db_info['default']['database'],
             'port'              => $db_info['default']['port'],
             'username'          => $db_info['default']['username'],
-            'password'          => $db_info['default']['password']
+            'password'          => $db_info['default']['password'],
+            'connection_string' => $db_info['default']['driver'].'//'.$db_info['default']['username'].':'.$db_info['default']['password'].'@'.$db_info['default']['host'].((!empty ($db_info['default']['port'])) ? ':'.$db_info['default']['port'] : '').'/'.$db_info['default']['database']
           );
-        // $status['drupal_version'] =
-print_r($status_data);die();
-        return $status_data;
-
+      // ==================
+      // Filters
+      // ==================
+      if (!($input->getOption('all'))) {
+        $status_data = array(
+            'console_version'  => $status_data['console_version'],
+            'drupal_version'   => $status_data['drupal_version'],
+            'database'         => array('connection_string' => $status_data['database']['connection_string']),
+            'theme'            => array('defaul' => $status_data['theme']['default']),
+            'directories'      => array('root' => $status_data['directories']['root'])
+        );
+      }
+      if ( $input->getArgument('field-group') != null ) {
+        switch ($input->getArgument('field-group')) {
+          case 'version':
+            $status_data = array(
+              'console_version' => $status_data['console_version'],
+              'drupal_version'  => $status_data['drupal_version'],
+              );
+            break;
+          case 'database':
+            $status_data  = $status_data['database'];
+            break;
+          case 'theme':
+            $status_data = $status_data['theme'];
+            break;
+          case 'directories':
+            $status_data = $status_data['directories'];
+            break;
+          default:
+            # code...
+            break;
+        }
+      }
+      // ==================
+      // Output Format
+      // ==================
+      switch ($input->getOption('format')) {
+        case self::FORMAT_JSON:
+          $status_data =  json_encode($status_data);
+          break;
+        case self::FORMAT_CSV:
+          # code...
+          break;
+        default:
+          $formatter = $this->getHelper('formatter');
+          $message = '<info>Site Status</info>' . PHP_EOL;
+          foreach ($status_data as $field_group => $field_value) {
+            if (is_array($field_value)) {
+              $seccion = '';
+              $section_msg = '';
+              $section = $field_group;
+              foreach ($field_value as $field => $value) {
+                $line = PHP_EOL. $field . ': ' . $value;
+                $section_msg = $section_msg . $line;
+              }
+              $formattedLine = $formatter->formatSection($section,$section_msg);
+              $message = $message . PHP_EOL . PHP_EOL .$formattedLine ;
+            } else {
+              $line = PHP_EOL . $field_group . ': ' . $field_value;
+              $message = $message . $line;
+            }
+          }
+          $status_data = $message;
+          break;
+      }
+      return $status_data;
     }
-
 
 }
