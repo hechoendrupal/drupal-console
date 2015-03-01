744354bc018e4a853f02eaad54acfec36f62b19b
dmouse <dmousex@gmail.com>
2015-03-01 10:40:51 -0600

    update way to execute commands to generate code

--- Test/script/create-controller.sh
+++ Test/script/create-controller.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-. Test/script/setup.sh
-
-$CONSOLE_BIN generate:controller --module="travis" --class-name="TravisController" --method-name="index" --route="/index" --services="twig" --test -n
+$CONSOLE_BIN generate:controller -n \
+--module="travis" \
+--class-name="TravisController" \
+--method-name="index" \
+--route="/index" \
+--services="twig" \
+--test
--- Test/script/create-form.sh
+++ Test/script/create-form.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-. Test/script/setup.sh
-
 $CONSOLE_BIN generate:form:config \
 --module="travis" \
 --class-name="FormTravis" \
