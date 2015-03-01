94636c220abaf158a589970f6219552f1248a89e
dmouse <dmousex@gmail.com>
2015-03-01 17:58:32 -0600

    update script to generate plugin block

--- Test/script/test-generation-commands.sh
+++ Test/script/test-generation-commands.sh
@@ -12,3 +12,4 @@ SCRIPT_DIR=$( dirname "$FILE" )
 . $SCRIPT_DIR/generate-entity-content.sh
 . $SCRIPT_DIR/generate-command.sh
 . $SCRIPT_DIR/generate-authentication-provider.sh
+. $SCRIPT_DIR/generate-plugin-block.sh
