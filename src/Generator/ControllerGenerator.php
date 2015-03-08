b52da62515c77bd830e9aa9376fa9be42f6c07dc
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-03-08 14:09:40 -0700

    #497 Pass learning option to twig parameters

--- src/Generator/ControllerGenerator.php
+++ src/Generator/ControllerGenerator.php
@@ -11,14 +11,15 @@ class ControllerGenerator extends Generator
 
     public function generate($module, $class_name, $method_name, $route, $test, $services, $class_machine_name)
     {
-        $parameters = array(
+        $parameters = [
           'class_name' => $class_name,
           'services' => $services,
           'module' => $module,
           'method_name' => $method_name,
           'class_machine_name' => $class_machine_name,
           'route' => $route,
-        );
+          'learning' => $this->isLearning()
+        ];
 
         $this->renderFile(
           'module/src/Controller/controller.php.twig',
