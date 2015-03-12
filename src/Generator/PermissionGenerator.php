c99a92048681b35bb4c32c6a416e201bd302b261
Omar Aguirre <omersguchigu@gmail.com>
2015-03-11 18:09:36 -0600

    adding new params to call the generator

--- src/Generator/PermissionGenerator.php
+++ src/Generator/PermissionGenerator.php
@@ -12,18 +12,21 @@ class PermissionGenerator extends Generator
      * @param  $permission
      * @param  $permission_title
      */
-    public function generate($module, $permission, $permission_title)
+    public function generate($module, $permission, $title, $description, $restrictAccess)
     {
         $parameters = array(
           'module_name' => $module,
           'permissions' => $permission,
-          'permission_title' => $permission_title,
+          'title' => $title,
+          'description' => $description,
+          'restrict_access' => $restrictAccess
         );
 
         $this->renderFile(
           'module/permission.yml.twig',
           $this->getModulePath($module) . '/' . $module . '.permissions.yml',
-          $parameters
+          $parameters,
+          FILE_APPEND
         );
     }
 }
