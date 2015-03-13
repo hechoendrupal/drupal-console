508a0c485c23380dd043b41e7fdf9574156a4954
Omar Aguirre <omersguchigu@gmail.com>
2015-03-12 18:09:03 -0600

    remove initial confirmation

--- src/Command/Helper/PermissionTrait.php
+++ src/Command/Helper/PermissionTrait.php
@@ -14,65 +14,62 @@ trait PermissionTrait
     /**
      * @param OutputInterface $output
      * @param HelperInterface $dialog
+     *
      * @return mixed
      */
-    public function permissionQuestion(OutputInterface $output, HelperInterface $dialog)
-    {
-        if ($dialog->askConfirmation(
-          $output,
-          $dialog->getQuestion($this->trans('commands.common.questions.inputs.permission'), 'yes', '?'),
-          true
-        )
-        ) {
-            $permissions = [];
-            while (true) {
-                $permission = $dialog->ask(
-                  $output,
-                  $dialog->getQuestion($this->trans('commands.generate.permission.options.permission'),
-                    'access content'),
-                  'access content'
-                );
-                $title = $dialog->ask(
-                  $output,
-                  $dialog->getQuestion($this->trans('commands.generate.permission.options.title'),
-                    'Access Content'),
-                  'Access Content'
-                );
-                $description = $dialog->ask(
-                  $output,
-                  $dialog->getQuestion($this->trans('commands.generate.permission.options.description'),
-                    'Allow access to my content'),
-                  'Allow access to my content'
-                );
-                $restrictAccess = $dialog->ask(
-                  $output,
-                  $dialog->getQuestion($this->trans('commands.generate.permission.options.restrict-access'), 'false', '?'),
-                  'false'
-                );
-                if (!$dialog->askConfirmation(
-                  $output,
-                  $dialog->getQuestion($this->trans('commands.generate.permission.questions.add'), 'y', '?'),
-                  true
-                )) {
-                    break;
-                }
+    public function permissionQuestion(
+      OutputInterface $output,
+      HelperInterface $dialog
+    ) {
+        $permissions = [];
+        while (true) {
+            $permission = $dialog->ask(
+              $output,
+              $dialog->getQuestion($this->trans('commands.generate.permission.options.permission'),
+                'access content'),
+              'access content'
+            );
+            $title = $dialog->ask(
+              $output,
+              $dialog->getQuestion($this->trans('commands.generate.permission.options.title'),
+                'Access Content'),
+              'Access Content'
+            );
+            $description = $dialog->ask(
+              $output,
+              $dialog->getQuestion($this->trans('commands.generate.permission.options.description'),
+                'Allow access to my content'),
+              'Allow access to my content'
+            );
+            $restrictAccess = $dialog->ask(
+              $output,
+              $dialog->getQuestion($this->trans('commands.generate.permission.options.restrict-access'),
+                'false', '?'),
+              'false'
+            );
 
-                if (empty($permission)) {
+            $permission = $this->getStringUtils()
+              ->camelCaseToLowerCase($permission);
+            $title = $this->getStringUtils()->camelCaseToUcFirst($title);
 
-                }
-                $permission = $this->getStringUtils()->camelCaseToLowerCase($permission);
-                $title = $this->getStringUtils()->camelCaseToUcFirst($title);
+            array_push($permissions, array(
+              'permission' => $permission,
+              'title' => $title,
+              'description' => $description,
+              'restrict_access' => $restrictAccess,
+            ));
 
-                array_push($permissions, array(
-                  'permission' => $permission,
-                  'title' => $title,
-                  'description' => $description,
-                  'restrict_access' => $restrictAccess,
-                ));
+            if (!$dialog->askConfirmation(
+              $output,
+              $dialog->getQuestion($this->trans('commands.generate.permission.questions.add'),
+                'y', '?'),
+              true
+            )
+            ) {
+                break;
             }
-
-            return $permissions;
         }
-        return null;
+
+        return $permissions;
     }
 }
