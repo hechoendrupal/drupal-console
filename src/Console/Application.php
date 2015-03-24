ac3cd6a4a93005e67f2fbb988dbdb8f710719f29
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-03-23 23:28:23 -0700

    Rename var autoload to auto_load

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -110,7 +110,7 @@ class Application extends BaseApplication
     {
         $drupal_root = $input->getParameterOption(['--drupal', '-d'], false);
 
-        $autoload = $this
+        $auto_load = $this
           ->getHelperSet()
           ->get('drupal-autoload')
           ->findAutoload($drupal_root);
@@ -119,8 +119,8 @@ class Application extends BaseApplication
             return false;
         }
 
-        if ($autoload && !$this->isBooted()) {
-            $this->drupalAutoload = require_once $autoload;
+        if ($auto_load && !$this->isBooted()) {
+            $this->drupalAutoload = require_once $auto_load;
             if ($this->drupalAutoload instanceof ClassLoader) {
                 $this->setBooted(true);
                 return true;
