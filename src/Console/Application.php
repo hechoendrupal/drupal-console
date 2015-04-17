bd7fcbdc608e31c46a7a1733cb94da87e748413d
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-04-16 22:48:27 -0700

    Validate kernelHelper is not null

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -121,7 +121,9 @@ class Application extends BaseApplication
         parent::doRun($input, $output);
 
         $kernelHelper = $this->getHelperSet()->get('kernel');
-        $kernelHelper->terminate();
+        if ($kernelHelper) {
+            $kernelHelper->terminate();
+        }
     }
 
     /**
