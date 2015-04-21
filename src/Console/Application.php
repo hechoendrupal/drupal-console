a3b4cb13acee571aa86dab42b79dfe5017a6c0d0
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-04-21 12:49:48 -0700

    Apply PSR2 standards

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -237,7 +237,7 @@ class Application extends BaseApplication
     public function bootstrap()
     {
         $kernelHelper = $this->getHelperSet()->get('kernel');
-        if ($kernelHelper){
+        if ($kernelHelper) {
             $kernelHelper->bootKernel();
             $kernelHelper->initCommands($this->all());
         }
@@ -254,7 +254,7 @@ class Application extends BaseApplication
     {
         /** @var \Drupal\AppConsole\Command\Helper\RegisterCommandsHelper $rc */
         $registerCommands = $this->getHelperSet()->get('register_commands');
-        if ($registerCommands){
+        if ($registerCommands) {
             $registerCommands->register();
         }
     }
