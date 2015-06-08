28befd096857d25413db37e5122646a6a06628f2
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-06-08 00:32:06 -0700

    #615 Update InitCommand extend class from Command and not ContainerAwareCommand

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class InitCommand extends ContainerAwareCommand
+class InitCommand extends Command
 {
 
     private $files = [
