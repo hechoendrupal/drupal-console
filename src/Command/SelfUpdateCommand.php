d14a234c6f9c01d2ab5addb6f0f7cfe829825a44
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-06-08 00:32:34 -0700

    #615 Update SelfUpdateCommand extend class from Command and not ContainerAwareCommand

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Herrera\Phar\Update\Manager;
 use Herrera\Phar\Update\Manifest;
 
-class SelfUpdateCommand extends ContainerAwareCommand
+class SelfUpdateCommand extends Command
 {
     const DRUPAL_CONSOLE_MANIFEST = "http://drupalconsole.com/manifest.json";
 
