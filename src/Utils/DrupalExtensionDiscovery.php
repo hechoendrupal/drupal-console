24df4f36250a560bc3fa5de69d0827a7df7ec3bd
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-06-14 02:06:30 -0700

    #691 Apply PSR2 fixer

--- src/Utils/DrupalExtensionDiscovery.php
+++ src/Utils/DrupalExtensionDiscovery.php
@@ -9,13 +9,13 @@ namespace Drupal\AppConsole\Utils;
 
 use Drupal\Core\Extension\ExtensionDiscovery;
 
-class DrupalExtensionDiscovery extends ExtensionDiscovery {
-
-  /**
-   * Reset internal static cache.
-   */
-  public function reset() {
-    static::$files = array();
-  }
-
+class DrupalExtensionDiscovery extends ExtensionDiscovery
+{
+    /**
+     * Reset internal static cache.
+     */
+    public function reset()
+    {
+        static::$files = array();
+    }
 }
