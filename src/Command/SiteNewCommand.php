798e38f9090d5635c5c03c35142de8dab0cf91bb
enzo - Eduardo Garcia <enzo@anexusit.com>
2015-07-03 14:03:09 -0700

    Working on project for new command site:new

---
+++ src/Command/SiteNewCommand.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\AppConsole\Command\ModuleDownloadCommand.
+ */
+
+namespace Drupal\AppConsole\Command;
+
+use Alchemy\Zippy\Zippy;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+class SiteNewCommand extends Command
+{
+
+    protected function configure()
+    {
+        $this
+          ->setName('site:new')
+          ->setDescription($this->trans('commands.site.new.description'))
+          ->addArgument('site-name', InputArgument::REQUIRED, $this->trans('commands.site.new.arguments.site-name'))
+          ->addArgument('version', InputArgument::OPTIONAL, $this->trans('commands.site.new.arguments.version'));
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        //$client = $this->getApplication()->getKernel()->getContainer()->get('http_client');
+        $client =  new Client();
+        $site_name = $input->getArgument('site-name');
+        $version = $input->getArgument('version');
+
+        if ($version) {
+            $release_selected = '8.x-' . $version;
+        } else {
+            // Getting Module page header and parse to get module Node
+          $output->writeln('[+] <info>' . sprintf($this->trans('commands.site.new.messages.getting-releases')) . '</info>');
+
+            // Page for Drupal releases filter by Drupal 8
+            $project_release_d8 = 'https://www.drupal.org/node/3060/release?api_version%5B%5D=7234';
+
+          // Parse release module page to get Drupal 8 releases
+          try {
+              $response = $client->get($project_release_d8);
+              print_r($response);
+              $html = $response->getBody()->__tostring();
+          } catch (\Exception $e) {
+              $output->writeln('[+] <error>' . $e->getMessage() . '</error>');
+              return;
+          }
+
+            $crawler = new Crawler($html);
+            $releases = [];
+            foreach ($crawler->filter('span.file a') as $element) {
+                if (strpos($element->nodeValue, ".tar.gz") > 0) {
+                    $release_name = str_replace(
+                      '.tar.gz', '',
+                      str_replace(
+                        'drupal-', '', $element->nodeValue
+                      )
+                    );
+                    $releases[$release_name] = $element->nodeValue;
+                }
+            }
+
+            if (empty($releases)) {
+                $output->writeln('[+] <error>' . sprintf($this->trans('commands.module.download.messages.no-releases'),
+                implode(',', array($module))) . '</error>');
+                return;
+            }
+
+          // List module releases to enable user to select his favorite release
+          $questionHelper = $this->getQuestionHelper();
+
+            $question = new ChoiceQuestion(
+            'Please select your favorite release',
+            array_keys($releases),
+            0
+          );
+
+            $release_selected = $questionHelper->ask($input, $output, $question);
+
+          // Start the process to download the zip file of release and copy in contrib folter
+          $output->writeln(
+            '[+] <info>' .
+            sprintf(
+              $this->trans('commands.site.new.messages.downloading'),
+              $release_selected
+            ) .
+            '</info>'
+          );
+        }
+
+        $release_file_path = 'http://ftp.drupal.org/files/projects/drupal-' . $release_selected . '.tar.gz';
+
+        // Destination file to download the release
+        $destination = tempnam(sys_get_temp_dir(), 'drupal.') . "tar.gz";
+
+        try {
+            $client->get($release_file_path, ['save_to' => $destination]);
+
+            // Prepare release to unzip and untar
+            $zippy = Zippy::load();
+            $archive = $zippy->open($destination);
+            $archive->extract('./');
+
+            try {
+              $fs = new Filesystem();
+              $fs->rename('./drupal-' . $release_selected, './' . $site_name);
+            } catch (IOExceptionInterface $e) {
+              $output->writeln('[+] <error>'. "An error occurred while renaming your directory at " . $e->getPath() . '</error>');
+            }
+
+            $output->writeln('[+] <info>' . sprintf($this->trans('commands.module.download.messages.downloaded'),
+                $module, $release_selected, $module_contrib_path) . '</info>');
+        } catch (\Exception $e) {
+            $output->writeln('[+] <error>' . $e->getMessage() . '</error>');
+            return;
+        }
+        return true;
+    }
+}
