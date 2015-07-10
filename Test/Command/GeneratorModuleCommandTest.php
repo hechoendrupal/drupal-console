99fb2b60e0b8cd77c2da6b7732c03723968dc183
enzo - Eduardo Garcia <enzo@anexusit.com>
2015-07-10 14:04:47 -0600

    Revert "Improve module generator options to controller"
    This reverts commit 8b0380628c852d27ac575bdd14c7561652f44485.

--- Test/Command/GeneratorModuleCommandTest.php
+++ Test/Command/GeneratorModuleCommandTest.php
@@ -77,12 +77,9 @@ class GeneratorModuleCommandTest extends GenerateCommandTest
               '--package' => 'Other',
               '--composer' => false,
               '--controller' => true,
-              '--controller-title' => 'Foo title',
-              '--controller-path' => '/foo/index',
-              '--controller-class-name' => 'DefaultController',
               '--test' => true
             ],
-            ["foo", "foo", $dir, "My Awesome Module", '8.x', 'Other', true, false, [], 'Foo title', '/foo/index', 'DefaultController', 'index', true],
+            ["foo", "foo", $dir, "My Awesome Module", '8.x', 'Other', true, false, [], true],
           ],
           [
             [
@@ -94,13 +91,9 @@ class GeneratorModuleCommandTest extends GenerateCommandTest
               '--package' => 'Other',
               '--composer' => false,
               '--controller' => true,
-              '--controller-title' => 'Foo title',
-              '--controller-path' => '/foo/index',
-              '--controller-class-name' => 'DefaultController',
-              '--controller-method-name' => 'index',
               '--test' => true
             ],
-            ["foo", 'foo', $dir, "My Awesome Module", '8.x', 'Other', true, true, [], 'Foo title', '/foo/index', 'DefaultController', 'index', true],
+            ["foo", 'foo', $dir, "My Awesome Module", '8.x', 'Other', true, false, [], true],
           ]
         ];
     }
