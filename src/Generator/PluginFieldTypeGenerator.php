21a7bbad43739c6e0fa817cc71abe1da6347404e
Erik Stielstra <info@erikstielstra.nl>
2015-07-14 08:35:56 +0200

    Fixes after testing.

--- src/Generator/PluginFieldTypeGenerator.php
+++ src/Generator/PluginFieldTypeGenerator.php
@@ -16,16 +16,18 @@ class PluginFieldTypeGenerator extends Generator
      * @param string $class_name         Plugin Class name
      * @param string $label              Plugin label
      * @param string $plugin_id          Plugin id
+     * @param string $description        Plugin description
      * @param string $default_widget     Default widget this field type used supports
      * @param string $default_formatter  Default formatter this field type used supports
      */
-    public function generate($module, $class_name, $label, $plugin_id, $default_widget, $default_formatter)
+    public function generate($module, $class_name, $label, $plugin_id, $description, $default_widget, $default_formatter)
     {
         $parameters = [
             'module' => $module,
             'class_name' => $class_name,
             'label' => $label,
             'plugin_id' => $plugin_id,
+            'description' => $description,
             'default_widget' => $default_widget,
             'default_formatter' => $default_formatter,
         ];
