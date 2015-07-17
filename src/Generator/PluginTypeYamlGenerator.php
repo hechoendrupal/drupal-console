9aa9fbdfed780dc960ebe8aa477df7897ccb4d1f
Erik Stielstra <info@erikstielstra.nl>
2015-07-17 21:54:06 +0200

    Initial version of yaml plugin type

---
+++ src/Generator/PluginTypeYamlGenerator.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\AppConsole\Generator\PluginTypeYamlGenerator.
+ */
+
+namespace Drupal\AppConsole\Generator;
+
+class PluginTypeYamlGenerator extends Generator
+{
+    /**
+     * Generator for Plugin type with Yaml discovery.
+     *
+     * @param  $module
+     * @param  $plugin_class
+     * @param  $plugin_name
+     * @param  $plugin_file_name
+     */
+    public function generate($module, $plugin_class, $plugin_name, $plugin_file_name)
+    {
+        $parameters = [
+            'module' => $module,
+            'class_name' => $plugin_class,
+            'plugin_name' => $plugin_name,
+            'plugin_file_name' => $plugin_file_name,
+        ];
+
+        $this->renderFile(
+            'module/src/yaml-plugin-manager.php.twig',
+            $this->getSourcePath($module) . '/' . $plugin_name . 'Manager.php',
+            $parameters
+        );
+
+        $this->renderFile(
+            'module/src/yaml-plugin-manager-interface.php.twig',
+            $this->getSourcePath($module) . '/' . $plugin_name . 'ManagerInterface.php',
+            $parameters
+        );
+    }
+}
