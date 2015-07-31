5ae9b5146d090cca30d598cd38dc4bee085d7aa7
Omar Aguirre Tenorio <omersguchigu@gmail.com>
2015-07-31 13:06:20 -0600

    fix missing var regions

--- src/Command/Helper/ThemeRegionTrait.php
+++ src/Command/Helper/ThemeRegionTrait.php
@@ -10,61 +10,61 @@ namespace Drupal\AppConsole\Command\Helper;
 use Symfony\Component\Console\Helper\HelperInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-trait ThemeRegionTrait
-{
-    /**
-     * @param OutputInterface $output
-     * @param HelperInterface $dialog
-     *
-     * @return mixed
-     */
-    public function regionQuestion(OutputInterface $output, HelperInterface $dialog)
-    {
-        $stringUtils = $this->getHelperSet()->get('stringUtils');
-        $validators = $this->getHelperSet()->get('validators');
+trait ThemeRegionTrait {
+  /**
+   * @param OutputInterface $output
+   * @param HelperInterface $dialog
+   *
+   * @return mixed
+   */
+  public function regionQuestion(OutputInterface $output, HelperInterface $dialog) {
 
-        $regions = [];
-        while (true) {
-            $region_name = $dialog->ask(
-              $output,
-              $dialog->getQuestion(
-                $this->trans('commands.generate.theme.questions.region-name'),
-                'Content'
-              ),
-              'Content'
-            );
+    $stringUtils = $this->getHelperSet()->get('stringUtils');
+    $validators = $this->getHelperSet()->get('validators');
 
-            $region_machine_name = $stringUtils->createMachineName($region_name);
-            $region_machine_name = $dialog->askAndValidate(
-              $output,
-              $dialog->getQuestion($this->trans('commands.generate.theme.questions.region-machine-name'), $region_machine_name),
-              function ($region_machine_name) use ($validators) {
-                  return $validators->validateMachineName($region_machine_name);
-              },
-              false,
-              $region_machine_name,
-              null
-            );
+    $regions = [];
+    while (TRUE) {
+      $region_name = $dialog->ask(
+          $output,
+          $dialog->getQuestion(
+            $this->trans('commands.generate.theme.questions.region-name'),
+            'Content'
+          ),
+          'Content'
+        );
 
-            array_push($permissions, array(
-              'region-name' => $region_name,
-              'region-machine-name' => $region_machine_name,
-            ));
+      $region_machine_name = $stringUtils->createMachineName($region_name);
+      $region_machine_name = $dialog->askAndValidate(
+          $output,
+          $dialog->getQuestion($this->trans('commands.generate.theme.questions.region-machine-name'), $region_machine_name),
+          function ($region_machine_name) use ($validators) {
+              return $validators->validateMachineName($region_machine_name);
+          },
+          FALSE,
+          $region_machine_name,
+          NULL
+        );
 
-            if (!$dialog->askConfirmation(
-              $output,
-              $dialog->getQuestion(
-                $this->trans('commands.generate.theme.questions.region-add'),
-                'yes',
-                '?'
-              ),
-              true
-            )
-            ) {
-                break;
-            }
-        }
+      array_push($regions, array(
+          'region_name' => $region_name,
+          'region_machine_name' => $region_machine_name,
+        ));
 
-        return $regions;
+      if (!$dialog->askConfirmation(
+          $output,
+          $dialog->getQuestion(
+            $this->trans('commands.generate.theme.questions.region-add'),
+            'yes',
+            '?'
+          ),
+          TRUE
+        )
+        ) {
+        break;
+      }
     }
+
+    return $regions;
+  }
+
 }
