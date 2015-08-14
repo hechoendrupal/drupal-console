83821920b266357362c818d11dc6a2238bade70e
enzo - Eduardo Garcia <josegarcia@appnovation.com>
2015-08-14 14:40:26 -0600

    Updated command config:import

--- src/Command/ConfigImportCommand.php
+++ src/Command/ConfigImportCommand.php
@@ -10,8 +10,10 @@ use Drupal\Core\Archiver\ArchiveTar;
 use Drupal\Core\Config\FileStorage;
 use Drupal\Component\Serialization\Yaml;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Parser;
 
 class ConfigImportCommand extends ContainerAwareCommand
 {
@@ -24,7 +26,8 @@ class ConfigImportCommand extends ContainerAwareCommand
           ->setName('config:import')
           ->setDescription($this->trans('commands.config.import.description'))
           ->addArgument('config-file', InputArgument::REQUIRED,
-            $this->trans('commands.config.import.arguments.config-file'));
+            $this->trans('commands.config.import.arguments.config-file'))
+          ->addOption('copy-only', '', InputOption::VALUE_NONE, $this->trans('commands.config.import.arguments.copy-only'));
     }
 
     /**
@@ -33,26 +36,55 @@ class ConfigImportCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $config_file = $input->getArgument('config-file');
+        $copy_only = $input->getOption('copy-only');
 
         try {
             $files = array();
             $archiver = new ArchiveTar($config_file, 'gz');
 
-            $this->showMessage($output, $this->trans('commands.config.import.messages.config_files_imported'));
+            $output->writeln($this->trans('commands.config.import.messages.config_files_imported'));
             foreach ($archiver->listContent() as $file) {
-              $files[] = $file['filename'];
-              print  $file['filename'] . "\n";
+              $pathinfo = pathinfo($file['filename']);
+              $files[$pathinfo['filename']] = $file['filename'];
+              $output->writeln('[-] <info>' .  $file['filename'] . '</info>');
             }
-            $archiver->extractList($files, config_get_config_directory(CONFIG_STAGING_DIRECTORY));
 
+            $config_staging_dir = config_get_config_directory(CONFIG_STAGING_DIRECTORY);
 
+            try {
+                $archiver->extract($config_staging_dir . '/');
+            } catch (\Exception $e) {
+                $output->writeln('[+] <error>' . $e->getMessage() . '</error>');
+                return;
+            }
 
+            if($copy_only) {
+                $output->writeln(sprintf($this->trans('commands.config.import.messages.copied'), CONFIG_STAGING_DIRECTORY));
+            }
+            else {
+                foreach($files as $cofig_name => $filename) {
+                    $config = $this->getConfigFactory()->getEditable($cofig_name);
+                    $parser = new Parser();
+                    $config_value = $parser->parse(file_get_contents($config_staging_dir . '/' . $filename));
+                    $config->setData($config_value);
+
+                    try {
+
+                        $config->save();
+                    } catch (\Exception $e) {
+                        $output->writeln('[+] <error>' . $e->getMessage() . '</error>');
+                        return;
+                    }
+                }
+
+                $output->writeln(sprintf($this->trans('commands.config.import.messages.imported'), CONFIG_STAGING_DIRECTORY));
+
+            }
         } catch (\Exception $e) {
             $output->writeln('[+] <error>' . $e->getMessage() . '</error>');
             return;
         }
 
-        $this->showMessage($output,
-            sprintf($this->trans('commands.config.import.messages.imported'), CONFIG_STAGING_DIRECTORY));
+
     }
 }
