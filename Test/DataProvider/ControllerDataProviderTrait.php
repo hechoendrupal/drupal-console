27625a04af71aaa3609476ceae1e0d6e321b7aad
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-08-16 18:36:56 -0700

    Remove Todo and unused services property

--- Test/DataProvider/ControllerDataProviderTrait.php
+++ Test/DataProvider/ControllerDataProviderTrait.php
@@ -15,22 +15,11 @@ trait ControllerDataProviderTrait
     {
         $this->setUpTemporalDirectory();
 
-        $services = [
-          'twig' => [
-            'name' => 'twig',
-            'machine_name' => 'twig',
-            'class' => '\Twig_Environment',
-            'short' => 'Twig_Environment',
-          ]
-        ];
-
         $routes = [
           ['title' => 'Foo Controller', 'method' => 'index', 'route' => 'index']
         ];
 
         return [
-          // ToDo: send sercives, issues with Twig container get
-          //['foo', 'FooController', $routes, true, $services, 'foo_controller'],
           ['foo', 'FooController', $routes, true, null, 'foo_controller'],
           ['foo', 'FooController', $routes, false, null, 'foo_controller'],
 
