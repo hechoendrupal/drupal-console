ade473173e64e7e96436f0895ea1fdea7c56b8af
Jorge Atempa <atempaitm@gmail.com>
2015-09-05 00:16:38 -0700

    It was created the test for 'generate:service' command, using basic parameters

--- Test/Command/GeneratorServiceCommandTest.php
+++ Test/Command/GeneratorServiceCommandTest.php
@@ -6,101 +6,51 @@
 
 namespace Drupal\AppConsole\Test\Command;
 
+use Drupal\AppConsole\Command\GeneratorServiceCommand;
 use Symfony\Component\Console\Tester\CommandTester;
+use Drupal\AppConsole\Test\DataProvider\ServiceDataProviderTrait;
 
 class GeneratorServiceCommandTest extends GenerateCommandTest
 {
+    use ServiceDataProviderTrait;
+
     /**
-     * @dataProvider getInteractiveData
+     * Service generator test
+     *
+     * @param $module
+     * @param $service_name
+     * @param $class_name
+     * @param $interface
+     * @param $services
+     *
+     * @dataProvider commandData
      */
-    public function testInteractive($options, $expected, $input)
-    {
-        list($module, $service_name, $class_name, $interface, $services) = $expected;
-
-        $generator = $this->getGenerator();
-        $generator
-            ->expects($this->once())
-            ->method('generate')
-            ->with($module, $service_name, $class_name, $interface, $services);
-
-        $command = $this->getCommand($generator, $input);
-        $cmd = new CommandTester($command);
-        $cmd->execute($options);
-    }
-
-    public function getInteractiveData()
-    {
-        $services = [
-          'twig' => [
-            'name' => 'twig',
-            'machine_name' => 'twig',
-            'class' => 'Twig_Environment',
-            'short' => 'Twig_Environment',
-          ]
-        ];
-
-        return [
-            // case one
-          [
-              // Inline options
-            [],
-              // Expected options
-            ['foo', 'foo.default', 'DefaultService', false, $services],
-              // User input options
-            "foo\nfoo.default\nDefaultService\nno\nyes\ntwig\n\n",
-          ],
-            // case two
-          [
-              // Inline options
-            ['--module' => 'foo'],
-              // Expected options
-            ['foo', 'foo.default', 'DefaultService', true, null],
-              // User input options
-            "foo.default\nDefaultService\nyes\nno\n",
-          ],
-            // case three
-          [
-              // Inline options
-            ['--module' => 'foo', '--service-name' => 'foo.default'],
-              // Expected options
-            ['foo', 'foo.default', 'DefaultService', false, null],
-              // User input options
-            "DefaultService\nno\nno\n",
-          ],
-            // case three
-          [
-              // Inline options
-            ['--module' => 'foo', '--service-name' => 'foo.default', '--class-name' => 'DefaultService'],
-              // Expected options
-            ['foo', 'foo.default', 'DefaultService', true, $services],
-              // User input options
-            "yes\nyes\ntwig\n\n",
-          ],
-        ];
-    }
-
-    protected function getCommand($generator, $input)
-    {
-        $command = $this
-            ->getMockBuilder('Drupal\AppConsole\Command\GeneratorServiceCommand')
-            ->setMethods(['getModules', 'getServices', '__construct'])
-            ->setConstructorArgs([$this->getTranslatorHelper()])
-            ->getMock();
-
-        $command->expects($this->any())
-            ->method('getModules')
-            ->will($this->returnValue(['foo']));
-        ;
-
-        $command->expects($this->any())
-            ->method('getServices')
-            ->will($this->returnValue(['twig', 'database']));
-
+    public function testGenerateService(
+        $module,
+        $service_name,
+        $class_name,
+        $interface,
+        $services
+    ) {
+        $command = new GeneratorServiceCommand($this->getTranslatorHelper());
         $command->setContainer($this->getContainer());
-        $command->setHelperSet($this->getHelperSet($input));
-        $command->setGenerator($generator);
+        $command->setHelperSet($this->getHelperSet());
+        $command->setGenerator($this->getGenerator());
 
-        return $command;
+        $commandTester = new CommandTester($command);
+
+        $code = $commandTester->execute(
+            [
+              '--module'         => $module,
+              '--service-name'   => $service_name,
+              '--class-name'     => $class_name,
+              '--interface'      => $interface,
+              '--services'       => $services,
+            ],
+            ['interactive' => false]
+        );
+
+        $this->assertEquals(0, $code);
     }
 
     private function getGenerator()
