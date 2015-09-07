2cc320154a7951d9f6b2106c8af72db0ee5c075a
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-09-07 14:27:38 -0700

    Apply phpqa using PSR2 standards

--- src/Generator/PluginConditionGenerator.php
+++ src/Generator/PluginConditionGenerator.php
@@ -12,13 +12,13 @@ class PluginConditionGenerator extends Generator
     /**
      * Generator Plugin Field Formatter.
      *
-     * @param string $module     Module name
-     * @param string $class_name Plugin condition Class name
-     * @param string $label      Plugin condition label
-     * @param string $plugin_id  Plugin condition id
-     * @param string $context_definition_id Plugin condition context definition id
-     * @param string $context_definition_label Plugin condition context definition label
-     * @param bool $context_definition_required Plugin condition context definition required
+     * @param string $module                      Module name
+     * @param string $class_name                  Plugin condition Class name
+     * @param string $label                       Plugin condition label
+     * @param string $plugin_id                   Plugin condition id
+     * @param string $context_definition_id       Plugin condition context definition id
+     * @param string $context_definition_label    Plugin condition context definition label
+     * @param bool   $context_definition_required Plugin condition context definition required
      */
     public function generate($module, $class_name, $label, $plugin_id, $context_definition_id, $context_definition_label, $context_definition_required)
     {
