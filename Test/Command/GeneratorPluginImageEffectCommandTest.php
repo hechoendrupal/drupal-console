1074d7b9226818a712df928b58a4759abf50aae4
Jorge Atempa <atempaitm@gmail.com>
2015-09-07 18:43:08 -0700

    It was created the test for 'generate:plugin:imageeffect' command, using basic parameters

--- Test/Command/GeneratorPluginImageEffectCommandTest.php
+++ Test/Command/GeneratorPluginImageEffectCommandTest.php
@@ -1,84 +1,56 @@
 <?php
 /**
  * @file
- * Contains \Drupal\AppConsole\Test\Command\GeneratorPluginImageEffectTest.
+ * Contains \Drupal\AppConsole\Test\Command\GeneratorPluginImageEffectCommandTest.
  */
 
 namespace Drupal\AppConsole\Test\Command;
 
+use Drupal\AppConsole\Command\GeneratorPluginImageEffectCommand;
 use Symfony\Component\Console\Tester\CommandTester;
+use Drupal\AppConsole\Test\DataProvider\PluginImageEffectDataProviderTrait;
 
 class GeneratorPluginImageEffectCommandTest extends GenerateCommandTest
 {
+    use PluginImageEffectDataProviderTrait;
+    
     /**
-     * @dataProvider getInteractiveData
+     * Plugin image effect generator test
+     *
+     * @param $module
+     * @param $class_name
+     * @param $plugin_label
+     * @param $plugin_id
+     * @param $description
+     *
+     * @dataProvider commandData
      */
-    public function testInteractive($options, $expected, $input)
-    {
-        list($module, $class_name, $plugin_label, $plugin_id, $description) = $expected;
-
-        $generator = $this->getGenerator();
-        $generator
-            ->expects($this->once())
-            ->method('generate')
-            ->with($module, $class_name, $plugin_label, $plugin_id, $description);
-
-        $command = $this->getCommand($generator, $input);
-        $cmd = new CommandTester($command);
-        $cmd->execute($options);
-    }
-
-    public function getInteractiveData()
-    {
-        return [
-            // case one
-          [
-              // Inline options
-            [],
-              // Expected options
-            ['foo', 'FooImagePlugin', 'Foo label', 'foo_id', 'Foo Description'],
-              // User input options
-            "foo\nFooImagePlugin\nFoo label\nfoo_id\nFoo Description\n",
-          ],
-            // case two
-          [
-              // Inline options
-            [
-              '--module' => 'foo',
-              '--class-name' => 'FooImagePlugin',
-              '--label' => 'Foo label',
-              '--plugin-id' => 'foo_id',
-              '--description' => 'Foo Description'
-            ],
-              // Expected options
-            ['foo', 'FooImagePlugin', 'Foo label', 'foo_id', 'Foo Description'],
-              // User input options
-            "",
-          ],
-        ];
-    }
-
-    protected function getCommand($generator, $input)
-    {
-        $command = $this
-            ->getMockBuilder('Drupal\AppConsole\Command\GeneratorPluginImageEffectCommand')
-            ->setMethods(['getModules', 'getServices', '__construct'])
-            ->setConstructorArgs([$this->getTranslatorHelper()])
-            ->getMock();
-
-        $command->expects($this->any())
-            ->method('getModules')
-            ->will($this->returnValue(['foo']));
-
-        $command->expects($this->any())
-            ->method('getServices')
-            ->will($this->returnValue(['twig', 'database']));
-
+    public function testGeneratePluginImageEffect(
+        $module,
+        $class_name,
+        $plugin_label,
+        $plugin_id,
+        $description
+    ) {
+        $command = new GeneratorPluginImageEffectCommand($this->getTranslatorHelper());
         $command->setContainer($this->getContainer());
-        $command->setHelperSet($this->getHelperSet($input));
-        $command->setGenerator($generator);
+        $command->setHelperSet($this->getHelperSet());
+        $command->setGenerator($this->getGenerator());
 
-        return $command;
+        $commandTester = new CommandTester($command);
+
+        $code = $commandTester->execute(
+            [
+              '--module'         => $module,
+              '--class-name'     => $class_name,
+              '--label'          => $plugin_label,
+              '--plugin-id'      => $plugin_id,
+              '--description'    => $description
+            ],
+            ['interactive' => false]
+        );
+
+        $this->assertEquals(0, $code);
     }
 
     private function getGenerator()
