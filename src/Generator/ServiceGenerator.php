17d8276feeb7f291da787394e7a2ec76968119fb
Omar Aguirre Tenorio <omersguchigu@gmail.com>
2015-09-12 00:49:55 -0500

    Refactor generate:services

--- src/Generator/ServiceGenerator.php
+++ src/Generator/ServiceGenerator.php
@@ -12,18 +12,18 @@ class ServiceGenerator extends Generator
     /**
      * Generator Service.
      *
-     * @param string $module       Module name
-     * @param string $service_name Service name
-     * @param string $class_name   Class name
-     * @param string $interface    If TRUE an interface for this service is generated
-     * @param array  $services     List of services
+     * @param string $module    Module name
+     * @param string $name      Service name
+     * @param string $class     Class name
+     * @param string $interface If TRUE an interface for this service is generated
+     * @param array  $services  List of services
      */
-    public function generate($module, $service_name, $class_name, $interface, $services)
+    public function generate($module, $name, $class, $interface, $services)
     {
         $parameters = [
           'module' => $module,
-          'service_name' => $service_name,
-          'class_name' => $class_name,
+          'name' => $name,
+          'class' => $class,
           'interface' => $interface,
           'services' => $services,
           'file_exists' => file_exists($this->getSite()->getModulePath($module).'/'.$module.'.services.yml'),
@@ -38,14 +38,14 @@ class ServiceGenerator extends Generator
 
         $this->renderFile(
             'module/src/service.php.twig',
-            $this->getSite()->getModulePath($module).'/src/'.$class_name.'.php',
+            $this->getSite()->getModulePath($module).'/src/'.$class.'.php',
             $parameters
         );
 
         if ($interface) {
             $this->renderFile(
                 'module/src/service-interface.php.twig',
-                $this->getSite()->getModulePath($module).'/src/'.$class_name.'Interface.php',
+                $this->getSite()->getModulePath($module).'/src/'.$class.'Interface.php',
                 $parameters
             );
         }
