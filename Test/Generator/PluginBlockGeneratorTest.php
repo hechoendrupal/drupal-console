8b3f5cb2c4e18eda7dbe74ba174869877be8772f
Jorge Atempa <atempaitm@gmail.com>
2015-09-12 14:05:42 -0700

    Add PluginBlockGeneratorTest class

--- Test/Generator/PluginBlockGeneratorTest.php
+++ Test/Generator/PluginBlockGeneratorTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * Contains Drupal\AppConsole\Test\Generator\PluginBlockGeneratorTest.
@@ -6,121 +7,49 @@
 
 namespace Drupal\AppConsole\Test\Generator;
 
+use Drupal\AppConsole\Generator\PluginBlockGenerator;
+use Drupal\AppConsole\Test\DataProvider\PluginBlockDataProviderTrait;
+
 class PluginBlockGeneratorTest extends GeneratorTest
 {
+    use PluginBlockDataProviderTrait;
+
     /**
+     * PluginBlock generator test
+     *
+     * @param $module
+     * @param $class_name
+     * @param $label
+     * @param $plugin_id
+     * @param $services
+     * @param $inputs
+     *
      * @dataProvider commandData
      */
-    public function testGeneratePluginBlock($parameters)
-    {
-        $this->setUpTemporalDirectory();
-
-        // Get parameters
-        list($module, $class_name, $label, $plugin_id, $services, $inputs) = $parameters;
-
-        // Get generator
-        $generator = $this->getGenerator();
+    public function testGeneratePluginBlock(
+        $module,
+        $class_name,
+        $label,
+        $plugin_id,
+        $services,
+        $inputs
+    ) {
+        $generator = new PluginBlockGenerator();
+        $generator->setSkeletonDirs(__DIR__ . '/../../templates');
         $generator->setHelpers($this->getHelperSet());
 
-        $dir_module = $this->dir . '/' . $module;
-        // Set plugin path.
-        $generator->expects($this->once())
-            ->method('getPluginPath')
-            ->will(
-                $this->returnValue(
-                    $dir_module . '/src/Plugin/Block'
-                )
-            );
-
-        // Generate plugin block
-        $generator->generate($module, $class_name, $label, $plugin_id, $services, $inputs);
-
-        //        $this->assertTrue(
-        //          file_exists($dir_module . '/src/Plugin/Block/' . $class_name . '.php'),
-        //          sprintf('%s has been generated', $dir_module . '/src/Plugin/Block/' . $class_name . '.php')
-        //        );
-
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
+        $generator->generate(
+            $module,
+            $class_name,
+            $label,
+            $plugin_id,
+            $services,
+            $inputs
         );
 
-        $contains = [
-          'build',
-          '@Block',
-          'id',
-          'label',
-          $class_name . ' extends BlockBase',
-        ];
-
-        if ($inputs) {
-            $contains += [
-              'blockForm()',
-              'blockForm($form, FormStateInterface $form_state)',
-              'blockSubmit()',
-              'blockSubmit($form, FormStateInterface $form_state)',
-            ];
-        }
-
-        if ($services) {
-            $contains += [
-              '__construct()',
-              'create()',
-              'create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)',
-              'implements ContainerFactoryPluginInterface',
-            ];
-        }
-
-        $content = file_get_contents($dir_module . '/src/Plugin/Block/' . $class_name . '.php');
-        foreach ($contains as $contain) {
-            $this->assertContains($contain, $content);
-        }
-    }
-
-    public function commandData()
-    {
-        $services = [
-          'twig' => [
-            'name' => 'twig',
-            'machine_name' => 'twig',
-            'class' => 'Twig_Environment',
-            'short' => 'Twig_Environment',
-          ]
-        ];
-
-        $inputs = [
-          [
-            'name' => 'foo',
-            'type' => 'textfield',
-            'label' => 'Foo',
-            'options' => 'foo',
-            'description' => 'Foo',
-          ]
-        ];
-
-        return [
-          [
-            ['plugin_block' . rand(), 'FooBlock', 'Foo Block', 'foo_block', null, []]
-          ],
-          [
-            ['plugin_block' . rand(), 'FooBlock', 'Foo Block', 'foo_block', $services, []]
-          ],
-          [
-            ['plugin_block' . rand(), 'FooBlock', 'Foo Block', 'foo_block', null, $inputs]
-          ],
-          [
-            ['plugin_block' . rand(), 'FooBlock', 'Foo Block', 'foo_block', $services, $inputs]
-          ],
-        ];
-    }
-
-    protected function getGenerator()
-    {
-        $generator = $this->getMockBuilder('\Drupal\AppConsole\Generator\PluginBlockGenerator')
-            ->setMethods(['getPluginPath'])
-            ->getMock();
-
-        $generator->setSkeletonDirs($this->getSkeletonDirs());
-
-        return $generator;
+        $this->assertTrue(
+            file_exists($generator->getSite()->getPluginPath($module, 'Block').'/'.$class_name.'.php'),
+            sprintf('%s does not exist', $class_name.'.php')
+        );        
     }
 }
