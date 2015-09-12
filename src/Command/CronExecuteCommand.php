66b9d9df24f38e5e70f31edcb6063578f36a8145
enzo - Eduardo Garcia <enzo@enzolutions.com>
2015-09-11 18:06:56 -0600

    cron commands

--- src/Command/CronExecuteCommand.php
+++ src/Command/CronExecuteCommand.php
@@ -10,6 +10,7 @@ namespace Drupal\AppConsole\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOptionuse;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\views\Entity\View;
 use Drupal\Component\Serialization\Yaml;
@@ -21,50 +22,52 @@ class CronExecuteCommand extends ContainerAwareCommand
         $this
             ->setName('cron:execute')
             ->setDescription($this->trans('commands.views.execute.description'))
-            ->addOption('module', '', InputOption::VALUE_REQUIRED, $this->trans('commands.common.options.module'));
+            ->addArgument('module', InputArgument::OPTIONAL, $this->trans('commands.common.options.module'));
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $module = $input->getOption('module');
+        $module = $input->getArgument('module');
+
         $module_handler = $this->getModuleHandler();
 
-        if ($module_handler->implementsHook($module, 'cron')) {
-            try {
-                $this->moduleHandler->invoke($module, 'cron');
-            } catch (\Exception $e) {
-                watchdog_exception('cron', $e);
+        if ($module != 'all') {
+            $modules = [$module];
+        } else {
+            $modules = $module_handler->getImplementations('cron');
+        }
+
+
+        foreach ($modules as $module) {
+            if ($module_handler->implementsHook($module, 'cron')) {
+                $output->writeln(
+                    '[-] <info>' .
+                    sprintf(
+                        $this->trans('commands.cron.execute.messages.executing-cron'),
+                        $module
+                    )
+                    . '</info>'
+                );
+                try {
+                    $module_handler->invoke($module, 'cron');
+                } catch (\Exception $e) {
+                    watchdog_exception('cron', $e);
+                    $output->writeln(
+                        '<error>' .
+                        $e->getMessage() .
+                        '</error>'
+                    );
+                }
+            } else {
                 $output->writeln(
                     '<error>' .
-                    $e->getMessage().
-                    '</error>'
+                    sprintf(
+                        $this->trans('commands.cron.execute.messages.module-invalid'),
+                        $module
+                    )
+                    . '</error>'
                 );
             }
-        } else {
-            $output->writeln(
-                '<error>' .
-                sprintf(
-                    $this->trans('commands.cron.execute.messages.module-invalid'),
-                $module
-                )
-                . '</error>'
-            );
         }
-
-        $table->setHeaders(
-            [
-                $this->trans('commands.cron.debug.messages.module'),
-            ]
-        );
-
-        foreach ($module_handler->getImplementations('cron') as $module) {
-            $table->addRow(
-                [
-                    $module,
-                ]
-            );
-        }
-
-        $table->render($output);
     }
 }
