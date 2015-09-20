1c948187bb8db04a648bd3a910ed8452abebbd42
enzo - Eduardo Garcia <enzo@anexusit.com>
2015-09-19 22:01:13 -0600

    Changed to enable see about command outside drupal installation

--- src/Command/AboutCommand.php
+++ src/Command/AboutCommand.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\views\Entity\View;
 use Drupal\Component\Serialization\Yaml;
 
-class AboutCommand extends ContainerAwareCommand
+class AboutCommand extends Command
 {
     protected function configure()
     {
