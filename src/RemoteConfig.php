d432beadb9b789c7bcb3ad27aab6e6e2c3da0d3a
dmouse <dmousex@gmail.com>
2015-09-23 14:46:04 +0200

    set the dockbloc

--- src/RemoteConfig.php
+++ src/RemoteConfig.php
@@ -7,11 +7,19 @@ use Symfony\Component\Debug\Exception\FatalThrowableError;
 class RemoteConfig extends Config
 {
 
+    /**
+     * @param string $target
+     * @return bool
+     */
     public function exist($target)
     {
         return file_exists($this->getUserHomeDir() . '/.console/remote/' . $target . '.yml');
     }
 
+    /**
+     * @param string $target
+     * @return array|mixed|null|\ThrowExceptionTestCase
+     */
     public function getTarget($target)
     {
         if ($this->exist($target)) {
