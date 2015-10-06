444aa29e131ea628eaf0d91e52bf0e4b4d21ffe6
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-10-05 20:36:18 -0700

    Update version to 0.9.2

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -24,7 +24,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const VERSION = '0.9.1';
+    const VERSION = '0.9.2';
     /**
      * @var Drupal\Console\UserConfig
      */
