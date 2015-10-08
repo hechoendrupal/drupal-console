85ffc0c9fc1e2bb031cf9f7c367f4a405c3c441e
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-10-08 00:56:46 -0700

    [helper] Improve remove calls to getHelpers

--- src/EventSubscriber/ShowGenerateDocListener.php
+++ src/EventSubscriber/ShowGenerateDocListener.php
@@ -18,16 +18,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class ShowGenerateDocListener implements EventSubscriberInterface
 {
-    private $skipCommands = [
-        'self-update',
-        'list',
-    ];
-
     private $skipOptions = [
         'generate-doc'
-    ];
-
-    private $skipArguments = [
     ];
 
     /**
@@ -60,7 +52,7 @@ class ShowGenerateDocListener implements EventSubscriberInterface
               'aliases' => $command->getAliases()
             ];
 
-            $renderedDoc = $application->getHelperSet()->get('renderer')->render(
+            $renderedDoc = $application->getRenderHelper()->render(
                 'gitbook/generate-doc.md.twig',
                 $parameters
             );
