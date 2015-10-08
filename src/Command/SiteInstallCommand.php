462bc6cd710ca0592367a85ecaeed4e8d3581623
enzo - Eduardo Garcia <enzo@anexusit.com>
2015-10-07 19:38:21 -0600

    Update files

--- src/Command/SiteInstallCommand.php
+++ src/Command/SiteInstallCommand.php
@@ -15,54 +15,52 @@ use Drupal\Core\Database\Database;
 use Drupal\Core\Extension\ExtensionDiscovery;
 use Drupal\Component\FileCache\FileCacheFactory;
 
-
 class SiteInstallCommand extends Command
 {
     protected $connection;
-    protected $migration_group;
 
     protected function configure()
     {
         $this
-            ->setName('site:install')
-            ->setDescription($this->trans('commands.site.install.description'))
-            ->addArgument('profile', InputArgument::OPTIONAL, $this->trans('commands.site.install.arguments.profile'))
-            ->addOption(
-                'db-host',
-                '',
-                InputOption::VALUE_REQUIRED,
-                $this->trans('commands.migrate.execute.options.db-host')
-            )
-            ->addOption(
-                'db-name',
-                '',
-                InputOption::VALUE_REQUIRED,
-                $this->trans('commands.migrate.execute.options.db-name')
-            )
-            ->addOption(
-                'db-user',
-                '',
-                InputOption::VALUE_REQUIRED,
-                $this->trans('commands.migrate.execute.options.db-user')
-            )
-            ->addOption(
-                'db-pass',
-                '',
-                InputOption::VALUE_OPTIONAL,
-                $this->trans('commands.migrate.execute.options.db-pass')
-            )
-            ->addOption(
-                'db-prefix',
-                '',
-                InputOption::VALUE_OPTIONAL,
-                $this->trans('commands.migrate.execute.options.db-prefix')
-            )
-            ->addOption(
-                'db-port',
-                '',
-                InputOption::VALUE_REQUIRED,
-                $this->trans('commands.migrate.execute.options.db-port')
-            );
+          ->setName('site:install')
+          ->setDescription($this->trans('commands.site.install.description'))
+          ->addArgument('profile', InputArgument::OPTIONAL, $this->trans('commands.site.install.arguments.profile'))
+          ->addOption(
+            'db-host',
+            '',
+            InputOption::VALUE_REQUIRED,
+            $this->trans('commands.migrate.execute.options.db-host')
+          )
+          ->addOption(
+            'db-name',
+            '',
+            InputOption::VALUE_REQUIRED,
+            $this->trans('commands.migrate.execute.options.db-name')
+          )
+          ->addOption(
+            'db-user',
+            '',
+            InputOption::VALUE_REQUIRED,
+            $this->trans('commands.migrate.execute.options.db-user')
+          )
+          ->addOption(
+            'db-pass',
+            '',
+            InputOption::VALUE_OPTIONAL,
+            $this->trans('commands.migrate.execute.options.db-pass')
+          )
+          ->addOption(
+            'db-prefix',
+            '',
+            InputOption::VALUE_OPTIONAL,
+            $this->trans('commands.migrate.execute.options.db-prefix')
+          )
+          ->addOption(
+            'db-port',
+            '',
+            InputOption::VALUE_REQUIRED,
+            $this->trans('commands.migrate.execute.options.db-port')
+          );
     }
 
     /**
@@ -87,11 +85,11 @@ class SiteInstallCommand extends Command
         $db_host = $input->getOption('db-host');
         if (!$db_host) {
             $db_host = $dialog->askAndValidate(
-                $output,
-                $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-host'), '127.0.0.1'),
-                $validator_required,
-                false,
-                '127.0.0.1'
+              $output,
+              $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-host'), '127.0.0.1'),
+              $validator_required,
+              false,
+              '127.0.0.1'
             );
         }
         $input->setOption('db-host', $db_host);
@@ -100,11 +98,11 @@ class SiteInstallCommand extends Command
         $db_name = $input->getOption('db-name');
         if (!$db_name) {
             $db_name = $dialog->askAndValidate(
-                $output,
-                $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-name'), ''),
-                $validator_required,
-                false,
-                null
+              $output,
+              $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-name'), ''),
+              $validator_required,
+              false,
+              null
             );
         }
         $input->setOption('db-name', $db_name);
@@ -113,11 +111,11 @@ class SiteInstallCommand extends Command
         $db_user = $input->getOption('db-user');
         if (!$db_user) {
             $db_user = $dialog->askAndValidate(
-                $output,
-                $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-user'), ''),
-                $validator_required,
-                false,
-                null
+              $output,
+              $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-user'), ''),
+              $validator_required,
+              false,
+              null
             );
         }
         $input->setOption('db-user', $db_user);
@@ -126,9 +124,9 @@ class SiteInstallCommand extends Command
         $db_pass = $input->getOption('db-pass');
         if (!$db_pass) {
             $db_pass = $dialog->askHiddenResponse(
-                $output,
-                $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-pass'), ''),
-                ''
+              $output,
+              $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-pass'), ''),
+              ''
             );
         }
         $input->setOption('db-pass', $db_pass);
@@ -137,9 +135,9 @@ class SiteInstallCommand extends Command
         $db_prefix = $input->getOption('db-prefix');
         if (!$db_prefix) {
             $db_prefix = $dialog->ask(
-                $output,
-                $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-prefix'), ''),
-                ''
+              $output,
+              $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-prefix'), ''),
+              ''
             );
         }
         $input->setOption('db-prefix', $db_prefix);
@@ -148,32 +146,14 @@ class SiteInstallCommand extends Command
         $db_port = $input->getOption('db-port');
         if (!$db_port) {
             $db_port = $dialog->askAndValidate(
-                $output,
-                $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-port'), '3306'),
-                $validator_required,
-                false,
-                '3306'
+              $output,
+              $dialog->getQuestion($this->trans('commands.migrate.execute.questions.db-port'), '3306'),
+              $validator_required,
+              false,
+              '3306'
             );
         }
         $input->setOption('db-port', $db_port);
-
-        // Get migrations available
-        //$this->registerDB($input);
-
-        //$this->getConnection($output);
-    }
-
-    protected function getConnection(OutputInterface $output)
-    {
-        try {
-            $this->connection = Database::getConnection('default', 'install');
-        } catch (\Exception $e) {
-            $output->writeln('[+] <error>'.$this->trans('commands.migrate.execute.messages.destination-error').': '.$e->getMessage().'</error>');
-
-            return;
-        }
-
-        return $this;
     }
 
     protected function registerDB(InputInterface $input, OutputInterface $output)
@@ -214,13 +194,6 @@ class SiteInstallCommand extends Command
         $profiles = $this->getProfiles();
 
         print_r($profiles);
-
-        /*if (!$this->connection) {
-            $this->registerSourceDB($input);
-            $this->getConnection($output);
-        }*/
-
-        $entity_manager = $this->getEntityManager();
     }
 
     protected function getProfiles() {
@@ -228,8 +201,6 @@ class SiteInstallCommand extends Command
         $listing = null;
 
         $drupal = $this->getDrupalHelper();
-
-        print_r($drupal->getRoot());
 
         // Add list of all available profiles to the installation state.
         $listing = new ExtensionDiscovery($drupal->getRoot());
