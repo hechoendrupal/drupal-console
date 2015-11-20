b5daa694d4b2bac03fef4e837ebffae28fc93669
enzo - Eduardo Garcia <enzo@anexusit.com>
2015-11-20 10:39:19 -0600

    [console] Replaced deprecated classes in generated code

--- src/Command/Views/DebugCommand.php
+++ src/Command/Views/DebugCommand.php
@@ -218,7 +218,6 @@ class DebugCommand extends ContainerAwareCommand
             $definition = $displayManager->getDefinition($display['display_plugin']);
             if (!empty($definition['admin'])) {
                 // Cast the admin label to a string since it is an object.
-                // @see \Drupal\Core\StringTranslation\TranslationWrapper
                 $displays[$definition['id']]['name'] = (string) $definition['admin'];
                 $displays[$definition['id']]['description'] = (string) $definition['help'];
             }
