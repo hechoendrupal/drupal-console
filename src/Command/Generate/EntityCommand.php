ab9796fc7a6e41c45daf791f7dd06f509e7a410a
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-11-22 09:59:42 -0800

    [generate:entity:*] Improve PR #1321

--- src/Command/Generate/EntityCommand.php
+++ src/Command/Generate/EntityCommand.php
@@ -11,8 +11,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Command\ModuleTrait;
-use Drupal\Console\Generator\EntityConfigGenerator;
-use Drupal\Console\Generator\EntityContentGenerator;
 use Drupal\Console\Command\GeneratorCommand;
 
 abstract class EntityCommand extends GeneratorCommand
@@ -42,79 +40,42 @@ abstract class EntityCommand extends GeneratorCommand
      */
     protected function configure()
     {
-      if ('EntityContent' == $this->entityType) {
-        $this
-            ->setName($this->commandName)
-            ->setDescription(
-                sprintf(
-                    $this->trans('commands.generate.entity.content.description'),
-                    $this->entityType
-                )
-            )
-            ->setHelp(
-                sprintf(
-                    $this->trans('commands.generate.entity.content.help'),
-                    $this->commandName,
-                    $this->entityType
-                )
-            )
-            ->addOption('module', null, InputOption::VALUE_REQUIRED, $this->trans('commands.common.options.module'))
-            ->addOption(
-                'entity-class',
-                null,
-                InputOption::VALUE_REQUIRED,
-                $this->trans('commands.generate.entity.content.options.entity-class')
-            )
-            ->addOption(
-                'entity-name',
-                null,
-                InputOption::VALUE_REQUIRED,
-                $this->trans('commands.generate.entity.content.options.entity-name')
-            )
-            ->addOption(
-                'label',
-                null,
-                InputOption::VALUE_REQUIRED,
-                $this->trans('commands.generate.entity.content.options.label')
-            );
-      }
-      else {
-        $this
-            ->setName($this->commandName)
-            ->setDescription(
-                sprintf(
-                    $this->trans('commands.generate.entity.config.description'),
-                    $this->entityType
-                )
-            )
-            ->setHelp(
-                sprintf(
-                    $this->trans('commands.generate.entity.config.help'),
-                    $this->commandName,
-                    $this->entityType
-                )
-            )
-            ->addOption('module', null, InputOption::VALUE_REQUIRED, $this->trans('commands.common.options.module'))
-            ->addOption(
-                'entity-class',
-                null,
-                InputOption::VALUE_REQUIRED,
-                $this->trans('commands.generate.entity.config.options.entity-class')
-            )
-            ->addOption(
-                'entity-name',
-                null,
-                InputOption::VALUE_REQUIRED,
-                $this->trans('commands.generate.entity.config.options.entity-name')
-            )
-            ->addOption(
-                'label',
-                null,
-                InputOption::VALUE_REQUIRED,
-                $this->trans('commands.generate.entity.config.options.label')
-            );
-      }
+        $commandKey = str_replace(':', '.', $this->commandName);
 
+        $this
+            ->setName($this->commandName)
+            ->setDescription(
+                sprintf(
+                    $this->trans('commands.'.$commandKey.'.description'),
+                    $this->entityType
+                )
+            )
+            ->setHelp(
+                sprintf(
+                    $this->trans('commands.'.$commandKey.'.help'),
+                    $this->commandName,
+                    $this->entityType
+                )
+            )
+            ->addOption('module', null, InputOption::VALUE_REQUIRED, $this->trans('commands.common.options.module'))
+            ->addOption(
+                'entity-class',
+                null,
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.'.$commandKey.'.options.entity-class')
+            )
+            ->addOption(
+                'entity-name',
+                null,
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.'.$commandKey.'.options.entity-name')
+            )
+            ->addOption(
+                'label',
+                null,
+                InputOption::VALUE_REQUIRED,
+                $this->trans('commands.'.$commandKey.'.options.label')
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -153,7 +114,7 @@ abstract class EntityCommand extends GeneratorCommand
             $entity_class = 'DefaultEntity';
             $entity_class = $dialog->askAndValidate(
                 $output,
-                $dialog->getQuestion($this->trans('commands.generate.entity.questions.entity-class'), $entity_class),
+                $dialog->getQuestion($this->trans('commands.'.$commandKey.'.questions.entity-class'), $entity_class),
                 function ($entity_class) {
                     return $this->validateSpaces($entity_class);
                 },
@@ -171,7 +132,7 @@ abstract class EntityCommand extends GeneratorCommand
         if (!$entity_name) {
             $entity_name = $dialog->askAndValidate(
                 $output,
-                $dialog->getQuestion($this->trans('commands.generate.entity.questions.entity-name'), $machine_name),
+                $dialog->getQuestion($this->trans('commands.'.$commandKey.'.questions.entity-name'), $machine_name),
                 function ($machine_name) {
                     return $this->validateMachineName($machine_name);
                 },
@@ -189,7 +150,7 @@ abstract class EntityCommand extends GeneratorCommand
         if (!$label) {
             $label = $dialog->ask(
                 $output,
-                $dialog->getQuestion($this->trans('commands.generate.entity.questions.label'), $default_label),
+                $dialog->getQuestion($this->trans('commands.'.$commandKey.'.questions.label'), $default_label),
                 $default_label
             );
         }
@@ -198,10 +159,5 @@ abstract class EntityCommand extends GeneratorCommand
 
     protected function createGenerator()
     {
-        if ('EntityContent' == $this->entityType) {
-            return new EntityContentGenerator();
-        }
-
-        return new EntityConfigGenerator();
     }
 }
