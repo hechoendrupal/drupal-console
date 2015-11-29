bdb963883b4009a78d6350a7dbb24efc273b1fc8
Omar Aguirre <omersguchigu@gmail.com>
2015-11-28 21:17:52 -0600

    Update self-update command using padraic/phar-updater

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -9,12 +9,12 @@ namespace Drupal\Console\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Herrera\Phar\Update\Manager;
-use Herrera\Phar\Update\Manifest;
+use Humbug\SelfUpdate\Strategy\GithubStrategy;
+use Humbug\SelfUpdate\Updater;
+use Drupal\Console\Application;
 
 class SelfUpdateCommand extends Command
 {
-    const DRUPAL_CONSOLE_MANIFEST = 'http://drupalconsole.com/manifest.json';
 
     /**
      * {@inheritdoc}
@@ -32,21 +32,27 @@ class SelfUpdateCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $manager = new Manager(
-            Manifest::loadFile(
-                self::DRUPAL_CONSOLE_MANIFEST
-            )
-        );
 
-        if ($manager->update($this->getApplication()->getVersion(), true)) {
-            $output->writeln($this->trans('commands.self-update.messages.success'));
-        } else {
-            $output->writeln(
-                sprintf(
-                    $this->trans('commands.self-update.messages.current-version'),
-                    $this->getApplication()->getVersion()
-                )
-            );
+        $updateStrategy = new GithubStrategy();
+        $updateStrategy->setPackageName('drupal/console');
+        $updateStrategy->setStability(GithubStrategy::STABLE);
+        $updateStrategy->setPharName('console.phar');
+        $updateStrategy->setCurrentLocalVersion(Application::VERSION);
+
+        $updater = new Updater(null, false);
+        $updater->setStrategyObject($updateStrategy);
+        if ($updater->update()) {
+            $output->writeln(sprintf(
+              $this->trans('commands.self-update.messages.success'),
+              $updater->getOldVersion(),
+              $updater->getNewVersion()
+            ));
+        }
+        else {
+            $output->writeln(sprintf(
+              $this->trans('commands.self-update.messages.current-version'),
+              $updater->getOldVersion()
+            ));
         }
 
         // Recommended by Commerce Guys CLI
