47322b5fafc86221743b89a5b1e5279b063b6f50
Omar Aguirre <omersguchigu@gmail.com>
2015-12-12 16:00:54 -0600

    Change output on RebuildCommand class with DrupalStyle Fix #1463

--- src/Command/Router/RebuildCommand.php
+++ src/Command/Router/RebuildCommand.php
@@ -10,6 +10,7 @@ namespace Drupal\Console\Command\Router;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Command\ContainerAwareCommand;
+use Drupal\Console\Style\DrupalStyle;
 
 class RebuildCommand extends ContainerAwareCommand
 {
@@ -22,11 +23,17 @@ class RebuildCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('');
-        $output->writeln('[+] <comment>'.$this->trans('commands.router.rebuild.messages.rebuilding').'</comment>');
+        $output = new DrupalStyle($input, $output);
+        $output->newLine();
+        $output->writeln(
+          sprintf(
+            '<comment>%s</comment>',
+            $this->trans('commands.router.rebuild.messages.rebuilding')
+          )
+        );
         $container = $this->getContainer();
         $router_builder = $container->get('router.builder');
         $router_builder->rebuild();
-        $output->writeln('[+] <info>'.$this->trans('commands.router.rebuild.messages.completed').'</info>');
+        $output->success($this->trans('commands.router.rebuild.messages.completed'));
     }
 }
