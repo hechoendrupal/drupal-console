699ef6e2568515e92dff8c3f0bf6ff857101ae75
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-12-14 14:40:29 -0800

    [generate:module] Apply several Command fixes

--- src/Command/ModuleTrait.php
+++ src/Command/ModuleTrait.php
@@ -9,18 +9,26 @@ namespace Drupal\Console\Command;
 
 use Drupal\Console\Style\DrupalStyle;
 
+/**
+ * Class ModuleTrait
+ * @package Drupal\Console\Command
+ */
 trait ModuleTrait
 {
     /**
-     * @param DrupalStyle $output
-     *
-     * @return mixed
+     * @param \Drupal\Console\Style\DrupalStyle $io
+     * @return string
+     * @throws \Exception
      */
-    public function moduleQuestion(DrupalStyle $output)
+    public function moduleQuestion(DrupalStyle $io)
     {
         $modules = $this->getSite()->getModules(false, false, false, true, true);
 
-        $module = $output->choiceNoList(
+        if (empty($modules)) {
+            throw new \Exception('No modules available, execute `generate:module` command to generate one.');
+        }
+
+        $module = $io->choiceNoList(
             $this->trans('commands.common.questions.module'),
             $modules
         );
