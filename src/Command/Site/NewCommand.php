78136c59c8bd33b3a139b8d775642f3a66348b54
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-12-19 15:01:24 -0800

    [site:*] Implement DrupalStyle

--- src/Command/Site/NewCommand.php
+++ src/Command/Site/NewCommand.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Contains \Drupal\Console\Command\ModuleDownloadCommand.
+ * Contains \Drupal\Console\Command\Site\NewCommand.
  */
 
 namespace Drupal\Console\Command\Site;
@@ -9,7 +9,6 @@ namespace Drupal\Console\Command\Site;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
@@ -24,91 +23,93 @@ class NewCommand extends Command
         $this
             ->setName('site:new')
             ->setDescription($this->trans('commands.site.new.description'))
-            ->addArgument('site-name', InputArgument::REQUIRED, $this->trans('commands.site.new.arguments.site-name'))
-            ->addArgument('version', InputArgument::OPTIONAL, $this->trans('commands.site.new.arguments.version'));
+            ->addArgument(
+                'site-name',
+                InputArgument::REQUIRED,
+                $this->trans('commands.site.new.arguments.site-name')
+            )
+            ->addArgument(
+                'version',
+                InputArgument::OPTIONAL,
+                $this->trans('commands.site.new.arguments.version')
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output = new DrupalStyle($input, $output);
+        $io = new DrupalStyle($input, $output);
 
         $httpClient = $this->getHttpClientHelper();
 
-        $site_name = $input->getArgument('site-name');
+        $siteName = $input->getArgument('site-name');
         $version = $input->getArgument('version');
 
         if ($version) {
-            $release_selected = $version;
+            $releaseSelected = $version;
         } else {
             // Getting Module page header and parse to get module Node
-            $output->writeln('[+] <info>' . sprintf($this->trans('commands.site.new.messages.getting-releases')) . '</info>');
+            $io->info(
+                sprintf($this->trans('commands.site.new.messages.getting-releases'))
+            );
 
             // Page for Drupal releases filter by Drupal 8
-            $project_release_d8 = 'https://www.drupal.org/node/3060/release?api_version%5B%5D=7234';
+            $projectReleaseSelected = 'https://www.drupal.org/node/3060/release?api_version%5B%5D=7234';
 
             // Parse release module page to get Drupal 8 releases
             try {
-                $html = $httpClient->getHtml($project_release_d8);
+                $html = $httpClient->getHtml($projectReleaseSelected);
             } catch (\Exception $e) {
-                $output->writeln('[+] <error>' . $e->getMessage() . '</error>');
+                $io->error($e->getMessage());
+
                 return;
             }
             $crawler = new Crawler($html);
             $releases = [];
             foreach ($crawler->filter('span.file a') as $element) {
                 if (strpos($element->nodeValue, ".tar.gz") > 0) {
-                    $release_name = str_replace(
+                    $releaseName = str_replace(
                         '.tar.gz', '',
                         str_replace(
                             'drupal-', '', $element->nodeValue
                         )
                     );
-                    $releases[$release_name] = $element->nodeValue;
+                    $releases[$releaseName] = $element->nodeValue;
                 }
             }
 
             if (empty($releases)) {
-                $output->writeln('[+] <error>' . $this->trans('commands.module.site.new.no-releases') . '</error>');
+                $io->error($this->trans('commands.module.site.new.no-releases'));
+
                 return;
             }
 
-            // List module releases to enable user to select his favorite release
-            $questionHelper = $this->getQuestionHelper();
-
-            $question = new ChoiceQuestion(
+            $releaseSelected = $io->choice(
                 'Please select your favorite release',
-                array_combine(array_keys($releases), array_keys($releases)),
-                0
+                array_keys($releases)
             );
-
-            $release_selected = $questionHelper->ask($input, $output, $question);
         }
 
-        $release_file_path = 'http://ftp.drupal.org/files/projects/drupal-' . $release_selected . '.tar.gz';
+        $releaseFilePath = 'http://ftp.drupal.org/files/projects/drupal-' . $releaseSelected . '.tar.gz';
 
         // Destination file to download the release
         $destination = tempnam(sys_get_temp_dir(), 'drupal.') . "tar.gz";
 
         try {
             // Start the process to download the zip file of release and copy in contrib folter
-            $output->writeln(
-                '[+] <info>' .
+            $io->info(
                 sprintf(
                     $this->trans('commands.site.new.messages.downloading'),
-                    $release_selected
-                ) .
-                '</info>'
+                    $releaseSelected
+                )
             );
 
-            $httpClient->downloadFile($release_file_path, $destination);
+            $httpClient->downloadFile($releaseFilePath, $destination);
 
-            $output->writeln(
-                '[+] <info>' .
+            $io->info(
                 sprintf(
                     $this->trans('commands.site.new.messages.extracting'),
-                    $release_selected
-                ) .
-                '</info>'
+                    $releaseSelected
+                )
             );
 
             $zippy = Zippy::load();
@@ -116,27 +117,29 @@ class NewCommand extends Command
             $archive->extract('./');
 
             try {
-                $fs = new Filesystem();
-                $fs->rename('./drupal-' . $release_selected, './' . $site_name);
+                $filesyStem = new Filesystem();
+                $filesyStem->rename('./drupal-' . $releaseSelected, './' . $siteName);
             } catch (IOExceptionInterface $e) {
-                $output->writeln(
-                    '[+] <error>'. sprintf(
+                $io->error(
+                    sprintf(
                         $this->trans('commands.site.new.messages.error-copying'),
                         $e->getPath()
-                    )  . '</error>'
+                    )
                 );
             }
 
-            $output->success(
+            $io->success(
                 sprintf(
                     $this->trans('commands.site.new.messages.downloaded'),
-                    $release_selected, $site_name
+                    $releaseSelected, $siteName
                 )
             );
         } catch (\Exception $e) {
-            $output->writeln('[+] <error>' . $e->getMessage() . '</error>');
-            return;
+            $io->error($e->getMessage());
+
+            return false;
         }
+
         return true;
     }
 }
