b514391f0368fc13281496cc657736287290713d
Nicolas Moncada <nicolas.moncada@tifon.cl>
2015-12-22 17:46:10 -0300

    #1542 fixed interact method in [user:login:clear:attempts]

--- src/Command/User/LoginCleanAttemptsCommand.php
+++ src/Command/User/LoginCleanAttemptsCommand.php
@@ -32,47 +32,44 @@ class LoginCleanAttemptsCommand extends ContainerAwareCommand
     }
 
     /**
-     * Verify if given User ID is valid value for question uid.
-     *
-     * @param  int $uid User ID to check.
-     * @return int A valid User ID.
-     * @throws \Symfony\Component\Process\Exception\InvalidArgumentException
-     */
-    public function validateQuestionsUid($uid)
-    {
-        // Check if $uid is numeric.
-        $message = (!is_numeric($uid)) ?
-        $this->trans('commands.user.login.clear.attempts.questions.numeric-uid') :
-        false;
-        // Check if $uid is upper than zero.
-        if (!$message && $uid <= 0) {
-            $message = $this->trans('commands.user.login.clear.attempts.questions.invalid-uid');
-        }
-        // Check if message was defined.
-        if ($message) {
-            throw new \InvalidArgumentException(
-                $message
-            );
-        }
-        // Return a valid $uid.
-        return (int) $uid;
-    }
-
-    /**
      * {@inheritdoc}
      */
     protected function interact(InputInterface $input, OutputInterface $output)
     {
         $io = new DrupalStyle($input, $output);
 
+        $uid = $input->getArgument('uid');
         // Check if $uid argument is already set.
-        if (!$uid = $input->getArgument('uid')) {
-            // Request $uid argument.
-            $uid = $io->ask(
-                $this->trans('commands.user.login.clear.attempts.questions.uid'),
-                1,
-                $this->validateQuestionsUid($uid)
-            );
+        if (!$uid) {
+            while (true) {
+                // Request $uid argument.
+                $uid = $io->ask(
+                    $this->trans('commands.user.login.clear.attempts.questions.uid'),
+                    1,
+                    function ($uid) use ($io) {
+                        $message = (!is_numeric($uid)) ?
+                        $this->trans('commands.user.login.clear.attempts.questions.numeric-uid') :
+                        false;
+                        // Check if $uid is upper than zero.
+                        if (!$message && $uid <= 0) {
+                            $message = $this->trans('commands.user.login.clear.attempts.questions.invalid-uid');
+                        }
+                        // Check if message was defined.
+                        if ($message) {
+                            $io->error($message);
+
+                            return false;
+                        }
+                        // Return a valid $uid.
+                        return (int) $uid;
+                    }
+                );
+
+                if ($uid) {
+                    break;
+                }
+            }
+
             $input->setArgument('uid', $uid);
         }
     }
@@ -89,12 +86,14 @@ class LoginCleanAttemptsCommand extends ContainerAwareCommand
 
         if (!$account) {
             // Error loading User entity.
-            throw new \InvalidArgumentException(
+            $io->error(
                 sprintf(
                     $this->trans('commands.user.login.clear.attempts.errors.invalid-user'),
                     $uid
                 )
             );
+
+            return;
         }
 
         // Define event name and identifier.
