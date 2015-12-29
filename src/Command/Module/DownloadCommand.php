c818c736d6c8a117f04caef642d06d514bbe9646
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2015-12-29 01:47:15 -0800

    [console] Refactor module/theme download and site new.

--- src/Command/Module/DownloadCommand.php
+++ src/Command/Module/DownloadCommand.php
@@ -7,159 +7,63 @@
 
 namespace Drupal\Console\Command\Module;
 
-use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Alchemy\Zippy\Zippy;
 use Drupal\Console\Command\Command;
 use Drupal\Console\Style\DrupalStyle;
+use Drupal\Console\Command\ProjectDownloadTrait;
 
 class DownloadCommand extends Command
 {
+    use ProjectDownloadTrait;
+
     protected function configure()
     {
         $this
             ->setName('module:download')
             ->setDescription($this->trans('commands.module.download.description'))
-            ->addArgument('module', InputArgument::REQUIRED, $this->trans('commands.module.download.options.module'))
-            ->addArgument('version', InputArgument::OPTIONAL, $this->trans('commands.module.download.options.version'));
+            ->addArgument(
+                'module',
+                InputArgument::REQUIRED,
+                $this->trans('commands.module.download.options.module')
+            )
+            ->addArgument(
+                'version',
+                InputArgument::OPTIONAL,
+                $this->trans('commands.module.download.options.version'),
+                null
+            );
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new DrupalStyle($input, $output);
-        $httpClient = $this->getHttpClientHelper();
 
         $module = $input->getArgument('module');
-
         $version = $input->getArgument('version');
 
-        if ($version) {
-            $release_selected = $version;
-        } else {
-            // Getting Module page header and parse to get module Node
-            $io->info(
-                sprintf(
-                    $this->trans('commands.module.download.messages.getting-releases'),
-                    implode(',', array($module))
-                )
-            );
-
-            try {
-                $link = $httpClient->getHeader('https://www.drupal.org/project/'.$module, 'link');
-            } catch (\Exception $e) {
-                $io->error($e->getMessage());
-                return;
-            }
-
-            $header_link = explode(';', $link);
-            $project_node = str_replace('<', '', str_replace('>', '', $header_link[0]));
-            $project_release_d8 = $project_node.'/release?api_version%5B%5D=7234';
-
-            // Parse release module page to get Drupal 8 releases
-            try {
-                $html = $httpClient->getHtml($project_release_d8);
-            } catch (\Exception $e) {
-                $io->error($e->getMessage());
-                return;
-            }
-
-            $crawler = new Crawler($html);
-            $releases = [];
-            foreach ($crawler->filterXPath('.//*/div[2]/div[2]/div/table/tbody/tr[1]/td[1]/span[@class=\'file\']/a') as $element) {
-                if (strpos($element->nodeValue, '.tar.gz') > 0) {
-                    $release_name = str_replace(
-                        '.tar.gz',
-                        '',
-                        str_replace(
-                            $module.'-',
-                            '',
-                            $element->nodeValue
-                        )
-                    );
-                    $releases[$release_name] = $element->nodeValue;
-                }
-            }
-
-            if (empty($releases)) {
-                $io->error(
-                    sprintf(
-                        $this->trans('commands.module.download.messages.no-releases'),
-                        implode(',', array($module))
-                    )
-                );
-                return;
-            }
-
-            $release_selected = $io->choice(
-                $this->trans('commands.module.download.messages.select-release'),
-                array_keys($releases)
-            );
-        }
-
-        // Start the process to download the zip file of release and copy in contrib folter
-
-        $io->info(
-            sprintf(
-                $this->trans('commands.module.download.messages.downloading'),
-                $module,
-                $release_selected
-            )
-        );
-
-        $release_file_path = 'http://ftp.drupal.org/files/projects/'.$module.'-'.$release_selected.'.tar.gz';
-
-        // Destination file to download the release
-        $destination = tempnam(sys_get_temp_dir(), 'console.').'.tar.gz';
-
-        try {
-            $httpClient->downloadFile($release_file_path, $destination);
-
-            // Determine destination folder for contrib modules
-            $drupal = $this->getDrupalHelper();
-            $drupalRoot = $drupal->getRoot();
-            if ($drupalRoot) {
-                $module_contrib_path = $drupalRoot . '/modules/contrib';
-            } else {
-                $io->info(
-                    sprintf(
-                        $this->trans('commands.module.download.messages.outside-drupal'),
-                        $module,
-                        $release_selected
-                    )
-                );
-                $module_contrib_path = getcwd() . '/modules/contrib';
-            }
-
-            // Create directory if does not exist
-            if (!file_exists($module_contrib_path)) {
-                if (!mkdir($module_contrib_path, 0777, true)) {
-                    $io->error($this->trans('commands.module.download.messages.error-creating-folder') . ': ' . $module_contrib_path);
-                    return;
-                }
-            }
-
-            // Prepare release to unzip and untar
-            $zippy = Zippy::load();
-            $archive = $zippy->open($destination);
-            $archive->extract($module_contrib_path . '/');
-
-            unlink($destination);
-
-            $io->info(
-                sprintf(
-                    $this->trans('commands.module.download.messages.downloaded'),
-                    $module,
-                    $release_selected,
-                    $module_contrib_path
-                )
-            );
-        } catch (\Exception $e) {
-            $io->error($e->getMessage());
-            return;
-        }
+        $this->downloadProject($io, $module, $version, 'module');
 
         return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $io = new DrupalStyle($input, $output);
+
+        $module = $input->getArgument('module');
+        $version = $input->getArgument('version');
+
+        if (!$version) {
+            $version = $this->releasesQuestion($io, $module);
+            $input->setArgument('version', $version);
+        }
     }
 }
--- src/Command/Theme/DownloadCommand.php
+++ src/Command/Theme/DownloadCommand.php
@@ -7,17 +7,20 @@
 
 namespace Drupal\Console\Command\Theme;
 
-use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Command\Command;
-use Alchemy\Zippy\Zippy;
 use Drupal\Console\Style\DrupalStyle;
+use Drupal\Console\Command\ProjectDownloadTrait;
 
 class DownloadCommand extends Command
 {
+    use ProjectDownloadTrait;
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
@@ -27,148 +30,32 @@ class DownloadCommand extends Command
             ->addArgument('version', InputArgument::OPTIONAL, $this->trans('commands.theme.download.options.version'));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new DrupalStyle($input, $output);
-        $httpClient = $this->getHttpClientHelper();
 
         $theme = $input->getArgument('theme');
-
         $version = $input->getArgument('version');
 
-        if ($version) {
-            $release_selected = $version;
-        } else {
-            // Getting Theme page header and parse to get theme Node
-            $io->info(
-                sprintf(
-                    $this->trans('commands.theme.download.messages.getting-releases'),
-                    implode(',', array($theme))
-                )
-            );
+        $this->downloadProject($io, $theme, $version, 'theme');
+    }
 
-            try {
-                $link = $httpClient->getHeader('https://www.drupal.org/project/'.$theme, 'link');
-            } catch (\Exception $e) {
-                $io->error($e->getMessage());
-                return;
-            }
+    /**
+     * {@inheritdoc}
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $io = new DrupalStyle($input, $output);
 
-            $header_link = explode(';', $link);
-            $project_node = str_replace('<', '', str_replace('>', '', $header_link[0]));
-            $project_release_d8 = $project_node.'/release?api_version%5B%5D=7234';
+        $theme = $input->getArgument('theme');
+        $version = $input->getArgument('version');
 
-            // Parse release theme page to get Drupal 8 releases
-            try {
-                $html = $httpClient->getHtml($project_release_d8);
-            } catch (\Exception $e) {
-                $io->error($e->getMessage());
-
-                return;
-            }
-
-            $crawler = new Crawler($html);
-            $releases = [];
-            foreach ($crawler->filter('span.file a') as $element) {
-                if (strpos($element->nodeValue, '.tar.gz') > 0) {
-                    $release_name = str_replace(
-                        '.tar.gz',
-                        '',
-                        str_replace(
-                            $theme.'-',
-                            '',
-                            $element->nodeValue
-                        )
-                    );
-                    $releases[$release_name] = $element->nodeValue;
-                }
-            }
-
-            if (empty($releases)) {
-                $io->error(
-                    sprintf(
-                        $this->trans('commands.theme.download.messages.no-releases'),
-                        implode(',', array($theme))
-                    )
-                );
-
-                return;
-            }
-
-            // List theme releases to enable user to select his favorite release
-            $questionHelper = $this->getQuestionHelper();
-
-            $question = new ChoiceQuestion(
-                $this->trans('commands.theme.download.messages.select-release'),
-                array_combine(array_keys($releases), array_keys($releases)),
-                '0'
-            );
-
-            $release_selected = $questionHelper->ask($input, $output, $question);
+        if (!$version) {
+            $version = $this->releasesQuestion($io, $theme);
+            $input->setArgument('version', $version);
         }
-
-        // Start the process to download the zip file of release and copy in contrib folter
-        $io->info(
-            sprintf(
-                $this->trans('commands.theme.download.messages.downloading'),
-                $theme,
-                $release_selected
-            )
-        );
-
-        $release_file_path = 'http://ftp.drupal.org/files/projects/'.$theme.'-'.$release_selected.'.tar.gz';
-
-        // Destination file to download the release
-        $destination = tempnam(sys_get_temp_dir(), 'console.').'.tar.gz';
-
-        try {
-            $httpClient->downloadFile($release_file_path, $destination);
-
-            // Determine destination folder for contrib theme
-            $drupal = $this->getDrupalHelper();
-            $drupalRoot = $drupal->getRoot();
-            if ($drupalRoot) {
-                $theme_contrib_path = $drupalRoot . '/themes/contrib';
-            } else {
-                $io->info(
-                    sprintf(
-                        $this->trans('commands.theme.download.messages.outside-drupal'),
-                        $theme,
-                        $release_selected
-                    )
-                );
-                $theme_contrib_path = getcwd() . '/themes/contrib';
-            }
-
-            // Create directory if does not exist
-            if (!file_exists($theme_contrib_path)) {
-                if (!mkdir($theme_contrib_path, 0777, true)) {
-                    $io->error($this->trans('commands.theme.download.messages.error-creating-folder') . ': ' . $theme_contrib_path);
-                    return;
-                }
-            }
-
-            // Prepare release to unzip and untar
-            $zippy = Zippy::load();
-            $archive = $zippy->open($destination);
-            $archive->extract($theme_contrib_path . '/');
-
-            unlink($destination);
-
-            $io->info(
-                sprintf(
-                    $this->trans('commands.theme.download.messages.downloaded'),
-                    $theme,
-                    $release_selected,
-                    $theme_contrib_path
-                )
-            );
-        } catch (\Exception $e) {
-            $io->error($e->getMessage());
-
-            return;
-        }
-
-        return true;
     }
 }
