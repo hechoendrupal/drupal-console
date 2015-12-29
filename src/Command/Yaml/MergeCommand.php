ec34e9dba9b6abc7023d6bb85b103112016cd6b4
Nicolas Moncada <nicolas.moncada@tifon.cl>
2015-12-29 18:29:59 -0300

    is_file is added and the error is translated

--- src/Command/Yaml/MergeCommand.php
+++ src/Command/Yaml/MergeCommand.php
@@ -59,7 +59,7 @@ class MergeCommand extends Command
                     sprintf(
                         '%s: %s',
                         $this->trans('commands.yaml.merge.messages.error-parsing'),
-                        $e->getMessage().'</error>'
+                        $e->getMessage()
                     )
                 );
 
@@ -123,9 +123,8 @@ class MergeCommand extends Command
         $io = new DrupalStyle($input, $output);
 
         $validator_filename = function ($value) use ($io) {
-            if (!strlen(trim($value))) {
-                // @todo: translate
-                $io->error(' You must provide a valid file path.');
+            if (!strlen(trim($value)) || !is_file($value)) {
+                $io->error($this->trans('commands.common.errors.invalid-file-path'));
 
                 return false;
             }
