cf8b63cebce4b71c511657058c08b974f1cc575a
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-03-16 22:46:40 -0700

    [update:entities] Add new command.

--- src/Command/Update/EntitiesCommand.php
+++ src/Command/Update/EntitiesCommand.php
@@ -1,8 +1,10 @@
 <?php
+
 /**
  * @file
  * Contains \Drupal\Console\Command\Update\EntitiesCommand.
  */
+
 namespace Drupal\Console\Command\Update;
 
 use Drupal\Console\Command\ContainerAwareCommand;
@@ -13,42 +15,45 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Style\DrupalStyle;
 
 /**
- * Class EntityUpdatesCommand.
+ * Class EntitiesCommand.
  *
- * @package Drupal\entityupdates
+ * @package Drupal\Console\Command\Update
  */
-class EntitiesCommand extends ContainerAwareCommand {
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function configure() {
-    $this
-      ->setName('update:entities')
-      ->setDescription($this->trans('command.update.entities.description'));
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function execute(InputInterface $input, OutputInterface $output) {
-    $io = new DrupalStyle($input, $output);
-
-    $io->info($this->trans('command.update.entities.messages.start'));
-    $state = $this->getService('state');
-    $state->set('system.maintenance_mode', TRUE);
-
-    try {
-      \Drupal::entityDefinitionUpdateManager()->applyUpdates();
-    } catch (EntityStorageException $e) {
-      $variables = Error::decodeException($e);
-      $io->info($this->trans('command.update.entities.messages.error'));
-      $io->info($variables);
+class EntitiesCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('update:entities')
+            ->setDescription($this->trans('commands.update.entities.description'));
     }
 
-    $state->set('system.maintenance_mode', FALSE);
-    $io->info($this->trans('commands.site.maintenance.messages.maintenance-off'));
-    $this->getChain()->addCommand('cache:rebuild', ['cache' => 'all']);
-    $io->info($this->trans('command.update.entities.messages.end'));
-  }
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new DrupalStyle($input, $output);
+
+        $state = $this->getService('state');
+        $io->info($this->trans('commands.site.maintenance.messages.maintenance-on'));
+        $io->info($this->trans('commands.update.entities.messages.start'));
+        $state->set('system.maintenance_mode', true);
+
+        try {
+            $this->getService('entity.definition_update_manager')->applyUpdates();
+        } catch (EntityStorageException $e) {
+            $variables = Error::decodeException($e);
+            $io->info($this->trans('commands.update.entities.messages.error'));
+            $io->info($variables);
+        }
+
+        $state->set('system.maintenance_mode', false);
+        $io->info($this->trans('commands.update.entities.messages.end'));
+        $this->getChain()->addCommand('cache:rebuild', ['cache' => 'all']);
+        $io->info($this->trans('commands.site.maintenance.messages.maintenance-off'));
+    }
 }
