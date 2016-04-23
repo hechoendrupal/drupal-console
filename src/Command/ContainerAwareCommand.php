dd982293570a51c16aca0108bc32c89f230d6ce1
Darryl Norris <darol100@2667123.no-reply.drupal.org>
2016-04-23 04:31:05 -0700

    [container-aware] Moving strings to yml files so it can be translate.

--- src/Command/ContainerAwareCommand.php
+++ src/Command/ContainerAwareCommand.php
@@ -454,7 +454,7 @@ abstract class ContainerAwareCommand extends Command
         $machine_name = $this->validateMachineName($machine_name);
         $modules = $this->getSite()->getModules(false, true, true, true, true, true);
         if (in_array($machine_name, $modules)) {
-            throw new \InvalidArgumentException(sprintf('Module "%s" already exist.', $machine_name));
+            throw new \InvalidArgumentException(sprintf('commands.container-aware.errors.module-exist', $machine_name));
         }
 
         return $machine_name;
@@ -480,7 +480,7 @@ abstract class ContainerAwareCommand extends Command
         $machine_name = $this->getValidator()->validateMachineName($machine_name);
 
         if ($this->getEntityManager()->hasDefinition($machine_name)) {
-            throw new \InvalidArgumentException(sprintf('Machine name "%s" is duplicated.', $machine_name));
+            throw new \InvalidArgumentException(sprintf('commands.container-aware.errors.machine-name.duplicated', $machine_name));
         }
 
         return $machine_name;
