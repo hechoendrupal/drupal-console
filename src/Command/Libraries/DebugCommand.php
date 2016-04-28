d2a8a63b19392a9ac23a0374553a66bf04ebb49a
Darryl Norris <darol100@2667123.no-reply.drupal.org>
2016-04-28 09:46:36 -0700

    libraries-debug command

---
+++ src/Command/Libraries/DebugCommand.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Console\Command\Libraries\DebugCommand.
+ */
+
+namespace Drupal\Console\Command\Libraries;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Drupal\Console\Command\ContainerAwareCommand;
+use Drupal\Console\Style\DrupalStyle;
+use Drupal\Component\Serialization\Yaml;
+
+
+class DebugCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('libraries:debug')
+            ->setDescription($this->trans('commands.libraries.debug.description'))
+            ->addArgument(
+                'group',
+                InputArgument::OPTIONAL,
+                $this->trans('commands.libraries.debug.options.group')
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new DrupalStyle($input, $output);
+
+        $group = $input->getArgument('group');
+        if (!$group) {
+            $groups = $this->getAllLibraries();
+
+            $tableHeader = [
+                $this->trans('commands.libraries.debug.messages.name'),
+            ];
+
+            $io->table($tableHeader, $groups, 'compact');
+        }
+        else {
+            $librariesData = $this->getLibraryByName($group);
+
+            foreach ($librariesData as $key => $libraries ) {
+                $io->comment($key);
+                $io->writeln(Yaml::encode($libraries));
+
+            }
+
+        }
+    }
+
+    private function getAllLibraries()
+    {
+        $modules = \Drupal::moduleHandler()->getModuleList();
+        $themes = \Drupal::service('theme_handler')->rebuildThemeData();
+        
+        $extensions = array_merge($modules, $themes);
+        $libraryDiscovery = \Drupal::service('library.discovery');
+
+        $root = \Drupal::root();
+        foreach ($extensions as $extension_name => $extension) {
+            $library_file = $extension->getPath() . '/' . $extension_name . '.libraries.yml';
+            if (is_file($root . '/' . $library_file)) {
+                $libraries[$extension_name] = $libraryDiscovery->getLibrariesByExtension($extension_name);
+            }
+        }
+        $extensionLibraries = array_keys($libraries);
+        return $extensionLibraries;
+    }
+
+    /**
+     * @param $group    String
+     */
+    private function getLibraryByName($group)
+    {
+        $libraryDiscovery = \Drupal::service('library.discovery');
+        $library = $libraryDiscovery->getLibrariesByExtension($group);
+        return  $library;
+    }
+}
