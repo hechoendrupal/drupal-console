42106f46d4691edb31a608a61a9fda23ac459622
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-05-02 11:54:12 -0700

    [console] Tag new release 1.0.0-alpha2

--- src/Application.php
+++ src/Application.php
@@ -30,7 +30,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const VERSION = '1.0.0-alpha1';
+    const VERSION = '1.0.0-alpha2';
 
     /**
      * @var string
