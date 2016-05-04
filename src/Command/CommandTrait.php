879de87d2c1937bd617315324e6ec02d44a46bc7
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-05-03 22:52:23 -0700

    [console] Add CommandTrait class.

---
+++ src/Command/CommandTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\Console\Command;
+
+/**
+ * Class CommandTrait
+ * @package Drupal\Console\Command
+ */
+trait CommandTrait
+{
+    /** @var  \Drupal\Console\Helper\TranslatorHelper */
+    protected $translator;
+
+    /**
+     * CommandTrait constructor.
+     * @param \Drupal\Console\Helper\TranslatorHelper $translator
+     */
+    public function __construct(
+        \Drupal\Console\Helper\TranslatorHelper $translator
+    ) {
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param $key string
+     *
+     * @return string
+     */
+    public function trans($key) {
+        if ($this->translator) {
+            return $this->translator->trans($key);
+        }
+
+        return $key;
+    }
+
+    /**
+     * @return \Drupal\Console\Application;
+     */
+    public function getApplication()
+    {
+        return parent::getApplication();
+    }
+}
