bf189ba301256626ebb7f8eaf58f5fadae16c041
David Flores <dmousex@gmail.com>
2016-05-04 00:39:53 -0500

    move site helper like service in test

--- Test/BaseTestCase.php
+++ Test/BaseTestCase.php
@@ -86,7 +86,7 @@ abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
                     'string' => $stringHelper,
                     'validator' => $validator,
                     'translator' => $translator,
-                    'site' => $siteHelper,
+                    'site' => $container->get('helper_site'),
                     'chain' => $chain,
                     'drupal' => $drupal,
                     'container' => new ContainerHelper($container),
