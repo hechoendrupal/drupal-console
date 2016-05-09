a6ff8817992aa44b50bcf006526552d0fe689782
Kenneth Bolivar <keboca@gmail.com>
2016-05-09 00:04:57 +0000

    #2228 Add "all" argument to config:delete

--- src/Command/Config/DeleteCommand.php
+++ src/Command/Config/DeleteCommand.php
@@ -12,80 +12,149 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Command\ContainerAwareCommand;
 use Drupal\Console\Style\DrupalStyle;
 
-class DeleteCommand extends ContainerAwareCommand
-{
-    /**
-     * {@inheritdoc}
-     */
-    protected function configure()
-    {
-        $this
-            ->setName('config:delete')
-            ->setDescription($this->trans('commands.config.delete.description'))
-            ->addArgument(
-                'name',
-                InputArgument::OPTIONAL,
-                $this->trans('commands.config.delete.arguments.name')
-            );
+class DeleteCommand extends ContainerAwareCommand {
+  protected $allConfig = [];
+  protected $configFactory = NULL;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this
+      ->setName('config:delete')
+      ->setDescription($this->trans('commands.config.delete.description'))
+      ->addArgument(
+        'name',
+        InputArgument::OPTIONAL,
+        $this->trans('commands.config.delete.arguments.name')
+      );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function interact(InputInterface $input, OutputInterface $output) {
+    // Init Drupal style and retrieve name argument.
+    $io = new DrupalStyle($input, $output);
+    $name = $input->getArgument('name');
+    // Check config name is not missing.
+    if (!$name) {
+      // Define choice list.
+      $name = $io->choiceNoList(
+        $this->trans('commands.config.delete.arguments.name'),
+        $this->getAllConfigNames(),
+        'all'
+      );
+      $input->setArgument('name', $name);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    // Init Drupal style and retrieve name argument.
+    $io = new DrupalStyle($input, $output);
+    $name = $input->getArgument('name');
+    // Check config name is not missing.
+    if (!$name) {
+      $io->error($this->trans('commands.config.delete.messages.name'));
+      return 1;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function interact(InputInterface $input, OutputInterface $output)
-    {
-        $io = new DrupalStyle($input, $output);
-        $name = $input->getArgument('name');
-        if (!$name) {
-            $configFactory = $this->getService('config.factory');
-            $names = $configFactory->listAll();
-            $name = $io->choiceNoList(
-                $this->trans('commands.config.delete.arguments.name'),
-                $names
-            );
-            $input->setArgument('name', $name);
+    // Check if current option chose was "all".
+    if ('all' === $name) {
+      // Caveat about remove all configuration.
+      $io->caution($this->trans('commands.config.delete.warnings.undo'));
+      // Double check before execute it.
+      if ($io->confirm($this->trans('commands.config.delete.questions.sure'), FALSE)) {
+        // Remove all configuration.
+        foreach ($this->yieldAllConfig() as $name) {
+          $this->removeConfig($io, $name, FALSE);
         }
+        // Define successful message.
+        $io->success($this->trans('commands.config.delete.messages.all'));
+      }
+    } // Load $configStorage and check config name already exists.
+    elseif (($configStorage = $this->getService('config.storage')) && ($configStorage->exists($name))) {
+      $this->removeConfig($io, $name);
+    }
+    else {
+      // Otherwise, shows up error because config name does not exist.
+      $message = sprintf($this->trans('commands.config.delete.messages.not-exists'), $name);
+      $io->error($message);
+      return 1;
+    }
+  }
+
+  /**
+   * Retrieve config factory property.
+   *
+   * @return \Drupal\Core\Config\ConfigFactoryInterface|null
+   */
+  private function configFactory() {
+    // Define config factory from service if it does not exist.
+    $this->configFactory = $this->configFactory ?: $this->getConfigFactory();
+    return $this->configFactory;
+  }
+
+  /**
+   * Retrieve configuration names form cache or service factory.
+   *
+   * @return array
+   *   All configuration names.
+   */
+  private function getAllConfigNames() {
+    // If configuration names exist, then return them.
+    if (!empty($this->allConfig)) {
+      return $this->allConfig;
+    }
+    // Retrieve configuration factory.
+    foreach ($this->configFactory()->listAll() as $name) {
+      // Store configuration name.
+      $this->allConfig[] = $name;
+    }
+    // Return all configuration names.
+    return $this->allConfig;
+  }
+
+  /**
+   * Yield configuration names.
+   *
+   * @return \Generator
+   *   Yield generator with config name.
+   */
+  private function yieldAllConfig() {
+    // Walk trough all config names and yield them.
+    foreach ($this->allConfig as $name) {
+      yield $name;
+    }
+  }
+
+  /**
+   * Delete given config name.
+   *
+   * @param DrupalStyle $io IO instance.
+   * @param String $name Given config name.
+   * @param bool $show_message Flag to show message or not.
+   * @return int
+   *   When exception was threw.
+   */
+  private function removeConfig(DrupalStyle $io, $name, $show_message = TRUE) {
+    try {
+      // Retrieve config factory and delete given configuration.
+      $this->configFactory()->getEditable($name)->delete();
+    } catch (\Exception $e) {
+      // Show error message.
+      $io->error($e->getMessage());
+      return 1;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $io = new DrupalStyle($input, $output);
-        $configFactory = $this->getService('config.factory');
-        $name = $input->getArgument('name');
-        if (!$name) {
-            $io->error($this->trans('commands.config.delete.messages.name'));
-
-            return 1;
-        }
-
-        $configStorage = $this->getService('config.storage');
-        if (!$configStorage->exists($name)) {
-            $io->error(
-                sprintf(
-                    $this->trans('commands.config.delete.messages.not-exists'),
-                    $name
-                )
-            );
-
-            return 1;
-        }
-
-        try {
-            $configFactory->getEditable($name)->delete();
-        } catch (\Exception $e) {
-            $io->error($e->getMessage());
-
-            return 1;
-        }
-
-        $io->success(
-            sprintf(
-                $this->trans('commands.config.delete.messages.deleted'),
-                $name
-            )
-        );
+    // Check flag to show message.
+    if ($show_message) {
+      // Define and print successful message.
+      $message = sprintf($this->trans('commands.config.delete.messages.deleted'), $name);
+      $io->success($message);
     }
+  }
 }
