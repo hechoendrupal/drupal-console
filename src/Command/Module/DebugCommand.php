2d06f451a7fcc28fb430dc212d48c0df2d707c47
leandro <leandro@leandro.org>
2016-05-27 19:45:27 +0200

    info about a module in «module:debug namemodule»

--- src/Command/Module/DebugCommand.php
+++ src/Command/Module/DebugCommand.php
@@ -7,19 +7,29 @@
 
 namespace Drupal\Console\Command\Module;
 
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Command\ContainerAwareCommand;
 use Drupal\Console\Style\DrupalStyle;
 
+
 class DebugCommand extends ContainerAwareCommand
 {
+
+    private $urlRepo = "https://packagist.drupal-composer.org/packages/drupal/";
+
     protected function configure()
     {
         $this
             ->setName('module:debug')
             ->setDescription($this->trans('commands.module.debug.description'))
+            ->addArgument(
+                'module',
+                InputArgument::OPTIONAL | InputArgument::IS_ARRAY,
+                $this->trans('commands.module.debug.module')
+            )
             ->addOption('status', null, InputOption::VALUE_OPTIONAL, $this->trans('commands.module.debug.options.status'))
             ->addOption('type', null, InputOption::VALUE_OPTIONAL, $this->trans('commands.module.debug.options.type'));
     }
@@ -32,6 +42,41 @@ class DebugCommand extends ContainerAwareCommand
 
         $status = $input->getOption('status');
         $type = $input->getOption('type');
+        $modules = $input->getArgument('module');
+
+        if ($modules) {
+          foreach ($modules as $module) {
+            $url = $this->urlRepo . $module . ".json";
+            $data = $this->getHelperSet()->get('httpClient')->getUrlAsJson($url);
+
+            $tableHeader = [
+                '<info>'.$data->package->name.'</info>'
+            ];
+            $tableRows = [];
+
+            $tableRows[] = [
+                $data->package->description
+            ];
+
+            $tableRows[] = [
+                '<comment>'.$this->trans('commands.module.debug.messages.total-downloads').'</comment>',
+                $data->package->downloads->total
+            ];
+
+            $tableRows[] = [
+                '<comment>'.$this->trans('commands.module.debug.messages.total-monthly').'</comment>',
+                $data->package->downloads->monthly
+            ];
+
+            $tableRows[] = [
+                '<comment>'.$this->trans('commands.module.debug.messages.total-daily').'</comment>',
+                $data->package->downloads->daily
+            ];
+
+            $io->table($tableHeader, $tableRows, 'compact');
+          }
+          return;
+        }
 
         if (strtolower($status) == 'enabled') {
             $status = 1;
