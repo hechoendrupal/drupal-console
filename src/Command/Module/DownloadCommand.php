1e2193301d314fa4bfdacf6b725654e7f9c9c04c
enzo - Eduardo Garcia <enzo@enzolutions.com>
2016-05-27 15:02:08 +1000

    [generate:post:update] New Command (#2336)

--- src/Command/Module/DownloadCommand.php
+++ src/Command/Module/DownloadCommand.php
@@ -7,7 +7,6 @@
 
 namespace Drupal\Console\Command\Module;
 
-
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
