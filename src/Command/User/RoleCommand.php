06b32bc89b84fcb9f79e2b02897be2c88ea7bb18
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-05-28 01:34:33 -0700

    [console] Apply PSR2 code style. (#2347)

--- src/Command/User/RoleCommand.php
+++ src/Command/User/RoleCommand.php
@@ -56,9 +56,9 @@ class RoleCommand extends ContainerAwareCommand
         $role = $input->getArgument('role');
 
         if (!$op || !$user || !$role) {
-          throw new \Exception(
-            $this->trans('commands.user.role.messages.bad-arguments')
-          );
+            throw new \Exception(
+                $this->trans('commands.user.role.messages.bad-arguments')
+            );
         }
     }
 
@@ -75,39 +75,37 @@ class RoleCommand extends ContainerAwareCommand
         $systemRoles = $this->getDrupalApi()->getRoles();
 
         if (is_numeric($user)) {
-          $userObject = user_load($user);
-        }
-        else {
-          $userObject = user_load_by_name($user);
+            $userObject = user_load($user);
+        } else {
+            $userObject = user_load_by_name($user);
         }
 
 
         if (!is_object($userObject)) {
-          if (!filter_var($user, FILTER_VALIDATE_EMAIL) === false) {
-            $userObject = user_load_by_mail($user);
-          }
+            if (!filter_var($user, FILTER_VALIDATE_EMAIL) === false) {
+                $userObject = user_load_by_mail($user);
+            }
         }
 
         if (!is_object($userObject)) {
-          $io->error(sprintf($this->trans('commands.user.role.messages.no-user-found'), $user));
-          return 1;
-
+            $io->error(sprintf($this->trans('commands.user.role.messages.no-user-found'), $user));
+            return 1;
         }
 
         if (!array_key_exists($role, $systemRoles)) {
-          $io->error(sprintf($this->trans('commands.user.role.messages.no-role-found'), $role));
-          return 1;
+            $io->error(sprintf($this->trans('commands.user.role.messages.no-role-found'), $role));
+            return 1;
         }
 
         if ("add" == $op) {
             $userObject->addRole($role);
             $userObject->save();
             $io->success(
-              sprintf(
-                $this->trans('commands.user.role.messages.add-success'),
-                $userObject->name->value . " (" . $userObject->mail->value . ") ",
-                $role
-              )
+                sprintf(
+                    $this->trans('commands.user.role.messages.add-success'),
+                    $userObject->name->value . " (" . $userObject->mail->value . ") ",
+                    $role
+                )
             );
         }
 
@@ -116,11 +114,11 @@ class RoleCommand extends ContainerAwareCommand
             $userObject->save();
 
             $io->success(
-              sprintf(
-                $this->trans('commands.user.role.messages.remove-success'),
-                $userObject->name->value . " (" . $userObject->mail->value . ") ",
-                $role
-              )
+                sprintf(
+                    $this->trans('commands.user.role.messages.remove-success'),
+                    $userObject->name->value . " (" . $userObject->mail->value . ") ",
+                    $role
+                )
             );
         }
     }
