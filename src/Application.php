8e444b182c3f484e905c4378657f0a41732b5645
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-06-02 11:31:26 -0700

    [console] Tag new version 1.0.0-beta2.

--- src/Application.php
+++ src/Application.php
@@ -30,7 +30,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const VERSION = '1.0.0-beta1';
+    const VERSION = '1.0.0-beta2';
 
     /**
      * @var string
