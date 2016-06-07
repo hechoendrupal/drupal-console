6439b15d77df06f735b408f7d3acd5c7518b46d4
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-06-07 09:33:53 -0700

    [router:debug] Method not found error. Fix 2400

--- src/Command/Generate/HelpCommand.php
+++ src/Command/Generate/HelpCommand.php
@@ -11,8 +11,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Generator\HelpGenerator;
-use Drupal\Console\Command\ModuleTrait;
-use Drupal\Console\Command\ConfirmationTrait;
+use Drupal\Console\Command\Shared\ModuleTrait;
+use Drupal\Console\Command\Shared\ConfirmationTrait;
 use Drupal\Console\Command\GeneratorCommand;
 use Drupal\Console\Style\DrupalStyle;
 
