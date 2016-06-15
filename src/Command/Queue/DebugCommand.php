477200d91d6d355a17c7033ec00dbae7360024cf
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-06-15 10:32:05 -0700

    Queue improvements (#2422)
    * [queue:*] Commands improvements.
    
    * [queue:*] Remove class property.
    
    * [queue:*] Fix indentation.

--- src/Command/Queue/DebugCommand.php
+++ src/Command/Queue/DebugCommand.php
@@ -8,10 +8,10 @@
 namespace Drupal\Console\Command\Queue;
 
 use Symfony\Component\Console\Command\Command;
-use Drupal\Console\Style\DrupalStyle;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Command\Shared\ContainerAwareCommandTrait;
+use Drupal\Console\Style\DrupalStyle;
 
 /**
  * Class DebugCommand
@@ -19,52 +19,62 @@ use Drupal\Console\Command\Shared\ContainerAwareCommandTrait;
  */
 class DebugCommand extends Command
 {
-  use ContainerAwareCommandTrait;
+    use ContainerAwareCommandTrait;
 
-  /**
-   * @var $queueManager \Drupal\Core\Queue\QueueWorkerManagerInterface
-   */
-  private $queueManager;
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function configure()
-  {
-    $this
-      ->setName('queue:debug')
-      ->setDescription($this->trans('commands.queue.debug.description'));
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function execute(InputInterface $input, OutputInterface $output)
-  {
-    $io = new DrupalStyle($input, $output);
-    $this->queueManager = $this->getDrupalService('plugin.manager.queue_worker');
-    $this->listQueues($io);
-  }
-
-  /**
-   * @param \Drupal\Console\Style\DrupalStyle $io
-   */
-  private function listQueues(DrupalStyle $io) {
-    $header = ['queue', 'items', 'class'];
-    foreach ($this->queueManager->getDefinitions() as $name => $info) {
-      $queues[$name] = $this->formatQueues($name, $header);
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('queue:debug')
+            ->setDescription($this->trans('commands.queue.debug.description'));
     }
-    $io->table($header, $queues);
-  }
 
-  /**
-   * @param $name
-   * @param array $header
-   * @return array
-   */
-  private function formatQueues($name, $header) {
-    $q = $this->getDrupalService('queue')->get($name);
-    return array_combine($header, [$name, $q->numberOfItems(), get_class($q)]);
-  }
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new DrupalStyle($input, $output);
 
+        $tableHeader = [
+            $this->trans('commands.queue.debug.messages.queue'),
+            $this->trans('commands.queue.debug.messages.items'),
+            $this->trans('commands.queue.debug.messages.class')
+        ];
+
+        $tableBody = $this->listQueues();
+
+        $io->table($tableHeader, $tableBody);
+    }
+
+    /**
+     * listQueues.
+     */
+    private function listQueues()
+    {
+        $queueManager = $this->getDrupalService('plugin.manager.queue_worker');
+        $queues = [];
+        foreach ($queueManager->getDefinitions() as $name => $info) {
+            $queues[$name] = $this->formatQueue($name);
+        }
+
+        return $queues;
+    }
+
+    /**
+     * @param $name
+     * @return array
+     */
+    private function formatQueue($name)
+    {
+        $q = $this->getDrupalService('queue')->get($name);
+
+        return [
+            $name,
+            $q->numberOfItems(),
+            get_class($q)
+        ];
+    }
 }
