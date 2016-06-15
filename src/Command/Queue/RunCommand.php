8e8b0d3acfe77a76fd3d73a0a7124f5da7a9cc90
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-06-15 10:21:11 -0700

    [queue:*] Commands improvements.

--- src/Command/Queue/RunCommand.php
+++ src/Command/Queue/RunCommand.php
@@ -7,13 +7,12 @@
 
 namespace Drupal\Console\Command\Queue;
 
-use MongoDB\BSON\Timestamp;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
-use Drupal\Console\Style\DrupalStyle;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Command\Shared\ContainerAwareCommandTrait;
+use Drupal\Console\Style\DrupalStyle;
 
 /**
  * Class RunCommand
@@ -21,95 +20,103 @@ use Drupal\Console\Command\Shared\ContainerAwareCommandTrait;
  */
 class RunCommand extends Command
 {
-  use ContainerAwareCommandTrait;
+    use ContainerAwareCommandTrait;
 
-  /**
-   * @var $queueManager \Drupal\Core\Queue\QueueWorkerManagerInterface
-   */
-  private $queueManager;
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('queue:run')
+            ->setDescription($this->trans('commands.queue.run.description'))
+            ->addArgument(
+                'name',
+                InputArgument::OPTIONAL,
+                $this->trans('commands.queue.run.arguments.name')
+            );
+    }
 
-  /**
+    /**
    * {@inheritdoc}
    */
-  protected function configure()
-  {
-    $this
-      ->setName('queue:run')
-      ->setDescription($this->trans('commands.queue.run.description'))
-      ->addArgument(
-        'queue-name',
-        InputArgument::REQUIRED,
-        $this->trans('commands.queue.run.arguments.queue-name')
-      );
-  }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new DrupalStyle($input, $output);
+        $name = $input->getArgument('name');
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function execute(InputInterface $input, OutputInterface $output)
-  {
-    $io = new DrupalStyle($input, $output);
-    $queue_name = $input->getArgument('queue-name');
-    if ($queue_name) {
-      $this->queueManager = $this->getDrupalService('plugin.manager.queue_worker');
-      $this->runQueue($io, $queue_name);
+        if (!$name) {
+            $io->error(
+                $this->trans('commands.queue.run.messages.missing-name')
+            );
+
+            return 1;
+        }
+
+        $queueManager = $this->getDrupalService('plugin.manager.queue_worker');
+
+        try {
+            $worker = $queueManager->createInstance($name);
+        } catch (\Exception $e) {
+            $io->error(
+                sprintf(
+                    $this->trans('commands.queue.run.messages.invalid-name'),
+                    $name
+                )
+            );
+
+            return 1;
+        }
+
+        $start = microtime(true);
+        $result = $this->runQueue($worker, $name);
+        $time = microtime(true) - $start;
+
+        if (!empty($result['error'])) {
+            $io->error(
+                sprintf(
+                    $this->trans('commands.queue.run.messages.failed'),
+                    $name,
+                    $result['error']
+                )
+            );
+
+            return 1;
+        }
+
+        $io->success(
+            sprintf(
+                $this->trans('commands.queue.run.success'),
+                $name,
+                $result['count'],
+                $result['total'],
+                round($time, 2)
+            )
+        );
     }
 
-  }
+    /**
+     * @param $worker
+     * @param $name
+     *
+     * @return array
+     */
+    private function runQueue($worker, $name)
+    {
+        $q = $this->getDrupalService('queue')->get($name);
+        $result['count'] = 0;
+        $result['total'] = $q->numberOfItems();
+        while ($item = $q->claimItem()) {
+            try {
+                $worker->processItem($item->data);
+                $q->deleteItem($item);
+                $result['count']++;
+            } catch (SuspendQueueException $e) {
+                $q->releaseItem($item);
+                $result['error'] = $e;
+            }
+        }
 
-  /**
-   * @param \Drupal\Console\Style\DrupalStyle $io
-   * @param $queue_name
-   */
-  private function runQueue(DrupalStyle $io, $queue_name) {
-    $worker = $this->queueManager->createInstance($queue_name);
-    $q = $this->getDrupalService('queue')->get($queue_name);
-    $start = microtime(true);
-    $result = $this->clearQueue($worker, $q);
-    $time = microtime(true) - $start;
-    if (empty($result['error'])) {
-      $io->success(
-        sprintf(
-          $this->trans('commands.queue.run.success'),
-          $queue_name,
-          $result['count'],
-          $result['total'],
-          round($time, 2)
-        )
-      );
-    } else {
-      $io->error(
-        sprintf(
-          $this->trans('commands.queue.run.error'),
-          $queue_name,
-          $result['error']
-        )
-      );
+        return $result;
     }
-  }
-
-  /**
-   * @param \Drupal\Core\Queue\QueueWorkerManagerInterface $worker
-   * @param \Drupal\Core\Queue\Queue $q
-   * @return array
-   */
-  private function clearQueue($worker, $q) {
-    $result['count'] = 0;
-    $result['total'] = $q->numberOfItems();
-    while ($item = $q->claimItem()) {
-      try {
-        $worker->processItem($item->data);
-        $q->deleteItem($item);
-        $result['count']++;
-      }
-      catch (SuspendQueueException $e) {
-        $q->releaseItem($item);
-        $result['error'] = $e;
-      }
-
-    }
-
-    return $result;
-  }
-
 }
