10063f3b7e1a2d36e61cde381fc79dc8db5a4816
Saphyel <Saphyel@gmail.com>
2016-07-01 13:51:17 +0200

    Add behat:run command.

---
+++ src/Command/Behat/RunCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Console\Command\Behat\InitCommand.
+ */
+
+namespace Drupal\Console\Command\Behat;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+use Drupal\Console\Command\Shared\ContainerAwareCommandTrait;
+use Drupal\Console\Style\DrupalStyle;
+
+class RunCommand extends Command
+{
+    use ContainerAwareCommandTrait;
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+          ->setName('behat:run')
+          ->setDescription($this->trans('commands.behat.run.description'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new DrupalStyle($input, $output);
+
+        // @TODO: Add exception
+        $behatdir = $this->getDrupalContainer()->getParameter('behat.dir');
+        $dir = $this->get('site')->getRoot() . '/' . $behatdir;
+        // @TODO: Add arguments
+        $this->get('chain_queue')->addCommand(
+          'exec',
+          ['bin' => "cd $dir && vendor/bin/behat"]
+        );
+
+    }
+
+}
