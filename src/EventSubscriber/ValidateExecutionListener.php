12c44bffb578a107523f004c86c1d3f7b734a221
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-07-31 12:23:19 -0700

    [console] validate command execution.

---
+++ src/EventSubscriber/ValidateExecutionListener.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Console\EventSubscriber\ValidateDependenciesListener.
+ */
+
+namespace Drupal\Console\EventSubscriber;
+
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Console\Command\Command;
+use Drupal\Console\Style\DrupalStyle;
+
+class ValidateExecutionListener implements EventSubscriberInterface
+{
+    /**
+     * @param ConsoleCommandEvent $event
+     */
+    public function validateExecution(ConsoleCommandEvent $event)
+    {
+        /* @var Command $command */
+        $command = $event->getCommand();
+        /* @var DrupalStyle $io */
+        $io = $event->getOutput();
+
+        $application = $command->getApplication();
+        $configuration = $application->getConfig();
+        $mapping = $configuration->get('application.disabled.commands')?:[];
+
+        if (array_key_exists($command, $mapping)) {
+            $extra = $mapping[$command->getName()];
+            $io->commentBlock($extra);
+        }
+    }
+
+    /**
+     * @{@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [ConsoleEvents::COMMAND => 'validateExecution'];
+    }
+}
