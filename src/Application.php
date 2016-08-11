aa265198e93197897a858a0fd41f2d3306d4b5a4
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-08-10 18:41:03 -0700

    [console] Add application options.

--- src/Application.php
+++ src/Application.php
@@ -4,6 +4,7 @@ namespace Drupal\Console;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Class Application
@@ -25,6 +26,7 @@ class Application extends ConsoleApplication
     {
         parent::__construct($this::NAME, $this::VERSION);
         $this->container = $container;
+        $this->addOptions();
     }
 
     /**
@@ -37,6 +39,40 @@ class Application extends ConsoleApplication
         parent::doRun($input, $output);
     }
 
+    private function addOptions()
+    {
+        $this->getDefinition()->addOption(
+            new InputOption('--env', '-e', InputOption::VALUE_OPTIONAL, $this->trans('application.options.env'), $this->env)
+        );
+        $this->getDefinition()->addOption(
+            new InputOption('--root', null, InputOption::VALUE_OPTIONAL, $this->trans('application.options.root'))
+        );
+        $this->getDefinition()->addOption(
+            new InputOption('--no-debug', null, InputOption::VALUE_NONE, $this->trans('application.options.no-debug'))
+        );
+        $this->getDefinition()->addOption(
+            new InputOption('--learning', null, InputOption::VALUE_NONE, $this->trans('application.options.learning'))
+        );
+        $this->getDefinition()->addOption(
+            new InputOption('--generate-chain', '-c', InputOption::VALUE_NONE, $this->trans('application.options.generate-chain'))
+        );
+        $this->getDefinition()->addOption(
+            new InputOption('--generate-inline', '-i', InputOption::VALUE_NONE, $this->trans('application.options.generate-inline'))
+        );
+        $this->getDefinition()->addOption(
+            new InputOption('--generate-doc', '-d', InputOption::VALUE_NONE, $this->trans('application.options.generate-doc'))
+        );
+        $this->getDefinition()->addOption(
+            new InputOption('--target', '-t', InputOption::VALUE_OPTIONAL, $this->trans('application.options.target'))
+        );
+        $this->getDefinition()->addOption(
+            new InputOption('--uri', '-l', InputOption::VALUE_REQUIRED, $this->trans('application.options.uri'))
+        );
+        $this->getDefinition()->addOption(
+            new InputOption('--yes', '-y', InputOption::VALUE_NONE, $this->trans('application.options.yes'))
+        );
+    }
+
     private function registerCommands()
     {
         $consoleCommands = $this->container->getParameter('console.commands');
@@ -44,19 +80,14 @@ class Application extends ConsoleApplication
         var_export($consoleCommands);
 
         foreach ($consoleCommands as $name) {
-            $command = null;
-            if ($this->container->has($name)) {
-                $command = $this->container->get($name);
-            }
-
-            if (!$command) {
+            if (!$this->container->has($name)) {
                 continue;
             }
 
+            $command = $this->container->get($name);
             if (method_exists($command, 'setTranslator')) {
                 $command->setTranslator($this->container->get('console.translator_manager'));
             }
-
             $this->add($command);
         }
     }
