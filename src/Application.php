9987ff6155ba7cceb79fd4c6659b347bb025b4ef
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-08-10 18:21:21 -0700

    [console] Add register commands method.

--- src/Application.php
+++ src/Application.php
@@ -2,8 +2,8 @@
 
 namespace Drupal\Console;
 
-//use Symfony\Component\Console\Input\InputInterface;
-//use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Class Application
@@ -26,4 +26,40 @@ class Application extends ConsoleApplication
         parent::__construct($this::NAME, $this::VERSION);
         $this->container = $container;
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        $this->registerCommands();
+
+
+        parent::doRun($input, $output);
+    }
+
+    private function registerCommands()
+    {
+        $consoleCommands = $this->container->getParameter('console.commands');
+
+        var_export($consoleCommands);
+
+        foreach ($consoleCommands as $name) {
+            $command = null;
+            if ($this->container->has($name)) {
+                $command = $this->container->get($name);
+            }
+
+            if (!$command) {
+                continue;
+            }
+
+            if (method_exists($command, 'setTranslator')) {
+                $command->setTranslator($this->container->get('translator'));
+            }
+
+            $this->add($command);
+        }
+    }
+
 }
