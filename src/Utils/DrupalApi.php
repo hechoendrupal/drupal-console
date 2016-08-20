02d8f3fb49ce4b900cfe4f089edd832a546c12da
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-08-19 20:44:22 -0700

    [cache] Register service on drupal container. (#2596)
    * [cache] Register service on drupal container.
    
    * [cache] Register service on drupal container.
    
    * [cache] Register service on drupal container.

--- src/Utils/DrupalApi.php
+++ src/Utils/DrupalApi.php
@@ -7,22 +7,25 @@
 
 namespace Drupal\Console\Utils;
 
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Parser;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\Logger\LoggerChannelFactory;
 use Drupal\Core\Language\Language;
 use Drupal\Core\Language\LanguageManager;
 use Drupal\Core\Site\Settings;
-use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\Finder\Finder;
-use Symfony\Component\Yaml\Parser;
+use Drupal\Core\Cache\Cache;
 
 /**
  * Class DrupalHelper
  * @package Drupal\Console\Utils
  */
-class Site
+class DrupalApi
 {
     protected $appRoot;
+
+    private $caches = [];
 
     /**
      * ServerCommand constructor.
@@ -165,5 +168,134 @@ class Site
     public function getDrupalVersion()
     {
         return \Drupal::VERSION;
+    }
+
+    /**
+     * Auxiliary function to get all available drupal caches.
+     *
+     * @return array The all available drupal caches
+     */
+    public function getCaches()
+    {
+        if (!$this->caches) {
+            foreach (Cache::getBins() as $name => $bin) {
+                $this->caches[$name] = $bin;
+            }
+        }
+
+        return $this->caches;
+    }
+
+    /**
+     * Validate if a string is a valid cache.
+     *
+     * @param string $cache The cache name
+     *
+     * @return mixed The cache name if valid or FALSE if not valid
+     */
+    public function isValidCache($cache)
+    {
+        // Get the valid caches
+        $caches = $this->getCaches();
+        $cacheKeys = array_keys($caches);
+        $cacheKeys[] = 'all';
+
+        if (!in_array($cache, array_values($cacheKeys))) {
+            return false;
+        }
+
+        return $cache;
+    }
+
+    /* @todo fix */
+    /**
+     * Validate if module name exist.
+     *
+     * @param string $moduleName Module name
+     *
+     * @return string
+     */
+    public function validateModuleExist($moduleName)
+    {
+        if (!$this->isModule($moduleName)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'Module "%s" is not in your application. Try generate:module to create it.',
+                    $moduleName
+                )
+            );
+        }
+
+        return $moduleName;
+    }
+
+    /**
+     * Check if module name exist.
+     *
+     * @param string $moduleName Module name
+     *
+     * @return string
+     */
+    public function isModule($moduleName)
+    {
+        $modules = $this->getSite()->getModules(false, true, true, true, true, true);
+
+        return in_array($moduleName, $modules);
+    }
+
+    /**
+     * @param $moduleList
+     * @return array
+     */
+    public function getMissingModules($moduleList)
+    {
+        $modules = $this->getSite()->getModules(true, true, true, true, true, true);
+
+        return array_diff($moduleList, $modules);
+    }
+
+    /**
+     * Validate if module is installed.
+     *
+     * @param string $moduleName Module name
+     *
+     * @return string
+     */
+    public function validateModuleInstalled($moduleName)
+    {
+        if (!$this->isModuleInstalled($moduleName)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'Module "%s" is not installed. Try module:install to install it.',
+                    $moduleName
+                )
+            );
+        }
+
+        return $moduleName;
+    }
+
+    /**
+     * Check if module is installed.
+     *
+     * @param  $moduleName
+     * @return bool
+     */
+    public function isModuleInstalled($moduleName)
+    {
+        $modules = $this->getSite()->getModules(false, true, false, true, true, true);
+
+        return in_array($moduleName, $modules);
+    }
+
+    /**
+     * @param $moduleList
+     * @return array
+     */
+    public function getUninstalledModules($moduleList)
+    {
+        $modules = $this->getSite()->getModules(true, true, false, true, true, true);
+
+        return array_diff($moduleList, $modules);
     }
 }
