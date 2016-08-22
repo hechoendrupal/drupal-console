ac41aea26cdca4a90cc7afbce49376473722b5c5
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-08-22 16:08:02 -0700

    [#2593] Update cron:release command.

--- src/Command/Cron/ReleaseCommand.php
+++ src/Command/Cron/ReleaseCommand.php
@@ -11,12 +11,33 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
-use Drupal\Console\Command\Shared\ContainerAwareCommandTrait;
+use Drupal\Console\Command\Shared\CommandTrait;
 use Drupal\Console\Style\DrupalStyle;
 
 class ReleaseCommand extends Command
 {
-    use ContainerAwareCommandTrait;
+    use CommandTrait;
+
+    /** @var \Drupal\Core\Lock\LockBackendInterface  */
+    protected $lock;
+
+    /** @var  \Drupal\Console\Utils\ChainQueue */
+    protected $chainQueue;
+
+    /**
+     * ReleaseCommand constructor.
+     * @param \Drupal\Core\Lock\LockBackendInterface    $lock
+     * @param \Drupal\Console\Utils\ChainQueue          $chainQueue
+     */
+    public function __construct(
+        \Drupal\Core\Lock\LockBackendInterface $lock,
+        \Drupal\Console\Utils\ChainQueue $chainQueue
+    ) {
+        $this->lock = $lock;
+        $this->chainQueue = $chainQueue;
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this
@@ -28,16 +49,17 @@ class ReleaseCommand extends Command
     {
         $io = new DrupalStyle($input, $output);
 
-        $lock = $this->getDrupalService('lock');
-
         try {
-            $lock->release('cron');
+            $this->lock->release('cron');
 
             $io->info($this->trans('commands.cron.release.messages.released'));
         } catch (Exception $e) {
             $io->error($e->getMessage());
+
+            return 1;
         }
 
-        $this->get('chain_queue')->addCommand('cache:rebuild', ['cache' => 'all']);
+        $this->chainQueue->addCommand('cache:rebuild', ['cache' => 'all']);
+        return 0;
     }
 }
