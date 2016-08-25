6ad69382e518d702c8a9fb3f749984ae979759c8
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-08-25 00:04:44 -0700

    [#2593] Update exec command.

--- src/Utils/ShellProcess.php
+++ src/Utils/ShellProcess.php
@@ -1,8 +1,11 @@
 <?php
 namespace Drupal\Console\Utils;
 
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Drupal\Console\Style\DrupalStyle;
 
 /**
  * Class ShellProcess
@@ -20,28 +23,45 @@ class ShellProcess
     private $process;
 
     /**
+     * @var DrupalStyle
+     */
+    private $io;
+
+    /**
      * Process constructor.
      * @param string $appRoot
      */
     public function __construct($appRoot)
     {
         $this->appRoot = $appRoot;
+
+        $output = new ConsoleOutput();
+        $input = new ArrayInput([]);
+        $this->io = new DrupalStyle($input, $output);
     }
 
     /**
-     * @param $command string
+     * @param string $command
+     * @param string $workingDirectory
      *
      * @throws ProcessFailedException
      *
      * @return Process
      */
-    public function exec($command)
+    public function exec($command, $workingDirectory=null)
     {
+        if (!$workingDirectory) {
+            $workingDirectory = $this->appRoot;
+        }
         $this->process = new Process($command);
-        $this->process->setWorkingDirectory($this->appRoot);
+        $this->process->setWorkingDirectory($workingDirectory);
         $this->process->enableOutput();
         $this->process->setTimeout(null);
-        $this->process->start();
+        $this->process->run(
+            function ($type, $buffer) {
+                $this->io->write($buffer);
+            }
+        );
 
         if (!$this->process->isSuccessful()) {
             throw new ProcessFailedException($this->process);
