5927a23216c08fe5a7bda944db34d547bdde5f93
enzo - Eduardo Garcia <enzo@anexusit.com>
2016-08-26 10:12:21 -0600

    Register hyaml commands in container 2

--- src/Command/Yaml/DiffCommand.php
+++ src/Command/Yaml/DiffCommand.php
@@ -129,7 +129,10 @@ class DiffCommand extends Command
         }
 
         $statistics = ['total' => 0, 'equal'=> 0 , 'diff' => 0];
+        print_r($yamlLeftParsed);
+        print_r($yamlRightParsed);
         $diff = $this->nestedArray->arrayDiff($yamlLeftParsed, $yamlRightParsed, $negate, $statistics);
+        print_r($diff);
 
         if ($stats) {
             $io->info(
