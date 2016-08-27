ae101cbb5a323efeb9a07993e1ce0f00993faf86
leandro <leandro@leandro.org>
2016-08-27 21:29:23 +0200

    reconverting Locale command

--- src/Command/Shared/LocaleTrait.php
+++ src/Command/Shared/LocaleTrait.php
@@ -11,7 +11,7 @@ trait LocaleTrait
 {
     protected function getLanguages()
     {
-        $drupal = $this->getApplication()->getDrupalApi();
+        $drupal = $this->drupalApi;
         $languages = $drupal->getStandardLanguages();
 
         return $languages;
