1c11444467ef62ed6c0c02698b81515590c1c30f
enzo - Eduardo Garcia <enzo@anexusit.com>
2016-09-15 14:36:56 -0600

    Register generate update as service

--- src/Generator/UpdateGenerator.php
+++ src/Generator/UpdateGenerator.php
@@ -7,8 +7,23 @@
 
 namespace Drupal\Console\Generator;
 
+use Drupal\Console\Extension\Manager;
+
 class UpdateGenerator extends Generator
 {
+    /** @var Manager  */
+    protected $extensionManager;
+
+    /**
+     * AuthenticationProviderGenerator constructor.
+     * @param Manager $extensionManager
+     */
+    public function __construct(
+        Manager $extensionManager
+    ) {
+        $this->extensionManager = $extensionManager;
+    }
+
     /**
      * Generator Update N function.
      *
@@ -22,7 +37,7 @@ class UpdateGenerator extends Generator
           'update_number' => $update_number,
         ];
 
-        $module_path =  $this->getSite()->getModulePath($module);
+        $module_path =  $this->extensionManager->getModule($module)->getPath();
 
         $this->renderFile(
             'module/src/update.php.twig',
