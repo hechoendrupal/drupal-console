0b03232e005fb88c60317eb247923ee0d253cebb
enzo - Eduardo Garcia <enzo@anexusit.com>
2016-09-18 10:28:39 -0600

    Register generate plugin image formatter as service

--- src/Generator/PluginImageFormatterGenerator.php
+++ src/Generator/PluginImageFormatterGenerator.php
@@ -7,8 +7,21 @@
 
 namespace Drupal\Console\Generator;
 
+use Drupal\Console\Extension\Manager;
+
 class PluginImageFormatterGenerator extends Generator
 {
+    /**
+     * PluginImageFormatterGenerator constructor.
+     * @param Manager $extensionManager
+     */
+    public function __construct(
+        Manager $extensionManager
+    ) {
+        $this->extensionManager = $extensionManager;
+    }
+
+
     /**
      * Generator Plugin Image Formatter.
      *
@@ -28,7 +41,7 @@ class PluginImageFormatterGenerator extends Generator
 
         $this->renderFile(
             'module/src/Plugin/Field/FieldFormatter/imageformatter.php.twig',
-            $this->getSite()->getPluginPath($module, 'Field/FieldFormatter') . '/' . $class_name . '.php',
+            $this->extensionManager->getPluginPath($module, 'Field/FieldFormatter') . '/' . $class_name . '.php',
             $parameters
         );
     }
