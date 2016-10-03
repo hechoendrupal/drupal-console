9b0de9e1e7fe68c9df2aefa42d2c31f4e2110952
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-10-03 08:22:50 -0400

    [console] Tag 1.0.0-rc3 version. (#2753)

--- src/Application.php
+++ src/Application.php
@@ -22,7 +22,7 @@ class Application extends ConsoleApplication
     /**
      * @var string
      */
-    const VERSION = '1.0.0-rc2';
+    const VERSION = '1.0.0-rc3';
 
     public function __construct(ContainerInterface $container)
     {
