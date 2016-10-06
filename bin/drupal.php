204e623dafe61df778a29bf6e2d06db8002f223b
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-10-05 17:54:53 -0700

    [console] Improve error messages.

--- bin/drupal.php
+++ bin/drupal.php
@@ -4,9 +4,7 @@ use Drupal\Console\Application;
 use Drupal\Console\Bootstrap\Drupal;
 
 set_time_limit(0);
-$consoleRoot = realpath(__DIR__.'/../') . '/';
 $appRoot = getcwd() . '/';
-$siteRoot = realpath(__DIR__.'/../../../../') . '/';
 $root = $appRoot;
 
 $autoLoadFile = $appRoot.'/autoload.php';
@@ -15,10 +13,10 @@ if (file_exists($autoLoadFile)) {
     $autoload = include_once $autoLoadFile;
 } else {
     echo PHP_EOL .
-        ' Something goes wrong with your package.'.PHP_EOL.
-        ' Try downloading again.'. PHP_EOL .
-        ' Executing:'. PHP_EOL .
-        ' composer require drupal/console:~1.0 --prefer-dist --optimize-autoloader'. PHP_EOL;
+        ' DrupalConsole must be executed within a Drupal Site.'.PHP_EOL.
+        ' Try changing to a Drupal site directory and download it by executing:'. PHP_EOL .
+        ' composer require drupal/console:~1.0 --prefer-dist --optimize-autoloader'. PHP_EOL .
+        PHP_EOL;
 
     exit(1);
 }
@@ -28,10 +26,10 @@ if (!file_exists($appRoot.'composer.json')) {
 }
 
 if (!file_exists($root.'composer.json')) {
-    echo 'No composer.json file found at:' . PHP_EOL .
-        $root . PHP_EOL .
-        'you should try run this command,' . PHP_EOL .
-        'from project root directory.' . PHP_EOL;
+    echo ' No composer.json file found at:' . PHP_EOL .
+         ' '. $root . PHP_EOL .
+         ' you should try run this command,' . PHP_EOL .
+         ' from the Drupal root directory.' . PHP_EOL;
 
     exit(1);
 }
@@ -40,8 +38,9 @@ $drupal = new Drupal($autoload, $root, $appRoot);
 $container = $drupal->boot();
 
 if (!$container) {
-    echo 'In order to list all of the available commands,' . PHP_EOL .
-         'you should install drupal first.' . PHP_EOL;
+    echo ' In order to list all of the available commands you should try: ' . PHP_EOL .
+         ' Copy config files: drupal init ' . PHP_EOL .
+         ' Install Drupal site: drupal site:install ' . PHP_EOL;
 
     exit(1);
 }
