ac93d715c8225fa152db15c14bbb01de000a2774
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-10-14 17:51:05 -0700

    [taxonomy:term:delete] Minimal command fixes. (#2811)

--- src/Command/Taxonomy/DeleteTermCommand.php
+++ src/Command/Taxonomy/DeleteTermCommand.php
@@ -5,40 +5,78 @@ namespace Drupal\Console\Command\Taxonomy;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
-use Drupal\Console\Command\Shared\ContainerAwareCommandTrait;
-use Drupal\Console\Style\DrupalStyle;
 use Symfony\Component\Console\Input\InputArgument;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\Console\Command\Shared\CommandTrait;
+use Drupal\Console\Style\DrupalStyle;
 
 /**
  * Class DeleteTermCommand.
  *
  * @package Drupal\eco_migrate
  */
-class DeleteTermCommand extends Command {
+class DeleteTermCommand extends Command
+{
+    use CommandTrait;
 
-  use ContainerAwareCommandTrait;
-
-  use TermDeletionTrait;
-
-  /**
+    /**
    * {@inheritdoc}
    */
-  protected function configure() {
-    $this
-      ->setName('taxonomy:term:delete')
-      ->setDescription($this->trans('commands.taxonomy.term.delete.description'))
-      ->addArgument('vid',InputArgument::REQUIRED);
-  }
+    protected function configure()
+    {
+        $this
+            ->setName('taxonomy:term:delete')
+            ->setDescription($this->trans('commands.taxonomy.term.delete.description'))
+            ->addArgument('vid', InputArgument::REQUIRED);
+    }
 
-  /**
+    /**
    * {@inheritdoc}
    */
-  protected function execute(InputInterface $input, OutputInterface $output) {
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $vid = $input->getArgument('vid');
+        $io = new DrupalStyle($input, $output);
 
-    $io = new DrupalStyle($input, $output);
+        $this->deleteExistingTerms($vid, $io);
 
-    $this->deleteExistingTerms($input->getArgument('vid'), $io);
+        return 0;
+    }
 
-  }
+    /**
+   * Destroy all existing terms before import
+   * @param $vid
+   * @param $io
+   */
+    private function deleteExistingTerms($vid = null, DrupalStyle $io)
+    {
+        //Load the vid
+        $vocabularies = Vocabulary::loadMultiple();
 
+        if ($vid !== 'all') {
+            $vid = [$vid];
+        } else {
+            $vid = array_keys($vocabularies);
+        }
+
+        foreach ($vid as $item) {
+            if (!isset($vocabularies[$item])) {
+                $io->error("Invalid vid: {$item}.");
+            }
+            $vocabulary = $vocabularies[$item];
+            $terms = \Drupal::getContainer()
+              ->get('entity.manager')
+              ->getStorage('taxonomy_term')
+              ->loadTree($vocabulary->id());
+
+            foreach ($terms as $term) {
+                $treal = Term::load($term->tid);
+                if ($treal !== null) {
+                    $io->info("Deleting '{$term->name}' and all translations.");
+                    $treal->delete();
+                }
+            }
+        }
+    }
 }
