2c16be7eaac0ad51236a134032cabe922b8942f0
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-11-02 11:39:30 -0700

    Tag new release 1.0.0 rc7 (#2873)
    * [console] Tag 1.0.0-rc7 release.
    
    * [console] Tag 1.0.0-rc7 release.

--- src/Application.php
+++ src/Application.php
@@ -22,7 +22,7 @@ class Application extends ConsoleApplication
     /**
      * @var string
      */
-    const VERSION = '1.0.0-rc6';
+    const VERSION = '1.0.0-rc7';
 
     public function __construct(ContainerInterface $container)
     {
