b94ab35254d48c912fe37f190fad42f40e11243c
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-11-05 00:00:11 -0700

    Tag new release 1.0.0 rc8 (#2886)
    * [console] Tag 1.0.0-rc8 release.
    
    * [console] Update dependencies.

--- src/Application.php
+++ src/Application.php
@@ -22,7 +22,7 @@ class Application extends ConsoleApplication
     /**
      * @var string
      */
-    const VERSION = '1.0.0-rc7';
+    const VERSION = '1.0.0-rc8';
 
     public function __construct(ContainerInterface $container)
     {
