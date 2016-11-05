a5e08f3a4276f6c6a0de6ab743187376f8a7f3ba
David Flores <dmousex@gmail.com>
2016-11-05 01:34:00 -0500

    Fix service command test (#2885)
    * Fix ServiceCommandTest
    
    * Added StringConverter, formGenerator, routeProvider

--- Test/Builders/a.php
+++ Test/Builders/a.php
@@ -6,6 +6,12 @@ use Drupal\Console\Extension\Manager;
 use Drupal\Console\Generator\AuthenticationProviderGenerator;
 use Drupal\Console\Generator\CommandGenerator;
 use Drupal\Console\Generator\EntityBundleGenerator;
+use Drupal\Console\Generator\FormGenerator;
+use Drupal\Console\Generator\ServiceGenerator;
+use Drupal\Console\Utils\ChainQueue;
+use Drupal\Console\Utils\StringConverter;
+use Drupal\Core\Render\ElementInfoManager;
+use Drupal\Core\Routing\RouteProvider;
 use Prophecy\Prophet;
 
 class a
@@ -37,9 +43,60 @@ class a
         return self::prophet()->prophesize(AuthenticationProviderGenerator::class);
     }
 
+    /**
+     * @return \Prophecy\Prophecy\ObjectProphecy
+     */
     public static function commandGenerator()
     {
         return self::prophet()->prophesize(CommandGenerator::class);
+    }
+
+    /**
+     * @return \Prophecy\Prophecy\ObjectProphecy
+     */
+    public static function stringConverter()
+    {
+        return self::prophet()->prophesize(StringConverter::class);
+    }
+
+    /**
+     * @return \Prophecy\Prophecy\ObjectProphecy
+     */
+    public static function formGenerator()
+    {
+        return self::prophet()->prophesize(FormGenerator::class);
+    }
+
+    /**
+     * @return \Prophecy\Prophecy\ObjectProphecy
+     */
+    public static function routeProvider()
+    {
+        return self::prophet()->prophesize(RouteProvider::class);
+    }
+
+    /**
+     * @return \Prophecy\Prophecy\ObjectProphecy
+     */
+    public static function elementInfoManager()
+    {
+        return self::prophet()->prophesize(ElementInfoManager::class);
+    }
+
+    /**
+     * @return \Prophecy\Prophecy\ObjectProphecy
+     */
+    public static function chainQueue()
+    {
+        return self::prophet()->prophesize(ChainQueue::class);
+    }
+
+    /**
+     * @return \Prophecy\Prophecy\ObjectProphecy
+     */
+    public static function serviceGenerator()
+    {
+        return self::prophet()->prophesize(ServiceGenerator::class);
     }
 
     /**
