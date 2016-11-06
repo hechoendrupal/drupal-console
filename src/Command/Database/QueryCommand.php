aba09b179fa1f58d9d3594bdc7357fff321457eb
leandro <leandro@leandro.org>
2016-11-06 16:32:49 +0100

    added several options

--- src/Command/Database/QueryCommand.php
+++ src/Command/Database/QueryCommand.php
@@ -3,6 +3,11 @@
 /**
  * @file
  * Contains \Drupal\Console\Command\Database\QueryCommand.
+ *
+ *
+ * @TODO:
+ * 		- mysql -H option for html
+ * 		- mysql -X option for xml
  */
 
 namespace Drupal\Console\Command\Database;
@@ -41,6 +46,14 @@ class QueryCommand extends Command
                 $this->trans('commands.database.query.arguments.database'),
                 'default'
             )
+            ->addOption('quick', '', InputOption::VALUE_NONE, $this->trans('commands.database.query.options.quick'))
+            ->addOption('debug', '', InputOption::VALUE_NONE, $this->trans('commands.database.query.options.debug'))
+            ->addOption('html', '', InputOption::VALUE_NONE, $this->trans('commands.database.query.options.html'))
+            ->addOption('xml', '', InputOption::VALUE_NONE, $this->trans('commands.database.query.options.xml'))
+            ->addOption('raw', '', InputOption::VALUE_NONE, $this->trans('commands.database.query.options.raw'))
+            ->addOption('vertical', '', InputOption::VALUE_NONE, $this->trans('commands.database.query.options.vertical'))
+            ->addOption('batch', '', InputOption::VALUE_NONE, $this->trans('commands.database.query.options.batch'))
+
             ->setHelp($this->trans('commands.database.query.help'));
     }
 
@@ -69,6 +82,35 @@ class QueryCommand extends Command
 
 				$args = explode(' ', $connection);
 				$args[] = sprintf('--execute=%s', $query);
+
+				$opts = ["quick", "debug", "html", "xml", "raw", "vertical", "batch"];
+				array_walk($opts, function($opt) use ($input, &$args) {
+					if ($input->getOption($opt)) {
+						switch ($opt) {
+							case "quick":
+								$args[] = "--quick";
+								break;
+							case "debug":
+								$args[] = "-T";
+								break;
+							case "html":
+								$args[] = "-H";
+								break;
+							case "xml":
+								$args[] = "-X";
+								break;
+							case "raw":
+								$args[] = "--raw";
+								break;
+							case "vertical":
+								$args[] = "-E";
+								break;
+							case "batch":
+								$args[] = "--batch";
+								break;
+						}
+					}
+				});
 
 				if ($learning) {
             $io->commentBlock(
