fc48f8ebd90e286c98671fb635b31d501e76ae74
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2016-11-08 23:02:40 -0500

    [permission:debug] List all permissions and filter by user role

--- src/Command/PermissionDebugCommand.php
+++ src/Command/PermissionDebugCommand.php
@@ -11,7 +11,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Yaml\Yaml;
 use Drupal\Console\Command\Shared\ContainerAwareCommandTrait;
 use Drupal\Console\Style\DrupalStyle;
 
@@ -28,17 +27,12 @@ class PermissionDebugCommand extends Command
     protected function configure()
     {
         $this->setName('permission:debug')
-            ->setDescription($this->trans('commands.permission.debug.description'))
-            ->setHelp($this->trans('commands.permission.debug.help'))
+            ->setDescription($this->trans('commands.plugin.debug.description'))
+            ->setHelp($this->trans('commands.plugin.debug.help'))
             ->addArgument(
-                'type',
+                'role',
                 InputArgument::OPTIONAL,
-                $this->trans('commands.permission.debug.arguments.type')
-            )
-            ->addArgument(
-                'id',
-                InputArgument::OPTIONAL,
-                $this->trans('commands.permission.debug.arguments.id')
+                $this->trans('commands.plugin.debug.arguments.type')
             );
     }
 
@@ -49,27 +43,20 @@ class PermissionDebugCommand extends Command
     {
         $io = new DrupalStyle($input, $output);
 
-        $permissionType = $input->getArgument('type');
-        $permissionId = $input->getArgument('id');
-
-        // No permission type specified, show a list of permission types.
-        if (!$permissionType) {
+        $role = $input->getArgument('role');
+        // No role specified, show a list of ALL permissions.
+        if (!$role) {
             $tableHeader = [
                 $this->trans('commands.permission.debug.table-headers.permission-type-name'),
                 $this->trans('commands.permission.debug.table-headers.permission-type-class')
             ];
             $tableRows = [];
-            $serviceDefinitions = $this->container
-                ->getParameter('console.service_definitions');
-
-            foreach ($serviceDefinitions as $serviceId => $serviceDefinition) {
-                if (strpos($serviceId, 'permission.manager.') === 0) {
-                    $serviceName = substr($serviceId, 15);
-                    $tableRows[$serviceName] = [
-                        $serviceName,
-                        $serviceDefinition->getClass()
-                    ];
-                }
+            $permissions = \Drupal::service('user.permissions')->getPermissions();
+            foreach ($permissions as $permission_name => $permission) {
+                $tableRows[$permission_name] = [
+                    $permission_name,
+                    $permission['title']->__toString()
+                ];
             }
 
             ksort($tableRows);
@@ -77,54 +64,26 @@ class PermissionDebugCommand extends Command
 
             return true;
         }
-
-        $service = $this->container->get('permission.manager.' . $permissionType);
-        if (!$service) {
-            $io->error(
-                sprintf(
-                    $this->trans('commands.permission.debug.errors.permission-type-not-found'),
-                    $permissionType
-                )
-            );
-            return false;
-        }
-
-        // Valid permission type specified, no ID specified, show list of instances.
-        if (!$permissionId) {
+        else {
             $tableHeader = [
-                $this->trans('commands.permission.debug.table-headers.permission-id'),
-                $this->trans('commands.permission.debug.table-headers.permission-class')
+                $this->trans('commands.plugin.debug.table-headers.plugin-type-name'),
+                $this->trans('commands.plugin.debug.table-headers.plugin-type-class')
             ];
             $tableRows = [];
-            foreach ($service->getDefinitions() as $definition) {
-                $permissionId = $definition['id'];
-                $className = $definition['class'];
-                $tableRows[$permissionId] = [$permissionId, $className];
-            }
-            ksort($tableRows);
-            $io->table($tableHeader, array_values($tableRows));
-            return true;
+          $permissions = \Drupal::service('user.permissions')->getPermissions();
+          $roles = user_roles();
+          $user_permission = $roles[$role]->getPermissions();
+          foreach ($permissions as $permission_name => $permission) {
+              if (in_array($permission_name, $user_permission)) {
+                  $tableRows[$permission_name] = [
+                      $permission_name,
+                      $permission['title']->__toString()
+                  ];
+              }
+          }
+          ksort($tableRows);
+          $io->table($tableHeader, array_values($tableRows));
+          return true;
         }
-
-        // Valid permission type specified, ID specified, show the definition.
-        $definition = $service->getDefinition($permissionId);
-        $tableHeader = [
-            $this->trans('commands.permission.debug.table-headers.definition-key'),
-            $this->trans('commands.permission.debug.table-headers.definition-value')
-        ];
-        $tableRows = [];
-        foreach ($definition as $key => $value) {
-            if (is_object($value) && method_exists($value, '__toString')) {
-                $value = (string) $value;
-            } elseif (is_array($value) || is_object($value)) {
-                $value = Yaml::dump($value);
-            } elseif (is_bool($value)) {
-                $value = ($value) ? 'TRUE' : 'FALSE';
-            }
-            $tableRows[$key] = [$key, $value];
-        }
-        ksort($tableRows);
-        $io->table($tableHeader, array_values($tableRows));
-        return true;
     }
 }
