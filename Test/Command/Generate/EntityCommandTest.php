8c55f957cb2b39fe3ce89c59c7212c076351e27a
YoSaucedo <yaseuso@gmail.com>
2016-11-11 09:59:57 -0600

    Fixing the builder and the EntityCommandTest

--- Test/Command/Generate/EntityCommandTest.php
+++ Test/Command/Generate/EntityCommandTest.php
@@ -8,6 +8,9 @@ namespace Drupal\Console\Test\Command\Generate;
 
 use Drupal\Console\Command\Generate\EntityConfigCommand;
 use Symfony\Component\Console\Tester\CommandTester;
+use Drupal\Console\Utils\StringConverter;
+use Drupal\Console\Test\Builders\a as an;
+use Drupal\Console\Utils\Validator;
 use Drupal\Console\Test\DataProvider\EntityDataProviderTrait;
 
 class EntityCommandTest extends GenerateCommandTest
@@ -28,12 +31,18 @@ class EntityCommandTest extends GenerateCommandTest
         $module,
         $entity_class,
         $entity_name,
-        $label
+        $label,
+        $base_path
     ) {
-        $command = new EntityConfigCommand($this->getHelperSet());
-        $command->setHelperSet($this->getHelperSet());
-        $command->setGenerator($this->getGenerator());
-
+      $generator = an::entityConfigGenerator();
+      $manager = an::extensionManager();
+      $command = new EntityConfigCommand(
+        $manager,
+        $generator->reveal(),
+        new Validator($manager),
+        new StringConverter(),
+        new StringConverter()
+      );
         $commandTester = new CommandTester($command);
 
         $code = $commandTester->execute(
@@ -41,20 +50,16 @@ class EntityCommandTest extends GenerateCommandTest
               '--module'         => $module,
               '--entity-class'   => $entity_class,
               '--entity-name'    => $entity_name,
-              '--label'          => $label
+              '--label'          => $label,
+              '--base-path'      => $base_path
             ],
             ['interactive' => false]
         );
 
+        $generator
+            ->generate($module, $entity_name, $entity_class, $label, $base_path, $bundle_of=false)
+            ->shouldHaveBeenCalled()
+        ;
         $this->assertEquals(0, $code);
-    }
-
-    private function getGenerator()
-    {
-        return $this
-            ->getMockBuilder('Drupal\Console\Generator\EntityConfigGenerator')
-            ->disableOriginalConstructor()
-            ->setMethods(['generate'])
-            ->getMock();
     }
 }
