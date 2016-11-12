8c06214bb9d8b2efaee09806ccb36f5ac713dad6
YoSaucedo <yaseuso@gmail.com>
2016-11-12 02:32:49 -0600

    Add PermissionGenerator to builder

--- Test/Builders/a.php
+++ Test/Builders/a.php
@@ -3,16 +3,18 @@
 namespace Drupal\Console\Test\Builders;
 
 use Drupal\Console\Extension\Manager;
-use Drupal\Console\Generator\AuthenticationProviderGenerator;
-use Drupal\Console\Generator\CommandGenerator;
-use Drupal\Console\Generator\EntityBundleGenerator;
-use Drupal\Console\Generator\FormGenerator;
-use Drupal\Console\Generator\ServiceGenerator;
 use Drupal\Console\Utils\ChainQueue;
 use Drupal\Console\Utils\StringConverter;
 use Drupal\Core\Render\ElementInfoManager;
 use Drupal\Core\Routing\RouteProvider;
 use Prophecy\Prophet;
+
+use Drupal\Console\Generator\AuthenticationProviderGenerator;
+use Drupal\Console\Generator\CommandGenerator;
+use Drupal\Console\Generator\EntityBundleGenerator;
+use Drupal\Console\Generator\FormGenerator;
+use Drupal\Console\Generator\ServiceGenerator;
+use Drupal\Console\Generator\PermissionGenerator;
 
 class a
 {
@@ -34,6 +36,15 @@ class a
     {
         return self::prophet()->prophesize(EntityBundleGenerator::class);
     }
+
+    /**
+     * @return \Prophecy\Prophecy\ObjectProphecy
+     */
+    public static function permissionGenerator()
+    {
+        return self::prophet()->prophesize(PermissionGenerator::class);
+    }
+
 
     /**
      * @return \Prophecy\Prophecy\ObjectProphecy
