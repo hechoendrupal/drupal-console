dce10186cc82a8991f71be854c79cf71e0be8131
YoSaucedo <yaseuso@gmail.com>
2016-11-12 02:14:03 -0600

    Add EntityCommandGenerator to Builder

--- Test/Builders/a.php
+++ Test/Builders/a.php
@@ -1,11 +1,11 @@
 <?php
-
 namespace Drupal\Console\Test\Builders;
 
 use Drupal\Console\Extension\Manager;
 use Drupal\Console\Generator\AuthenticationProviderGenerator;
 use Drupal\Console\Generator\CommandGenerator;
 use Drupal\Console\Generator\EntityBundleGenerator;
+use Drupal\Console\Generator\EntityConfigGenerator;
 use Drupal\Console\Generator\FormGenerator;
 use Drupal\Console\Generator\ServiceGenerator;
 use Drupal\Console\Utils\ChainQueue;
@@ -42,6 +42,15 @@ class a
     {
         return self::prophet()->prophesize(AuthenticationProviderGenerator::class);
     }
+
+    /**
+     * @return \Prophecy\Prophecy\ObjectProphecy
+     */
+    public static function entityConfigGenerator()
+    {
+        return self::prophet()->prophesize(EntityConfigGenerator::class);
+    }
+
 
     /**
      * @return \Prophecy\Prophecy\ObjectProphecy
