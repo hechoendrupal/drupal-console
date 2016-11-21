07f3d0b839b1ebaa29416436918c47beb3e36c6d
Luis Montealegre <montealegreluis@gmail.com>
2016-11-20 21:54:36 -0600

    Refactor ConfirmGenerationTest.

--- Test/Command/Generate/Questions/ConfirmGenerationTest.php
+++ Test/Command/Generate/Questions/ConfirmGenerationTest.php
@@ -17,39 +17,73 @@ class ConfirmGenerationTest extends TestCase
     /** @test */
     function it_confirms_positively_when_default_value_is_set_to_true()
     {
-        $confirm = new ConfirmGeneration(
-            $this->prophesize(DrupalStyle::class)->reveal(),
-            $this->prophesize(TranslatorManager::class)->reveal()
-        );
-        $this->assertTrue($confirm->confirm(true));
+        $this->assertTrue($this->confirm->confirm(true));
     }
 
     /** @test */
     function it_confirms_when_user_answer_is_positive()
     {
-        $question = $this->prophesize(DrupalStyle::class);
-        $question->confirm(Argument::any(), true)->willReturn(true);
+        // Given
+        $this->userConfirmsGeneration();
 
-        $confirm = new ConfirmGeneration(
-            $question->reveal(),
-            $this->prophesize(TranslatorManager::class)->reveal()
-        );
-
-        $this->assertTrue($confirm->confirm());
+        $this->assertTrue($this->confirm->confirm());
     }
 
     /** @test */
     function it_shows_a_warning_if_answer_is_negative()
     {
-        $question = $this->prophesize(DrupalStyle::class);
-        $question->confirm(Argument::any(), true)->willReturn(false);
-        $question->warning(Argument::any())->shouldBeCalled();
+        // Given
+        $this->userCancelsGeneration();
+        // Then
+        $this->userGetsCancellationWarning();
 
-        $confirm = new ConfirmGeneration(
-            $question->reveal(),
-            $this->prophesize(TranslatorManager::class)->reveal()
-        );
-
-        $this->assertFalse($confirm->confirm());
+        $this->assertFalse($this->confirm->confirm());
     }
+
+    /** @before */
+    public function configure()
+    {
+        $this->configureCollaborators();
+        $this->configureSUT();
+    }
+
+    private function configureCollaborators()
+    {
+        $this->style = $this->prophesize(DrupalStyle::class);
+        $this->translator = $this->prophesize(TranslatorManager::class);
+    }
+
+    private function configureSUT()
+    {
+        $this->confirm = new ConfirmGeneration(
+            $this->style->reveal(),
+            $this->translator->reveal()
+        );
+    }
+
+    private function userConfirmsGeneration()
+    {
+        $confirm = true;
+        $this->style->confirm(Argument::any(), true)->willReturn($confirm);
+    }
+
+    private function userCancelsGeneration()
+    {
+        $confirm = false;
+        $this->style->confirm(Argument::any(), true)->willReturn($confirm);
+    }
+
+    private function userGetsCancellationWarning()
+    {
+        $this->style->warning(Argument::any())->shouldBeCalled();
+    }
+
+    /** @var ConfirmGeneration */
+    private $confirm;
+
+    /** @var DrupalStyle */
+    private $style;
+
+    /** @var TranslatorManager */
+    private $translator;
 }
