cc2920f35784995d5511c5ca65aef802d131b0d5
Tom Whiston <tom.whiston@pwc-digital.ch>
2016-12-03 13:58:34 +0100

    config schema validation and debugging commands

---
+++ src/Command/Config/PrintConfigValidationTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Console\Command\Config\PrintConfigValidationTrait.
+ */
+
+namespace Drupal\Console\Command\Config;
+
+use Drupal\Console\Style\DrupalStyle;
+
+
+trait PrintConfigValidationTrait {
+
+  protected function printResults($valid, DrupalStyle $io){
+
+    if($valid === TRUE){
+      $io->info($this->trans('commands.config.validate.messages.success'));
+      return 0;
+    }
+
+    foreach ($valid as $key => $error) {
+      $io->warning($key . ': ' . $error);
+    }
+    return 1;
+  }
+
+}
