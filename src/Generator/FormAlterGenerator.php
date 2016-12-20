2293b9bde48cc8f0787ffe51b7b2ea13cc6b0164
Nick Wilde <design@briarmoon.ca>
2016-12-20 12:31:42 -0800

    Update FormAlterGenerator.php

--- src/Generator/FormAlterGenerator.php
+++ src/Generator/FormAlterGenerator.php
@@ -12,8 +12,8 @@ use Drupal\Console\Extension\Manager;
 class FormAlterGenerator extends Generator
 {
     /**
- * @var Manager  
-*/
+     * @var Manager  
+     */
     protected $extensionManager;
 
     /**
