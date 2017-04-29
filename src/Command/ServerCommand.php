afe141376e656e9f9ae42768df30a07fe11f3be1
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2017-04-29 16:20:47 -0400

    Fix server command (#3289)
    * [server] Add docblock to variables.
    
    * [server] Load router.php file from proper route.

--- src/Command/ServerCommand.php
+++ src/Command/ServerCommand.php
@@ -15,6 +15,7 @@ use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Console\Command\Command;
 use Drupal\Console\Core\Command\Shared\CommandTrait;
 use Drupal\Console\Core\Style\DrupalStyle;
+use \Drupal\Console\Core\Utils\ConfigurationManager;
 
 /**
  * Class ServerCommand
@@ -25,8 +26,14 @@ class ServerCommand extends Command
 {
     use CommandTrait;
 
+    /**
+     * @var string
+     */
     protected $appRoot;
 
+    /**
+     * @var ConfigurationManager
+     */
     protected $configurationManager;
 
     /**
@@ -107,24 +114,27 @@ class ServerCommand extends Command
     /**
      * @return null|string
      */
-    private function getRouterPath()
-    {
-        $router = sprintf(
-            '%s/.console/router.php',
-            $this->configurationManager->getHomeDirectory()
-        );
+    private function getRouterPath() {
+        $routerPath = [
+            sprintf(
+                '%s/.console/router.php',
+                $this->configurationManager->getHomeDirectory()
+            ),
+            sprintf(
+                '%s/console/router.php',
+                $this->configurationManager->getApplicationDirectory()
+            ),
+            sprintf(
+                '%s/%s/config/dist/router.php',
+                $this->configurationManager->getApplicationDirectory(),
+                DRUPAL_CONSOLE_CORE
+            )
+        ];
 
-        if (file_exists($router)) {
-            return $router;
-        }
-
-        $router = sprintf(
-            '%s/config/dist/router.php',
-            $this->configurationManager->getApplicationDirectory()
-        );
-
-        if (file_exists($router)) {
-            return $router;
+        foreach ($routerPath as $router) {
+            if (file_exists($router)) {
+                return $router;
+            }
         }
 
         return null;
