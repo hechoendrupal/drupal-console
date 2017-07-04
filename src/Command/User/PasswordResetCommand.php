1d9198fc9babf47048703d3f23266d9343a5dcaf
Víctor Méndez <victor.ivan.mendez@gmail.com>
2017-07-04 15:47:46 -0500

    Password reset command properly indexed

--- src/Command/User/PasswordResetCommand.php
+++ src/Command/User/PasswordResetCommand.php
@@ -56,8 +56,16 @@ class PasswordResetCommand extends Command
             ->setName('user:password:reset')
             ->setDescription($this->trans('commands.user.password.reset.description'))
             ->setHelp($this->trans('commands.user.password.reset.help'))
-            ->addArgument('user', InputArgument::REQUIRED, $this->trans('commands.user.password.reset.options.user-id'))
-            ->addArgument('password', InputArgument::REQUIRED, $this->trans('commands.user.password.reset.options.password'));
+            ->addArgument(
+                'user',
+                InputArgument::REQUIRED,
+                $this->trans('commands.user.password.reset.options.user-id')
+            )
+            ->addArgument(
+                'password',
+                InputArgument::REQUIRED,
+                $this->trans('commands.user.password.reset.options.password')
+            );
     }
 
     /**
