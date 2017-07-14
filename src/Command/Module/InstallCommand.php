65983cb1a8d116db14fb9eb2e81c65e3f4a1c2ca
Blas <blasvicco@gmail.com>
2017-07-14 11:56:59 -0500

    Adding translation messages for commands (#3437)
    * Replacing in translation key from _ to -
    
    * Adding translation messages for the commands:
    config
    module:install
    taxonomy:term:delete

--- src/Command/Module/InstallCommand.php
+++ src/Command/Module/InstallCommand.php
@@ -175,14 +175,14 @@ class InstallCommand extends Command
                 if ($process->isSuccessful()) {
                     $io->info(
                         sprintf(
-                            'Module %s was downloaded with Composer.',
+                            $this->trans('commands.module.install.messages.download-with-composer'),
                             $moduleItem
                         )
                     );
                 } else {
                     $io->error(
                         sprintf(
-                            'Module %s seems not to be installed with Composer. Halting.',
+                            $this->trans('commands.module.install.messages.not-installed-with-composer'),
                             $moduleItem
                         )
                     );
@@ -202,7 +202,7 @@ class InstallCommand extends Command
                     unset($module[array_search($invalidModule, $module)]);
                     $io->error(
                         sprintf(
-                            'Invalid module name: %s',
+                            $this->trans('commands.module.install.messages.invalid-name'),
                             $invalidModule
                         )
                     );
