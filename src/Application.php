9d88bcdd8a6156b3e5eb89f27c47c663f2371b1c
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2017-08-05 16:32:53 -0600

    [console] Tag 1.0.0 stable release.

--- src/Application.php
+++ src/Application.php
@@ -25,7 +25,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const VERSION = '1.0.0-rc26';
+    const VERSION = '1.0.0';
 
     public function __construct(ContainerInterface $container)
     {
