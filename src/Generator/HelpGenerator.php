6fa6e4f0b83200c737cd82ddbf07ed68af2646cb
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2018-01-24 09:45:57 -0800

    [generate:help] Fix duplicated use statement.

--- src/Generator/HelpGenerator.php
+++ src/Generator/HelpGenerator.php
@@ -9,7 +9,6 @@ namespace Drupal\Console\Generator;
 
 use Drupal\Console\Core\Generator\GeneratorInterface;
 use Drupal\Console\Core\Generator\Generator;
-use Drupal\Console\Core\Generator\GeneratorInterface;
 use Drupal\Console\Extension\Manager;
 
 class HelpGenerator extends Generator implements GeneratorInterface
