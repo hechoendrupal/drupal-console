fde5701f540fdd25d25063d941850973a21f6f21
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2018-10-23 14:17:44 +0200

    Add ModuleAware class as a base for class AjaxCommand

--- src/Command/Generate/AjaxCommand.php
+++ src/Command/Generate/AjaxCommand.php
@@ -7,14 +7,12 @@
 
 namespace Drupal\Console\Command\Generate;
 
+use Drupal\Console\Command\ModuleAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Command\Shared\ServicesTrait;
-use Drupal\Console\Command\Shared\ConfirmationTrait;
-use Drupal\Console\Command\Shared\ModuleTrait;
 use Drupal\Console\Generator\AjaxCommandGenerator;
-use Drupal\Console\Core\Command\Command;
 use Drupal\Console\Core\Utils\ChainQueue;
 use Drupal\Console\Extension\Manager;
 use Drupal\Console\Utils\Validator;
@@ -24,26 +22,9 @@ use Drupal\Console\Utils\Validator;
  *
  * @package Drupal\Console\Command\Generate
  */
-class AjaxCommand extends Command
+class AjaxCommand extends ModuleAwareCommand
 {
-    use ModuleTrait;
     use ServicesTrait;
-    use ConfirmationTrait;
-
-    /**
-     * @var Manager
-     */
-    protected $extensionManager;
-
-    /**
-     * @var AjaxCommandGenerator
-     */
-    protected $generator;
-
-    /**
-     * @var Validator
-     */
-    protected $validator;
 
     /**
      * @var ChainQueue
@@ -53,22 +34,15 @@ class AjaxCommand extends Command
     /**
      * AjaxCommand constructor.
      *
-     * @param Manager              $extensionManager
      * @param AjaxCommandGenerator $generator
-     * @param Validator            $validator
      * @param ChainQueue           $chainQueue
      */
     public function __construct(
-        Manager $extensionManager,
         AjaxCommandGenerator $generator,
-        Validator $validator,
         ChainQueue $chainQueue
     ) {
-        $this->extensionManager = $extensionManager;
-        $this->generator = $generator;
-        $this->validator = $validator;
         $this->chainQueue = $chainQueue;
-        parent::__construct();
+        parent::__construct($generator);
     }
 
     /**
@@ -118,7 +92,7 @@ class AjaxCommand extends Command
         }
 
         $module = $this->validateModule($input->getOption('module'));
-        $class = $this->validator->validateClassName($input->getOption('class'));
+        $class = $this->validator()->validateClassName($input->getOption('class'));
         $method = $input->getOption('method');
         $js_name = $input->getOption('js-name');
 
@@ -152,7 +126,7 @@ class AjaxCommand extends Command
                 $this->trans('commands.generate.ajax.command.questions.class'),
                 'AjaxCommand',
                 function ($class) {
-                    return $this->validator->validateClassName($class);
+                    return $this->validator()->validateClassName($class);
                 }
             );
             $input->setOption('class', $class);
