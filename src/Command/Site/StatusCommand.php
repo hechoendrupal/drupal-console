df8b95b56b1816af19ec93a902b2676bebc69173
mondrake <mondrake.org@gmail.com>
2019-05-29 02:12:45 +0200

    [site:status] improvements (#4063)
    * Use Database::getConnectionInfoAsUrl to get the db URL
    
    * 1
    
    * 2
    
    * 3
    
    * 5
    
    * 6
    
    * 7
    
    * 8
    
    * 10
    
    * 11
    
    * 12
    
    * 13
    
    * 14
    
    * 15
    
    * 16
    
    * 17
    
    * 18
    
    * 19
    
    * 20
    
    * 21
    
    * Update StatusCommand.php
    
    * 22

--- src/Command/Site/StatusCommand.php
+++ src/Command/Site/StatusCommand.php
@@ -16,6 +16,8 @@ use Drupal\system\SystemManager;
 use Drupal\Core\Site\Settings;
 use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Extension\ThemeHandler;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  *  This command provides a report of the current drupal installation.
@@ -39,6 +41,20 @@ class StatusCommand extends ContainerAwareCommand
       'database',
       'theme',
       'directory',
+    ];
+
+    /**
+     * A list of system requirements to be skipped from output.
+     *
+     * @var array
+     */
+    protected $systemDataSkipList = [
+      // The PHP memory limit in CLI is different from the one available to the
+      // web server. Skip to avoid confusion.
+      'php_memory_limit',
+      // The web server cannot be determined in CLI since Drupal takes it from
+      // the $_SERVER variable in HTTP requests.
+      'webserver',
     ];
 
     /**
@@ -67,26 +83,34 @@ class StatusCommand extends ContainerAwareCommand
     protected $appRoot;
 
     /**
+     * @var RendererInterface
+     */
+    protected $renderer;
+
+    /**
      * DebugCommand constructor.
      *
-     * @param SystemManager $systemManager
-     * @param Settings      $settings
-     * @param ConfigFactory $configFactory
-     * @param ThemeHandler  $themeHandler
+     * @param SystemManager     $systemManager
+     * @param Settings          $settings
+     * @param ConfigFactory     $configFactory
+     * @param ThemeHandler      $themeHandler
      * @param $appRoot
+     * @param RendererInterface $renderer
      */
     public function __construct(
         SystemManager $systemManager = null,
         Settings $settings,
         ConfigFactory $configFactory,
         ThemeHandler $themeHandler,
-        $appRoot
+        $appRoot,
+        RendererInterface $renderer
     ) {
         $this->systemManager = $systemManager;
         $this->settings = $settings;
         $this->configFactory = $configFactory;
         $this->themeHandler = $themeHandler;
         $this->appRoot = $appRoot;
+        $this->renderer = $renderer;
         parent::__construct();
     }
 
@@ -149,14 +173,42 @@ class StatusCommand extends ContainerAwareCommand
         $systemData = [];
 
         foreach ($requirements as $key => $requirement) {
-            if ($requirement['title'] instanceof \Drupal\Core\StringTranslation\TranslatableMarkup) {
+            if (in_array($key, $this->systemDataSkipList)) {
+                continue;
+            }
+
+            if ($requirement['title'] instanceof TranslatableMarkup) {
                 $title = $requirement['title']->render();
             } else {
                 $title = $requirement['title'];
             }
 
-            $value = empty($requirement['description']) ? $requirement['value'] : $requirement['value'] . ' (' . $requirement['description'] . ')';
-            $systemData['system'][strip_tags($title)] = strip_tags($value); ;
+            $value = !empty($requirement['value']) ? strip_tags($requirement['value']) : '';
+            if (isset($requirement['severity'])) {
+                switch ($requirement['severity']) {
+                    case SystemManager::REQUIREMENT_ERROR:
+                        $value = "<error>$value</error>";
+                        break;
+
+                    case SystemManager::REQUIREMENT_WARNING:
+                        $value = "<comment>$value</comment>";
+                        break;
+
+                }
+            }
+
+            if ($this->getIo()->isVerbose()) {
+                $description = !empty($requirement['description']) ? $requirement['description'] : null;
+                if ($description instanceof TranslatableMarkup) {
+                    $description = $description->render();
+                }
+                if (is_array($description)) {
+                    $description = $this->renderer->renderPlain($description);
+                }
+                $value .= $description ? ' (' . strip_tags($description) . ')' : '';
+            }
+
+            $systemData['system'][strip_tags($title)] = $value;
         }
 
 
@@ -185,8 +237,10 @@ class StatusCommand extends ContainerAwareCommand
                 continue;
             }
 
-            $connectionKey = $this->trans('commands.site.status.messages.' . $connectionInfoKey);
-            $connectionData['database'][$connectionKey] = $connectionInfo['default'][$connectionInfoKey];
+            if (!empty($connectionInfo['default'][$connectionInfoKey])) {
+                $connectionKey = $this->trans('commands.site.status.messages.' . $connectionInfoKey);
+                $connectionData['database'][$connectionKey] = $connectionInfo['default'][$connectionInfoKey];
+            }
         }
 
         $connection_url = Database::getConnectionInfoAsUrl();
