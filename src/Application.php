7523ed9bf2c8d6c3302e90e3aba7bbbfc8f2714b
enzo - Eduardo Garcia <enzo@weknowinc.com>
2019-08-21 04:50:40 +1000

    Update version to 1.9.2

--- src/Application.php
+++ src/Application.php
@@ -25,7 +25,7 @@ class Application extends BaseApplication
     /**
      * @var string
      */
-    const VERSION = '1.9.1';
+    const VERSION = '1.9.2';
 
     public function __construct(ContainerInterface $container)
     {
