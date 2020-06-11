e8c04b597559aef9c1fb2c18ba3c247bcc70502d
John Brandenburg <jbrandenburg@forumone.com>
2020-06-11 16:58:38 -0400

    Throws exception when unable to load module extension for module generator.

--- src/Generator/ComposerGenerator.php
+++ src/Generator/ComposerGenerator.php
@@ -39,13 +39,18 @@ class ComposerGenerator extends Generator
      */
     public function generate(array $parameters)
     {
-        $machineName = $parameters['machine_name'];
-        $this->renderFile(
-          'module/composer.json.twig',
-          $this->extensionManager->getModule($machineName)
-            ->getPath() . '/composer.json',
-          $parameters
-        );
+      $machineName = $parameters['machine_name'];
+      $module = $this->extensionManager->getModule($machineName);
+      if (!$module) {
+        throw new \Exception(
+          "Unable to load module: \"{$machineName}\" from extension manager. This may be an unresolved issue in the module generator. This will prevent the generator from creating the composer.json file for the module. Try calling the command without setting dependencies. See https://github.com/hechoendrupal/drupal-console/issues/4118");
+      }
+      $this->renderFile(
+        'module/composer.json.twig',
+        $this->extensionManager->getModule($machineName)
+          ->getPath() . '/composer.json',
+        $parameters
+      );
     }
 
 }
